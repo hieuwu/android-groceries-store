3d3f5e1968cd41ea019ae827de7fc5e9fd670a79
hieuvu <hieuvu@floatingcube.com>
2021-05-13 18:03:59 +0700

    Remove unecessary generated code

---
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/fragments/ProductDetailFragment.kt
@@ -0,0 +1,21 @@
+package com.hieuwu.groceriesstore.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hieuwu.groceriesstore.R
+
+class ProductDetailFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+
+        return inflater.inflate(R.layout.fragment_product_detail, container, false)
+    }
+
+}
