071b5bbe1a9f9d7f8621324509dbca91db1d5195
hieuvu <hieuvu@floatingcube.com>
2021-05-14 16:35:23 +0700

    Setup recycler view with mock data

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/fragments/ExploreFragment.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/fragments/ExploreFragment.kt
@@ -1,28 +1,20 @@
 package com.hieuwu.groceriesstore.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hieuwu.groceriesstore.R
+import com.hieuwu.groceriesstore.adapters.GridListItemAdapter
 import com.hieuwu.groceriesstore.databinding.FragmentExploreBinding
-import com.hieuwu.groceriesstore.databinding.FragmentShopBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ExploreFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ExploreFragment : Fragment() {
     private lateinit var binding: FragmentExploreBinding
 
@@ -34,11 +26,31 @@ class ExploreFragment : Fragment() {
         binding = DataBindingUtil.inflate<FragmentExploreBinding>(
             inflater, R.layout.fragment_explore, container, false
         )
+        var dataSet: ArrayList<String> = ArrayList()
+        dataSet.add("a")
+        dataSet.add("b")
+        dataSet.add("a")
+        dataSet.add("b")
+        dataSet.add("a")
+        dataSet.add("b")
+        dataSet.add("a")
+        dataSet.add("b")
+        dataSet.add("a")
+        dataSet.add("b")
 
         var navController = NavHostFragment.findNavController(this)
         var bottomNavView = binding.bottomNavView
+
         bottomNavView.setupWithNavController(navController)
 
+        setUpRecyclerView(dataSet)
         return binding.root
+    }
+
+    private fun setUpRecyclerView(dataSet: ArrayList<String>) {
+
+        val adapter = GridListItemAdapter(dataSet)
+        binding.productRecyclerview.layoutManager = GridLayoutManager(this.context, 2)
+        binding.productRecyclerview.adapter = adapter
     }
 }
