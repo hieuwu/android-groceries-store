00ed8e7f43a87920f6ad58eb6992c47d5af0e131
Hieu Vu <star.hieu.99@gmail.com>
2021-06-09 23:15:58 +0700

    Hook viewmodel to UI controller in sign up screen

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/SignInFragment.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/SignInFragment.kt
@@ -6,9 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.databinding.FragmentSigninBinding
+import com.hieuwu.groceriesstore.presentation.viewmodels.SignInViewModel
+import com.hieuwu.groceriesstore.presentation.viewmodels.factory.ViewModelFactory
 
 
 class SignInFragment : Fragment() {
@@ -30,6 +33,13 @@ class SignInFragment : Fragment() {
         binding.signinButton.setOnClickListener {
             view?.findNavController()?.navigate(R.id.action_signInFragment_to_shopFragment)
         }
+
+        val viewModelFactory = ViewModelFactory(null, null)
+        val signInViewModel = ViewModelProvider(this, viewModelFactory)
+            .get(SignInViewModel::class.java)
+        binding.signInViewModel = signInViewModel
+
+        binding.lifecycleOwner = this
         return binding.root
     }
 
