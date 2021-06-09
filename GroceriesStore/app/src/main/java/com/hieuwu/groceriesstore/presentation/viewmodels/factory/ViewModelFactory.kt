1775b0a3c4f0545bd839d9d5fd9ae679b275e7d0
Hieu Vu <star.hieu.99@gmail.com>
2021-06-09 23:16:57 +0700

    Implement viewmodel factory

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/ViewModelFactory.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/ViewModelFactory.kt
@@ -1,4 +1,27 @@
 package com.hieuwu.groceriesstore.presentation.viewmodels.factory
 
-class ViewModelFactory {
+import android.animation.Animator
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.groceriesstore.data.GroceriesStoreDatabase
+import com.hieuwu.groceriesstore.presentation.viewmodels.SignInViewModel
+import com.hieuwu.groceriesstore.presentation.viewmodels.SignUpViewModel
+
+class ViewModelFactory(
+    private val valdataSource: GroceriesStoreDatabase?,
+    private val application: Application?
+) : ViewModelProvider.Factory {
+
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SignInViewModel::class.java)) {
+            return SignInViewModel() as T
+        }
+
+        if (modelClass.isAssignableFrom(SignUpViewModel::class.java)) {
+            return SignUpViewModel() as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
