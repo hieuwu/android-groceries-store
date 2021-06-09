3a20e9211dd2db8fd663093eed63787c5234bd15
Hieu Vu <star.hieu.99@gmail.com>
2021-06-09 23:16:19 +0700

    Hook viewmodel for sign in screen

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/SignUpFragment.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/SignUpFragment.kt
@@ -6,8 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.databinding.FragmentSignUpBinding
+import com.hieuwu.groceriesstore.presentation.viewmodels.SignInViewModel
+import com.hieuwu.groceriesstore.presentation.viewmodels.SignUpViewModel
+import com.hieuwu.groceriesstore.presentation.viewmodels.factory.ViewModelFactory
 
 class SignUpFragment : Fragment() {
 
@@ -20,6 +24,13 @@ class SignUpFragment : Fragment() {
         binding = DataBindingUtil.inflate<FragmentSignUpBinding>(
             inflater, R.layout.fragment_sign_up, container, false
         )
+
+        val viewModelFactory = ViewModelFactory(null, null)
+        val signUpViewModel = ViewModelProvider(this, viewModelFactory)
+            .get(SignUpViewModel::class.java)
+        binding.signUpViewModel = signUpViewModel
+
+        binding.lifecycleOwner = this
         return binding.root
     }
 
