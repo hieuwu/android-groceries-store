affa24ca0c83848070bfd37094968b6ea525c19e
Hieu Vu <star.hieu.99@gmail.com>
2021-06-11 12:10:47 +0700

    Import package

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/mapper/AtypeToBtypeImpl.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/mapper/AtypeToBtypeImpl.kt
@@ -3,6 +3,7 @@ package com.hieuwu.groceriesstore.data.mapper
 import com.hieuwu.groceriesstore.domain.mapper.AtypeToBtype
 import com.hieuwu.groceriesstore.domain.models.Order
 import com.hieuwu.groceriesstore.domain.models.Product
+import javax.inject.Inject
 
 class AtypeToBtypeImpl @Inject constructor() : AtypeToBtype {
     override fun map(from: Product): Order {
