b057efcc093788ddfc70306e89849d0c952d3ce2
Hieu Vu <star.hieu.99@gmail.com>
2021-07-08 15:55:09 +0700

    Create auth activity

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/activities/AuthActivity.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/activities/AuthActivity.kt
@@ -2,6 +2,7 @@ package com.hieuwu.groceriesstore.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hieuwu.groceriesstore.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
