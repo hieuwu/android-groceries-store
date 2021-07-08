5d78c538b01df4cfe74de36760349fbd6239bcc7
Hieu Vu <star.hieu.99@gmail.com>
2021-07-08 15:36:24 +0700

    Create Auth Activity to separate the authentication flow

---
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/activities/AuthActivity.kt
@@ -0,0 +1,11 @@
+package com.hieuwu.groceriesstore.presentation.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
