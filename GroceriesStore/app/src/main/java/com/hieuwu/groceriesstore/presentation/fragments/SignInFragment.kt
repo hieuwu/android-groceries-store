c0d67eedd64c48e17ed46ee53615ca96d27cbd83
Hieu Vu <star.hieu.99@gmail.com>
2021-07-10 21:48:04 +0700

    Navigate to main activity when click sign in button

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/SignInFragment.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/SignInFragment.kt
@@ -1,13 +1,17 @@
 package com.hieuwu.groceriesstore.presentation.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import com.hieuwu.groceriesstore.MainActivity
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.databinding.FragmentSigninBinding
 import com.hieuwu.groceriesstore.presentation.viewmodels.SignInViewModel
@@ -27,9 +31,7 @@ class SignInFragment : Fragment() {
             inflater, R.layout.fragment_signin, container, false
         )
 
-        binding.signUpTextview.setOnClickListener {
-            view?.findNavController()?.navigate(R.id.action_signInFragment_to_signUpFragment)
-        }
+
 
         val viewModelFactory = ViewModelFactory(null, null)
         val signInViewModel = ViewModelProvider(this, viewModelFactory)
@@ -37,6 +39,17 @@ class SignInFragment : Fragment() {
         binding.signInViewModel = signInViewModel
 
         binding.lifecycleOwner = this
+
+        binding.signUpTextview.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
+
+        binding.signinButton.setOnClickListener{
+            Log.d("SIGNIN", "${signInViewModel.email} - ${signInViewModel.password}")
+            var i = Intent(this.context, MainActivity::class.java)
+            startActivity(i)
+        }
+
         return binding.root
     }
 
