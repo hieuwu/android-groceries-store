54284b9d1c84be701a7706878a5bc20c2590929c
Hieu Vu <star.hieu.99@gmail.com>
2021-07-10 22:12:17 +0700

    Setup bottom navigation

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/MainActivity.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/MainActivity.kt
@@ -2,10 +2,23 @@ package com.hieuwu.groceriesstore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.appNavHostFragment
+        ) as NavHostFragment
+        var navController = navHostFragment.navController
+
+        // Setup the bottom navigation view with navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
