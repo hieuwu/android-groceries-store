3cb8df26b06cc6083e106ef3638613082cab451f
Hieu Vu <star.hieu.99@gmail.com>
2021-07-11 17:30:56 +0700

    Scope the instance to container

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/mapper/ProductModelToEntityImpl.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/mapper/ProductModelToEntityImpl.kt
@@ -1,13 +1,13 @@
 package com.hieuwu.groceriesstore.data.mapper
 
-import android.content.Context
 import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.mapper.ProductModelToEntity
 import com.hieuwu.groceriesstore.domain.models.ProductModel
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
-class ProductModelToEntityImpl @Inject constructor(private val context: Context) :
-    ProductModelToEntity {
+@ActivityScoped
+class ProductModelToEntityImpl @Inject constructor() : ProductModelToEntity {
     override fun map(from: ProductModel): Product {
         return Product(from.productId, from.productName, from.productName, from.productPrice)
     }
