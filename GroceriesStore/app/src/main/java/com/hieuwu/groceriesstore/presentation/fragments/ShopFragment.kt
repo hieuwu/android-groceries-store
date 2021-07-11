f3d627526864a6b4fd1486b39c5d3409221b0586
Hieu Vu <star.hieu.99@gmail.com>
2021-07-11 11:04:25 +0700

    Read sample data from json file

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/ShopFragment.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/ShopFragment.kt
@@ -1,19 +1,24 @@
 package com.hieuwu.groceriesstore.presentation.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.databinding.FragmentShopBinding
+import com.hieuwu.groceriesstore.domain.models.ProductModel
 import com.hieuwu.groceriesstore.presentation.adapters.GridListItemAdapter
+import java.io.IOException
 
 class ShopFragment : Fragment() {
     private lateinit var binding: FragmentShopBinding
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,6 +28,15 @@ class ShopFragment : Fragment() {
             inflater, R.layout.fragment_shop, container, false
         )
 
+        val jsonFileString = getJsonDataFromAsset(this.requireContext(), "SampleData.json")
+
+        val gson = Gson()
+        val listPersonType = object : TypeToken<List<ProductModel>>() {}.type
+        var products: List<ProductModel> = gson.fromJson(jsonFileString, listPersonType)
+
+        products.forEachIndexed { idx, product ->
+            Log.i("data", "> Item $idx:\n$product")
+        }
         return binding.root
     }
 
@@ -41,6 +55,17 @@ class ShopFragment : Fragment() {
         dataSet.add("b")
 
         setUpRecyclerView(dataSet)
+    }
+
+    private fun getJsonDataFromAsset(context: Context, fileName: String): String? {
+        val jsonString: String
+        try {
+            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
+        }
+        return jsonString
     }
 
     private fun setUpRecyclerView(dataSet: ArrayList<String>) {
