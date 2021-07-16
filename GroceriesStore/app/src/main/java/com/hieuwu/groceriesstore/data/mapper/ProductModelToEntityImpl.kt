0a4d402727c4d1ac7de80a88b2f39f8bda276220
Hieu Vu <star.hieu.99@gmail.com>
2021-07-16 22:45:57 +0700

    Scope mapping to singleton

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/mapper/ProductModelToEntityImpl.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/mapper/ProductModelToEntityImpl.kt
@@ -3,10 +3,12 @@ package com.hieuwu.groceriesstore.data.mapper
 import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.mapper.ProductModelToEntity
 import com.hieuwu.groceriesstore.domain.models.ProductModel
-import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class ProductModelToEntityImpl @Inject constructor() : ProductModelToEntity {
+
     override fun map(from: ProductModel): Product {
         return Product(from.productId, from.productName, from.productName, from.productPrice)
     }
