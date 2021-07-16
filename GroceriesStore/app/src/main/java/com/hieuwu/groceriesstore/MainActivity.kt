88a8e3c9e2168e7bc2c015919f0ca11ce64d0ecd
Hieu Vu <star.hieu.99@gmail.com>
2021-07-16 22:44:47 +0700

    Inject to activity

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/MainActivity.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
