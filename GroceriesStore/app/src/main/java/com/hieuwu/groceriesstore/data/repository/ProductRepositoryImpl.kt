9cd880c03ec58ceba2d95bb2c3fc1201817b462d
Hieu Vu <star.hieu.99@gmail.com>
2021-07-16 21:17:27 +0700

    Implement abstract repository

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/repository/ProductRepositoryImpl.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/repository/ProductRepositoryImpl.kt
@@ -1,6 +1,27 @@
 package com.hieuwu.groceriesstore.data.repository
 
+import com.hieuwu.groceriesstore.data.dao.ProductDao
+import com.hieuwu.groceriesstore.domain.entities.Product
+import com.hieuwu.groceriesstore.domain.mapper.ProductModelToEntity
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import javax.inject.Inject
 
-class ProductRepositoryImpl: ProductRepository {
+
+class ProductRepositoryImpl @Inject constructor() : ProductRepository {
+    @Inject
+    lateinit var productMapper: ProductModelToEntity
+
+    @Inject
+    lateinit var productDao: ProductDao
+    override suspend fun saveAll(products: List<Product>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAll(products: List<Product>): List<Product> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getById(id: List<Product>): Product {
+        TODO("Not yet implemented")
+    }
 }
--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/repository/Repository.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/repository/Repository.kt
@@ -1,4 +1,9 @@
 package com.hieuwu.groceriesstore.data.repository
 
+import com.hieuwu.groceriesstore.domain.entities.Product
+
 interface Repository {
+    suspend fun saveAll(products: List<Product>): Boolean
+    suspend fun getAll(products: List<Product>): List<Product>
+    suspend fun getById(id: List<Product>): Product
 }
