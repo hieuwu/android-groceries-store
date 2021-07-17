3d14167ccc3ee70cfd2641cdf4b6393cf97b2b38
Hieu Vu <star.hieu.99@gmail.com>
2021-07-17 10:41:39 +0700

    Move init data to viewmodel

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/ShopFragment.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/ShopFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -18,6 +19,8 @@ import com.hieuwu.groceriesstore.di.ProductMapper
 import com.hieuwu.groceriesstore.domain.mapper.ProductModelToEntity
 import com.hieuwu.groceriesstore.domain.models.ProductModel
 import com.hieuwu.groceriesstore.presentation.adapters.GridListItemAdapter
+import com.hieuwu.groceriesstore.presentation.viewmodels.ShopViewModel
+import com.hieuwu.groceriesstore.presentation.viewmodels.SignUpViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.IOException
 import javax.inject.Inject
@@ -26,8 +29,6 @@ import javax.inject.Inject
 class ShopFragment : Fragment() {
     private lateinit var binding: FragmentShopBinding
 
-    @ProductMapper
-    @Inject lateinit var productModelToEntity: ProductModelToEntity
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,18 +38,11 @@ class ShopFragment : Fragment() {
             inflater, R.layout.fragment_shop, container, false
         )
 
-        val jsonFileString = getJsonDataFromAsset(this.requireContext(), "SampleData.json")
+        val shopViewModel = ViewModelProvider(this)
+            .get(ShopViewModel::class.java)
+        shopViewModel.getJsonDataFromAsset(this.requireContext(),"SampleData.json")
 
-        val gson = Gson()
-        val listPersonType = object : TypeToken<List<ProductModel>>() {}.type
-        var products: List<ProductModel> = gson.fromJson(jsonFileString, listPersonType)
-
-        products.forEachIndexed { idx, product ->
-            var pro = productModelToEntity.map(product)
-            Log.i("data", "> Item $idx:\n$product")
-            Log.i("data x", "> Item $idx:\n$pro")
-
-        }
+        binding.lifecycleOwner = this
         return binding.root
     }
 
@@ -69,16 +63,6 @@ class ShopFragment : Fragment() {
         setUpRecyclerView(dataSet)
     }
 
-    private fun getJsonDataFromAsset(context: Context, fileName: String): String? {
-        val jsonString: String
-        try {
-            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
-        } catch (ioException: IOException) {
-            ioException.printStackTrace()
-            return null
-        }
-        return jsonString
-    }
 
     private fun setUpRecyclerView(dataSet: ArrayList<String>) {
         val adapter =
@@ -86,13 +70,16 @@ class ShopFragment : Fragment() {
                 dataSet
             )
         binding.exclusiveOfferRecyclerview.adapter = adapter
-        binding.exclusiveOfferRecyclerview.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, true)
+        binding.exclusiveOfferRecyclerview.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, true)
 
         binding.bestSellingRecyclerview.adapter = adapter
-        binding.bestSellingRecyclerview.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, true)
+        binding.bestSellingRecyclerview.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, true)
 
         binding.recommendedRecyclerview.adapter = adapter
-        binding.recommendedRecyclerview.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, true)
+        binding.recommendedRecyclerview.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, true)
     }
 
 
