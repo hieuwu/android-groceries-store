706cabf0852da04aa6efd2c9e4fedc73d2023565
Hieu Vu <star.hieu.99@gmail.com>
2021-07-17 10:41:56 +0700

    Separate functions

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/ShopViewModel.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/ShopViewModel.kt
@@ -1,4 +1,49 @@
 package com.hieuwu.groceriesstore.presentation.viewmodels
 
-class ShopViewModel {
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.hieuwu.groceriesstore.di.ProductMapper
+import com.hieuwu.groceriesstore.domain.mapper.ProductModelToEntity
+import com.hieuwu.groceriesstore.domain.models.ProductModel
+import java.io.IOException
+import javax.inject.Inject
+
+class ShopViewModel : ViewModel() {
+
+    @ProductMapper
+    @Inject
+    lateinit var productModelToEntity: ProductModelToEntity
+
+    lateinit var jsonFileString: String
+    lateinit var products: List<ProductModel>
+
+    init {
+        val gson = Gson()
+        val listPersonType = object : TypeToken<List<ProductModel>>() {}.type
+        var products: List<ProductModel> = gson.fromJson(jsonFileString, listPersonType)
+    }
+
+    fun saveProducts() {
+        products.forEachIndexed { idx, product ->
+            var pro = productModelToEntity.map(product)
+            Log.i("data", "> Item $idx:\n$product")
+            Log.i("data x", "> Item $idx:\n$pro")
+        }
+    }
+
+
+    fun getJsonDataFromAsset(context: Context, fileName: String) {
+        val jsonString: String
+        try {
+            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return
+        }
+        jsonFileString = jsonString
+    }
+
 }
