8165a06d765b416402a47a6a0f402e1db9e4e9d1
Hieu Vu <star.hieu.99@gmail.com>
2021-07-18 14:02:21 +0700

    Set up database module

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/di/DatabaseModule.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/di/DatabaseModule.kt
@@ -1,4 +1,33 @@
 package com.hieuwu.groceriesstore.di
 
-class DatabaseModule {
+import android.content.Context
+import androidx.room.Room
+import com.hieuwu.groceriesstore.data.GroceriesStoreDatabase
+import com.hieuwu.groceriesstore.data.dao.ProductDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context): GroceriesStoreDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            GroceriesStoreDatabase::class.java,
+            "groceries_store.db"
+        ).build()
+    }
+
+    @Provides
+    fun provideLogDao(database: GroceriesStoreDatabase): ProductDao {
+        return database.productDao()
+    }
 }
