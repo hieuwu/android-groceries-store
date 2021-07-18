76b35e2c293b360d931a7ebbaf5a907694dfaace
Hieu Vu <star.hieu.99@gmail.com>
2021-07-18 14:01:22 +0700

    Add repository to viewmodel factory

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/ShopViewModelFactory.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/ShopViewModelFactory.kt
@@ -1,4 +1,19 @@
 package com.hieuwu.groceriesstore.presentation.viewmodels.factory
 
-class ShopViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.groceriesstore.data.dao.ProductDao
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.presentation.viewmodels.ShopViewModel
+
+class ShopViewModelFactory(
+    private val repo: ProductRepository
+) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ShopViewModel::class.java)) {
+            return ShopViewModel(repo) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
