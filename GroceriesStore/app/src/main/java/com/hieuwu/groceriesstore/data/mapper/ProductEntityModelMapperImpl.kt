e89e9c41a8b36cfcc63560f87fc8d638412fd829
Hieu Vu <star.hieu.99@gmail.com>
2021-07-18 22:31:48 +0700

    Implement 2 way mapping for product entity

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/mapper/ProductEntityModelMapperImpl.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/mapper/ProductEntityModelMapperImpl.kt
@@ -1,4 +1,18 @@
 package com.hieuwu.groceriesstore.data.mapper
 
-class ProductEntityModelMapperImpl {
+import com.hieuwu.groceriesstore.domain.entities.Product
+import com.hieuwu.groceriesstore.domain.mapper.ProductEntityModelMapper
+import com.hieuwu.groceriesstore.domain.models.ProductModel
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ProductEntityModelMapperImpl @Inject constructor(): ProductEntityModelMapper {
+    override fun mapFromEntity(from: Product): ProductModel {
+        return ProductModel(from.id, from.name,from.price,null)
+    }
+
+    override fun mapToEntity(from: ProductModel): Product {
+        return Product(from.productId!!, from.productName, from.productName, from.productPrice)
+    }
 }
