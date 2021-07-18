c8d1b1797a13bf2861e1373e749e83d2675a74b7
Hieu Vu <star.hieu.99@gmail.com>
2021-07-18 14:02:50 +0700

    Implement repository to save data

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/repository/ProductRepositoryImpl.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/repository/ProductRepositoryImpl.kt
@@ -1,21 +1,41 @@
 package com.hieuwu.groceriesstore.data.repository
 
+import android.util.Log
 import com.hieuwu.groceriesstore.data.dao.ProductDao
+import com.hieuwu.groceriesstore.di.ProductMapper
 import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.mapper.ProductModelToEntity
+import com.hieuwu.groceriesstore.domain.models.ProductModel
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
+class ProductRepositoryImpl @Inject constructor(
+    private val productDao: ProductDao
+) : ProductRepository {
+    private val executorService: ExecutorService = Executors.newFixedThreadPool(4)
 
-class ProductRepositoryImpl @Inject constructor() : ProductRepository {
+    @ProductMapper
     @Inject
     lateinit var productMapper: ProductModelToEntity
 
-    @Inject
-    lateinit var productDao: ProductDao
-    override suspend fun saveAll(products: List<Product>): Boolean {
-        TODO("Not yet implemented")
+    override fun saveAll(products: List<ProductModel>): Boolean {
+        products.forEachIndexed { idx, product ->
+            val pro = productMapper.map(product)
+            saveProduct(pro)
+        }
+        return true
     }
+
+    private fun saveProduct(product: Product) {
+        executorService.execute {
+            productDao.insert(product)
+        }
+    }
+
 
     override suspend fun getAll(products: List<Product>): List<Product> {
         TODO("Not yet implemented")
