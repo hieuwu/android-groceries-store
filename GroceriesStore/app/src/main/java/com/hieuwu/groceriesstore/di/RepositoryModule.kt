a7ed14d190862530015e121b52d56bcba6b34f14
Hieu Vu <star.hieu.99@gmail.com>
2021-07-18 14:02:36 +0700

    Set up repository module

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/di/RepositoryModule.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/di/RepositoryModule.kt
@@ -1,4 +1,23 @@
 package com.hieuwu.groceriesstore.di
 
-class RepositoryModule {
+import com.hieuwu.groceriesstore.data.repository.ProductRepositoryImpl
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Qualifier
+annotation class ProductRepo
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class ProductRepositoryModule {
+
+    @ProductRepo
+    @Singleton
+    @Binds
+    abstract fun bindRepository(impl: ProductRepositoryImpl): ProductRepository
 }
