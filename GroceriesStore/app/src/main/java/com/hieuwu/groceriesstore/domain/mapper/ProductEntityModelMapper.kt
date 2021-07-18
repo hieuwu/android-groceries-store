e4f9607e26f3a3ff366c8dc8f1020744984f4f99
Hieu Vu <star.hieu.99@gmail.com>
2021-07-18 22:30:53 +0700

    Create product mapper

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/mapper/ProductEntityModelMapper.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/mapper/ProductEntityModelMapper.kt
@@ -1,4 +1,11 @@
 package com.hieuwu.groceriesstore.domain.mapper
 
-interface ProductEntityModelMapper {
+import com.hieuwu.groceriesstore.domain.entities.Product
+import com.hieuwu.groceriesstore.domain.mapper.entitymapper.EntityModelMapper
+import com.hieuwu.groceriesstore.domain.models.ProductModel
+
+interface ProductEntityModelMapper: EntityModelMapper<Product, ProductModel> {
+    override fun mapFromEntity(from: Product): ProductModel
+
+    override fun mapToEntity(from: ProductModel): Product
 }
