78dbc989e7ba3caee3214b8fafd16f2c777466c5
Hieu Vu <star.hieu.99@gmail.com>
2021-08-02 11:15:42 +0700

    Refactor grid adapter with diff util

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/GridListItemAdapter.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/GridListItemAdapter.kt
@@ -2,50 +2,46 @@ package com.hieuwu.groceriesstore.presentation.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.databinding.LayoutGridListItemBinding
+import com.hieuwu.groceriesstore.domain.models.ProductModel
 
-class GridListItemAdapter(private val dataSet: ArrayList<String>) :
-    RecyclerView.Adapter<GridListItemAdapter.GridListItemHolder>() {
-
-    override fun onBindViewHolder(holder: GridListItemHolder, position: Int) {
-        holder.bind(dataSet[position])
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridListItemHolder {
-        return GridListItemHolder(
-            DataBindingUtil.inflate(
-                LayoutInflater.from(parent.context),
-                R.layout.layout_grid_list_item,
-                parent,
-                false
-            )
-        )
-    }
-
-
-    class GridListItemHolder(
-        private val binding: LayoutGridListItemBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
-        init {
-            binding.productAddButton.setOnClickListener {
-                print("Product item add button click")
-            }
+class GridListItemAdapter(val onClickListener: OnClickListener) :
+    ListAdapter<ProductModel, GridListItemAdapter.ProductGridViewHolder>(DiffCallback) {
+    class ProductGridViewHolder(private var binding: LayoutGridListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(productModel: ProductModel) {
+            binding.product = productModel
+            binding.executePendingBindings()
         }
 
-        fun bind(item: String) {
-            binding.apply {
-                binding.productTitle.text = item
-                binding.productDescription.text = item
-                binding.productPrice.text = item
-                executePendingBindings()
-            }
+    }
+
+    override fun onBindViewHolder(holder: ProductGridViewHolder, position: Int) {
+        val product = getItem(position)
+        holder.bind(product)
+        holder.itemView.setOnClickListener {
+            onClickListener
         }
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductGridViewHolder {
+        return ProductGridViewHolder(LayoutGridListItemBinding.inflate(LayoutInflater.from(parent.context)))
+    }
 
-    override fun getItemCount() = dataSet.size
+    companion object DiffCallback : DiffUtil.ItemCallback<ProductModel>() {
+        override fun areItemsTheSame(oldItem: ProductModel, newItem: ProductModel): Boolean {
+            return oldItem === newItem
+        }
 
+        override fun areContentsTheSame(oldItem: ProductModel, newItem: ProductModel): Boolean {
+            return oldItem.productId == newItem.productId
+        }
+    }
+
+    class OnClickListener(val clickListener: (product: ProductModel) -> Unit) {
+        fun onClick(product: ProductModel) = clickListener(product)
+    }
 }
