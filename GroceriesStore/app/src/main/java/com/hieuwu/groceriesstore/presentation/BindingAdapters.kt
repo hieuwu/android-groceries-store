83d651ab540621cd738651ae6f5c938017b3d586
Hieu Vu <star.hieu.99@gmail.com>
2021-08-02 11:15:27 +0700

    Implement binding adapter for recyclerview

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/BindingAdapters.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/BindingAdapters.kt
@@ -1,4 +1,13 @@
 package com.hieuwu.groceriesstore.presentation
 
-class BindingAdapters {
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.hieuwu.groceriesstore.domain.models.ProductModel
+import com.hieuwu.groceriesstore.presentation.adapters.GridListItemAdapter
+
+
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<ProductModel>?){
+    val adapter = recyclerView.adapter as GridListItemAdapter
+    adapter.submitList(data)
 }
