9c7c87ad69456f630f0b63d6f0f9b56e29087875
Hieu Vu <star.hieu.99@gmail.com>
2021-08-03 14:40:01 +0700

    Get data from repository for shop screen

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/ShopViewModel.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/ShopViewModel.kt
@@ -1,11 +1,20 @@
 package com.hieuwu.groceriesstore.presentation.viewmodels
 
 import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.models.ProductModel
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import java.io.IOException
 import javax.inject.Inject
 
@@ -13,10 +22,16 @@ class ShopViewModel @Inject constructor(
     private val productRepository: ProductRepository
 ) : ViewModel() {
 
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
     var jsonFileString: String
 
     init {
         jsonFileString = ""
+        viewModelScope.launch {
+            getMarsRealEstateProperties()
+        }
     }
 
     lateinit var products: List<ProductModel>
@@ -37,4 +52,12 @@ class ShopViewModel @Inject constructor(
         productRepository.saveAll(products)
     }
 
+    private val _productList = MutableLiveData<List<ProductModel>>()
+    val productList: LiveData<List<ProductModel>>
+        get() = _productList
+
+    private suspend fun getMarsRealEstateProperties() {
+        val result: List<ProductModel>? = productRepository.getAll()
+        if (result!!.isNotEmpty()) _productList.value = result
+    }
 }
