348a46a4de31938c6589bee6e2bd7e8c65dae125
Hieu Vu <star.hieu.99@gmail.com>
2021-08-03 17:14:59 +0700

    Refactor binding adapter

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/BindingAdapters.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/BindingAdapters.kt
@@ -2,12 +2,12 @@ package com.hieuwu.groceriesstore.presentation
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.hieuwu.groceriesstore.domain.models.ProductModel
+import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.presentation.adapters.GridListItemAdapter
 
 
 @BindingAdapter("listData")
-fun bindRecyclerView(recyclerView: RecyclerView, data: List<ProductModel>?){
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<Product>?){
     val adapter = recyclerView.adapter as GridListItemAdapter
     adapter.submitList(data)
 }
