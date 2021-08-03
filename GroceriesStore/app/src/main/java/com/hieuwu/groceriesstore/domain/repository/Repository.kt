3b59efb266f0601e117002ba79e98a9c44d66f98
Hieu Vu <star.hieu.99@gmail.com>
2021-08-03 14:40:44 +0700

    Refactor repository to return model

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/Repository.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/Repository.kt
@@ -1,10 +1,11 @@
 package com.hieuwu.groceriesstore.domain.repository
 
+import androidx.lifecycle.LiveData
 import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.models.ProductModel
 
 interface Repository {
     fun saveAll(products: List<ProductModel>): Boolean
-    suspend fun getAll(products: List<Product>): List<Product>
+    suspend fun getAll():  List<ProductModel>?
     suspend fun getById(id: List<Product>): Product
 }
