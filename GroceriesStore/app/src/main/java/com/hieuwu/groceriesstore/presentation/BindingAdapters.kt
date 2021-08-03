aac9267907a67d3e52d394cfd3b9e572cafd46eb
Hieu Vu <star.hieu.99@gmail.com>
2021-08-03 20:32:19 +0700

    Create binding glide for image url

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/BindingAdapters.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/BindingAdapters.kt
@@ -1,13 +1,32 @@
 package com.hieuwu.groceriesstore.presentation
 
+import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.presentation.adapters.GridListItemAdapter
 
 
 @BindingAdapter("listData")
-fun bindRecyclerView(recyclerView: RecyclerView, data: List<Product>?){
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<Product>?) {
     val adapter = recyclerView.adapter as GridListItemAdapter
     adapter.submitList(data)
+}
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions().placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image)
+            )
+            .into(imgView)
+    }
 }
