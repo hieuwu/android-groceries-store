ae05a172726b23758bdf2d9a86097273c35b466b
Hieu Vu <star.hieu.99@gmail.com>
2021-08-04 09:19:03 +0700

    Implement retrofit instance to get data

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/network/ApiService.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/network/ApiService.kt
@@ -1,4 +1,34 @@
 package com.hieuwu.groceriesstore.data.network
 
-class ApiService {
+import com.hieuwu.groceriesstore.domain.entities.Product
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://example.com/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsApiService {
+    @GET("products")
+    fun getProducts():
+            Deferred<List<Product>>
+}
+
+object Api {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
 }
