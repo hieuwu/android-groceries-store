06876f73fd240b893dcfa218c55c9dc3eb189811
Hieu Vu <star.hieu.99@gmail.com>
2021-08-04 09:19:44 +0700

    Move methods to product repository

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
@@ -1,8 +1,12 @@
 package com.hieuwu.groceriesstore.domain.repository
 
 import com.hieuwu.groceriesstore.domain.entities.Product
+import com.hieuwu.groceriesstore.domain.models.ProductModel
 import kotlinx.coroutines.flow.Flow
 
-interface ProductRepository : Repository {
+interface ProductRepository {
+    fun saveAll(products: List<ProductModel>): Boolean
+    suspend fun getAll(): List<ProductModel>?
+    suspend fun getById(id: List<Product>): Product
     fun getAllProducts(): Flow<List<Product>>
 }
