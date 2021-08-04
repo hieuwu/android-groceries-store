e338975ffc2f52daae17d66777b67de98b49ed81
Hieu Vu <star.hieu.99@gmail.com>
2021-08-04 19:50:51 +0700

    Implement get data from server

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/repository/ProductRepositoryImpl.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/repository/ProductRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.hieuwu.groceriesstore.data.repository
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -9,6 +11,8 @@ import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.mapper.ProductEntityModelMapper
 import com.hieuwu.groceriesstore.domain.models.ProductModel
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -17,40 +21,50 @@ import javax.inject.Singleton
 
 @Singleton
 class ProductRepositoryImpl @Inject constructor(
-    private val productDao: ProductDao, private var fireStore: FirebaseFirestore
+    private val productDao: ProductDao
 ) : ProductRepository {
     private val executorService: ExecutorService = Executors.newFixedThreadPool(4)
-
-
-    init {
-        fireStore = Firebase.firestore
-        fireStore.collection("products").get().addOnSuccessListener { result ->
-            for (document in result) {
-                Timber.d("${document.id} => ${document.data}")
-            }
-        }
-            .addOnFailureListener { exception ->
-                Timber.w("Error getting documents.${exception}")
-            }
-    }
-
-
 
     @EntityModelProductMapper
     @Inject
     lateinit var productModelEntityMapper: ProductEntityModelMapper
 
     override fun saveAll(products: List<ProductModel>): Boolean {
-        for (item in products) {
-            val product = productModelEntityMapper.mapToEntity(item)
-            saveProduct(product)
-        }
+//        for (item in products) {
+//            val product = productModelEntityMapper.mapToEntity(item)
+//            saveProduct(product)
+//        }
         return true
     }
+
     private fun saveProduct(product: Product) {
         executorService.execute {
             productDao.insert(product)
         }
+    }
+
+    override suspend fun getFromServer() {
+        var fireStore = Firebase.firestore
+        var products = mutableListOf<Product>()
+
+        fireStore.collection("products").get().addOnSuccessListener { result ->
+            for (document in result) {
+                var id = document.id
+                var name: String = document.data["name"] as String
+                var description: String = document.data["description"] as String
+                var price: Number = document.data["price"] as Number
+                var image: String = document.data["image"] as String
+                executorService.execute {
+                    productDao.insert(Product(id, name, description, price.toDouble(), image))
+                }
+                Timber.d("${document.id} => ${document.data}")
+            }
+        }
+            .addOnFailureListener { exception ->
+                Timber.w("Error getting documents.${exception}")
+            }
+
+
     }
 
 
