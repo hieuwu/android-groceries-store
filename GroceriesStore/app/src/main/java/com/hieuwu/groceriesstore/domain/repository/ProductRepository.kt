17e7cd1821d438289e60a89004858685f225bcb8
Hieu Vu <star.hieu.99@gmail.com>
2021-08-05 09:09:12 +0700

    Remove unused code

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
@@ -5,8 +5,6 @@ import com.hieuwu.groceriesstore.domain.models.ProductModel
 import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
-    fun saveAll(products: List<ProductModel>): Boolean
-    suspend fun getAll(): List<ProductModel>?
     suspend fun getById(id: List<Product>): Product
     fun getAllProducts(): Flow<List<Product>>
     suspend fun getFromServer()
