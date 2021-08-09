4c89de7b455e4772ebdfa855c5e6202ef6c24380
Hieu Vu <star.hieu.99@gmail.com>
2021-08-09 09:54:13 +0700

    Bind view for product detail

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/ProductDetailFragment.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/ProductDetailFragment.kt
@@ -5,17 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.hieuwu.groceriesstore.R
+import com.hieuwu.groceriesstore.databinding.FragmentProductDetailBinding
+import com.hieuwu.groceriesstore.databinding.FragmentShopBinding
 
 class ProductDetailFragment : Fragment() {
+    private lateinit var binding: FragmentProductDetailBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        binding = DataBindingUtil.inflate<FragmentProductDetailBinding>(
+            inflater, R.layout.fragment_product_detail, container, false
+        )
 
-
-        return inflater.inflate(R.layout.fragment_product_detail, container, false)
+        return binding.root
     }
 
 }
