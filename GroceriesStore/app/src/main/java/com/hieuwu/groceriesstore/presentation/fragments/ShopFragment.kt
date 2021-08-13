789f8b717bff42a1f78d83b3c53f018ea9a84ab6
Hieu Vu <star.hieu.99@gmail.com>
2021-08-13 09:00:11 +0700

    Add on item click to go to screen detail

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/ShopFragment.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/fragments/ShopFragment.kt
@@ -9,6 +9,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hieuwu.groceriesstore.R
@@ -55,8 +56,13 @@ class ShopFragment : Fragment() {
             })
 
         viewModel.navigateToSelectedProperty.observe(this.viewLifecycleOwner, Observer {
-            if ( null != it ) {
-                this.findNavController().navigate(R.id.action_shopFragment_to_productDetailFragment)
+
+            if (null != it) {
+                val direction =
+                    ShopFragmentDirections.actionShopFragmentToProductDetailFragment(
+                        it.id
+                    )
+                findNavController().navigate(direction)
                 viewModel.displayPropertyDetailsComplete()
             }
         })
@@ -71,16 +77,18 @@ class ShopFragment : Fragment() {
         binding.exclusiveOfferRecyclerview.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
-        binding.bestSellingRecyclerview.adapter = GridListItemAdapter(GridListItemAdapter.OnClickListener {
-            Timber.d("on item clicked")
-        })
+        binding.bestSellingRecyclerview.adapter =
+            GridListItemAdapter(GridListItemAdapter.OnClickListener {
+                Timber.d("on item clicked")
+            })
 
         binding.bestSellingRecyclerview.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
-        binding.recommendedRecyclerview.adapter = GridListItemAdapter(GridListItemAdapter.OnClickListener {
-            Timber.d("on item clicked")
-        })
+        binding.recommendedRecyclerview.adapter =
+            GridListItemAdapter(GridListItemAdapter.OnClickListener {
+                Timber.d("on item clicked")
+            })
 
         binding.recommendedRecyclerview.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
