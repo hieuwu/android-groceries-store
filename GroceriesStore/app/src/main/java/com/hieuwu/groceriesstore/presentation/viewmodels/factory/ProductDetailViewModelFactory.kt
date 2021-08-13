d14928f20e78d8117194e7fef057e230c3fa2416
Hieu Vu <star.hieu.99@gmail.com>
2021-08-13 08:59:17 +0700

    Implement view model factory

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/ProductDetailViewModelFactory.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/ProductDetailViewModelFactory.kt
@@ -1,4 +1,18 @@
 package com.hieuwu.groceriesstore.presentation.viewmodels.factory
 
-class ProductDetailViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.presentation.viewmodels.ProductDetailViewModel
+
+class ProductDetailViewModelFactory(
+    private val id: String,
+    private val productRepository: ProductRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ProductDetailViewModel::class.java)) {
+            return ProductDetailViewModel(id, productRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
