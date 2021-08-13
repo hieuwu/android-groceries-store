9d9429fc25f90b93482dd2ddd626fe709719fcec
Hieu Vu <star.hieu.99@gmail.com>
2021-08-13 08:59:02 +0700

    Implement to get product detail by id

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/ProductDetailViewModel.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/ProductDetailViewModel.kt
@@ -1,4 +1,18 @@
 package com.hieuwu.groceriesstore.presentation.viewmodels
 
-class ProductDetailViewModel {
+import androidx.lifecycle.*
+import com.hieuwu.groceriesstore.domain.entities.Product
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class ProductDetailViewModel @Inject constructor(
+    id: String,
+    private val productRepository: ProductRepository
+) : ViewModel() {
+
+    val product = productRepository.getById(id).asLiveData()
+
 }
