ae0b4e429d9df48504d6125a2c59d3bd32c3ee3a
Hieu Vu <star.hieu.99@gmail.com>
2021-08-14 09:18:21 +0700

    Implement quantity converter

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/utils/Converter.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/utils/Converter.kt
@@ -1,4 +1,21 @@
 package com.hieuwu.groceriesstore.presentation.utils
 
-class Converter {
+import android.widget.EditText
+import androidx.databinding.InverseMethod
+
+object Converter {
+    @InverseMethod("intToString")
+    @JvmStatic
+    fun intToString(
+        value: Int
+    ): String {
+        return value.toString()
+    }
+
+    @JvmStatic
+    fun stringToInt(
+        value: String
+    ): Int {
+        return value.toInt()
+    }
 }
