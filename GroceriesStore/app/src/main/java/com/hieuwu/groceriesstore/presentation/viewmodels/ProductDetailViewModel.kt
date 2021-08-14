ffd80927ac7078a00ab139656df5980f192d731e
Hieu Vu <star.hieu.99@gmail.com>
2021-08-14 09:19:17 +0700

    Implement increase and decrease method for quantity

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/ProductDetailViewModel.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/ProductDetailViewModel.kt
@@ -1,8 +1,11 @@
 package com.hieuwu.groceriesstore.presentation.viewmodels
 
+import androidx.databinding.Bindable
 import androidx.lifecycle.*
+import com.hieuwu.groceriesstore.BR
 import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -11,8 +14,28 @@ import javax.inject.Inject
 class ProductDetailViewModel @Inject constructor(
     id: String,
     private val productRepository: ProductRepository
-) : ViewModel() {
+) : ObservableViewModel() {
 
     val product = productRepository.getById(id).asLiveData()
+
+    private var _qty: Int = 0
+    var qty: Int
+        @Bindable
+        get() {
+            return _qty
+        }
+        set(value) {
+            _qty = value
+            notifyPropertyChanged(BR.qty)
+        }
+
+
+    fun increaseQty() {
+        qty++
+    }
+
+    fun decreaseQty() {
+        qty--
+    }
 
 }
