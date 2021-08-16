f5052b037fd596cf0f795cb353b0f4add30bde61
Hieu Vu <star.hieu.99@gmail.com>
2021-08-16 09:46:03 +0700

    Implement line item

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/entities/LineItem.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/entities/LineItem.kt
@@ -1,4 +1,14 @@
 package com.hieuwu.groceriesstore.domain.entities
 
-class LineItem {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.hieuwu.groceriesstore.data.utils.DataConstant
+
+@Entity(tableName = DataConstant.LINE_ITEM_TABLE)
+data class LineItem(
+    @PrimaryKey
+    @ColumnInfo(name = "lineItemId")
+    val id: Long,
+    val productId: String
+)
