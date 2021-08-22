325f667f7bbf5346da3f49d507c6965e3e82b740
Hieu Vu <star.hieu.99@gmail.com>
2021-08-22 15:43:44 +0700

    Add line item data access to database

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/GroceriesStoreDatabase.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/GroceriesStoreDatabase.kt
@@ -2,10 +2,13 @@ package com.hieuwu.groceriesstore.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.hieuwu.groceriesstore.data.dao.LineItemDao
 import com.hieuwu.groceriesstore.data.dao.ProductDao
+import com.hieuwu.groceriesstore.domain.entities.LineItem
 import com.hieuwu.groceriesstore.domain.entities.Product
 
-@Database(entities = [Product::class], version = 1, exportSchema = false)
+@Database(entities = [Product::class, LineItem::class], version = 1, exportSchema = false)
 abstract class GroceriesStoreDatabase : RoomDatabase() {
     abstract fun productDao(): ProductDao
+    abstract fun lineItemDao(): LineItemDao
 }
