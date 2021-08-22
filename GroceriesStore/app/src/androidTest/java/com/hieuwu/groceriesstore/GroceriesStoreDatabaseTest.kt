b706f3f6f02fb43fe229ff743a7cf6c8339a0584
Hieu Vu <star.hieu.99@gmail.com>
2021-08-22 15:43:26 +0700

    Write unit test for product and line item

--- GroceriesStore/app/src/androidTest/java/com/hieuwu/groceriesstore/GroceriesStoreDatabaseTest.kt
+++ GroceriesStore/app/src/androidTest/java/com/hieuwu/groceriesstore/GroceriesStoreDatabaseTest.kt
@@ -1,23 +1,34 @@
 package com.hieuwu.groceriesstore
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import androidx.room.Room
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
 import com.hieuwu.groceriesstore.data.GroceriesStoreDatabase
+import com.hieuwu.groceriesstore.data.dao.LineItemDao
 import com.hieuwu.groceriesstore.data.dao.ProductDao
+import com.hieuwu.groceriesstore.domain.entities.LineItem
 import com.hieuwu.groceriesstore.domain.entities.Product
 import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
 class GroceriesStoreDatabaseTest {
-
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var productDao: ProductDao
+    private lateinit var linetItemDao: LineItemDao
+
     private lateinit var db: GroceriesStoreDatabase
 
     @Before
@@ -29,7 +40,9 @@ class GroceriesStoreDatabaseTest {
             // Allowing main thread queries, just for testing.
             .allowMainThreadQueries()
             .build()
-        productDao = db.productDao
+        productDao = db.productDao()
+        linetItemDao = db.lineItemDao()
+
     }
 
     @After
@@ -41,9 +54,23 @@ class GroceriesStoreDatabaseTest {
     @Test
     @Throws(Exception::class)
     fun insertAndGetProduct() {
-        val aProduct = Product("123", "Product Test","Test", 12.0)
+        val aProduct = Product("123", "Product Test","Test", 12.0, "empty")
         productDao.insert(aProduct)
-        val product = productDao.getById(aProduct.id)
-        assertEquals(product?.price, 12.0)
+        var product = productDao.getById(aProduct.id).asLiveData()
+        assertEquals(product.getOrAwaitValue().price, 12.0)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetLineItem() {
+        val aProduct = Product("123", "Product Test","Test", 12.0, "empty")
+        productDao.insert(aProduct)
+        val lineItem = LineItem(1,aProduct.id,"123",4,5.6)
+        linetItemDao.insert(lineItem)
+        var lineItemAfter = linetItemDao.getById(lineItem.id).asLiveData()
+
+        assertEquals(lineItemAfter.getOrAwaitValue().productId, "123")
+        assertEquals(lineItemAfter.getOrAwaitValue().quantity, 4)
+        assertEquals(lineItemAfter.getOrAwaitValue().subtotal, 5.6)
     }
 }
