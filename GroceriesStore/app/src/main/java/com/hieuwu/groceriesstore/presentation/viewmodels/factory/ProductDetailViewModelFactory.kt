8331b6262485a9f15140a531fb92be036b7f51ce
Hieu Vu <star.hieu.99@gmail.com>
2021-08-24 11:13:21 +0700

    Refactor view model factory

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/ProductDetailViewModelFactory.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/ProductDetailViewModelFactory.kt
@@ -2,16 +2,18 @@ package com.hieuwu.groceriesstore.presentation.viewmodels.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
 import com.hieuwu.groceriesstore.presentation.viewmodels.ProductDetailViewModel
 
 class ProductDetailViewModelFactory(
     private val id: String,
-    private val productRepository: ProductRepository
+    private val productRepository: ProductRepository,
+    private val orderRepository: OrderRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ProductDetailViewModel::class.java)) {
-            return ProductDetailViewModel(id, productRepository) as T
+            return ProductDetailViewModel(id, productRepository, orderRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
