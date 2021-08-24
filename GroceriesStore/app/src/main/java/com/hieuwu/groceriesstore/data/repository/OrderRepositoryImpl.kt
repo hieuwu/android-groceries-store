ab9ca2fe82cb0bb65de6a3c1d45b43913bd09889
Hieu Vu <star.hieu.99@gmail.com>
2021-08-24 14:02:25 +0700

    Handle thread in repository

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/repository/OrderRepositoryImpl.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/data/repository/OrderRepositoryImpl.kt
@@ -6,8 +6,15 @@ import com.hieuwu.groceriesstore.data.utils.OrderStatus
 import com.hieuwu.groceriesstore.domain.entities.LineItem
 import com.hieuwu.groceriesstore.domain.entities.Order
 import com.hieuwu.groceriesstore.domain.entities.OrderWithLineItems
+import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.repository.OrderRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -16,25 +23,31 @@ class OrderRepositoryImpl @Inject constructor(
     private val orderDao: OrderDao,
     private val lineItemDao: LineItemDao
 ) : OrderRepository {
+    private val executorService: ExecutorService = Executors.newFixedThreadPool(4)
+
+
     override fun hasCart(): Flow<Boolean> {
         return orderDao.isCartExisted(OrderStatus.IN_CART.value)
     }
 
     override suspend fun insert(order: Order) {
-        orderDao.insert(order)
+        executorService.execute {
+            orderDao.insert(order)
+        }
     }
 
     override suspend fun addLineItem(lineItem: LineItem) {
-        lineItemDao.insert(lineItem)
+        executorService.execute {
+            lineItemDao.insert(lineItem)
+        }
     }
 
-    override fun getOrderById(id: String): Flow<OrderWithLineItems>{
+    override fun getOrderById(id: String): Flow<OrderWithLineItems> {
         return orderDao.getById(id)
     }
 
-    override suspend fun getOrderInCart(): Flow<Order> {
-        return orderDao.getOrderInCart()
+    override fun getOrderInCart(status: OrderStatus): Flow<Order> {
+        return orderDao.getOrderInCart(status.value)
     }
-
 
 }
