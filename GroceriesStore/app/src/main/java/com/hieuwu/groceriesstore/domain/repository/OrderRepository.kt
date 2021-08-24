15438dcc9120c505265fc96c382ed4855d2ecdf5
Hieu Vu <star.hieu.99@gmail.com>
2021-08-24 14:02:38 +0700

    Refactor order repository

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/OrderRepository.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/OrderRepository.kt
@@ -1,14 +1,15 @@
 package com.hieuwu.groceriesstore.domain.repository
 
+import com.hieuwu.groceriesstore.data.utils.OrderStatus
 import com.hieuwu.groceriesstore.domain.entities.LineItem
 import com.hieuwu.groceriesstore.domain.entities.Order
 import com.hieuwu.groceriesstore.domain.entities.OrderWithLineItems
 import kotlinx.coroutines.flow.Flow
 
 interface OrderRepository {
-    fun hasCart(): Flow<Boolean>
+    fun hasCart(): Flow<Boolean>?
     suspend fun insert(order: Order)
     suspend fun addLineItem(lineItem: LineItem)
     fun getOrderById(id: String): Flow<OrderWithLineItems>
-    suspend fun getOrderInCart(): Flow<Order>
+    fun getOrderInCart(status: OrderStatus): Flow<Order>
 }
