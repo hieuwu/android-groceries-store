91c9119efaa85c5af2a41f62f263934d34367745
Hieu Vu <star.hieu.99@gmail.com>
2021-08-24 11:13:07 +0700

    Add order repository to product detail view model

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/ProductDetailViewModel.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/ProductDetailViewModel.kt
@@ -3,15 +3,21 @@ package com.hieuwu.groceriesstore.presentation.viewmodels
 import androidx.databinding.Bindable
 import androidx.lifecycle.*
 import com.hieuwu.groceriesstore.BR
+import com.hieuwu.groceriesstore.data.utils.OrderStatus
 import com.hieuwu.groceriesstore.domain.entities.LineItem
+import com.hieuwu.groceriesstore.domain.entities.Order
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import kotlinx.coroutines.launch
+import java.util.*
 
 import javax.inject.Inject
 
 class ProductDetailViewModel @Inject constructor(
     id: String,
-    private val productRepository: ProductRepository
+    private val productRepository: ProductRepository,
+    private val orderRepository: OrderRepository
 ) : ObservableViewModel() {
 
     val product = productRepository.getById(id).asLiveData()
@@ -29,8 +35,26 @@ class ProductDetailViewModel @Inject constructor(
 
     fun addToCart() {
         val subtotal = product.value?.price?.times(qty)
+        val hasCart = orderRepository.hasCart().asLiveData()
+        if (hasCart.value!!) {
+            //Add to cart
+            val currentCart = orderRepository.getOrderById()
+            var lineItem = LineItem(product.value!!.id, currentCart!!.id, _qty, subtotal!!)
+            viewModelScope.launch {
+                orderRepository.addLineItem(lineItem)
+            }
 
-
+        } else {
+            var id = UUID.randomUUID().toString()
+            var newOrder = Order(id, OrderStatus.IN_CART.value)
+            viewModelScope.launch {
+                orderRepository.insert(newOrder)
+            }
+            var lineItem = LineItem(product.value!!.id, newOrder.id, _qty, subtotal!!)
+            viewModelScope.launch {
+                orderRepository.addLineItem(lineItem)
+            }
+        }
     }
 
     fun removeFromCart() {
