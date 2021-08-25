ebb7491f033ab0e7847aed2d6f66ce83a57a84e5
Hieu Vu <star.hieu.99@gmail.com>
2021-08-25 16:18:33 +0700

    Handle get data from local in cart view model

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/CartViewModel.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/CartViewModel.kt
@@ -1,4 +1,41 @@
 package com.hieuwu.groceriesstore.presentation.viewmodels
 
-class CartViewModel {
+import androidx.lifecycle.*
+import com.hieuwu.groceriesstore.domain.entities.ProductAndLineItem
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class CartViewModel @Inject constructor(private val productRepository: ProductRepository) :
+    ObservableViewModel() {
+    private var _lineItemList = MutableLiveData<List<ProductAndLineItem>>()
+    val lineItemList: LiveData<List<ProductAndLineItem>>
+        get() = _lineItemList
+
+    init {
+        getLineItemFromDatabase()
+    }
+
+    private fun getLineItemFromDatabase() {
+        viewModelScope.launch {
+            getLineItemFromLocal()
+        }
+    }
+
+    private suspend fun getLineItemFromLocal() {
+        return withContext(Dispatchers.IO) {
+            _lineItemList =
+                productRepository.getProductAndLineItem()
+                    .asLiveData() as MutableLiveData<List<ProductAndLineItem>>
+        }
+    }
+
+    fun displayPropertyDetails(it: ProductAndLineItem) {
+
+    }
+
 }
