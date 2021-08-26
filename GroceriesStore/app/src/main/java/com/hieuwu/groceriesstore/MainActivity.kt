a370120fb26439d7d0ab88b338634fe032be34b5
Hieu Vu <star.hieu.99@gmail.com>
2021-08-26 10:10:45 +0700

    Disable item cart clicked and navigate with fab

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/MainActivity.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/MainActivity.kt
@@ -2,9 +2,11 @@ package com.hieuwu.groceriesstore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,6 +24,13 @@ class MainActivity : AppCompatActivity() {
 
         // Setup the bottom navigation view with navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        val fabButton = findViewById<FloatingActionButton>(R.id.fabButton)
+        fabButton.setOnClickListener {
+            navController.navigate(R.id.cartFragment)
+        }
         bottomNavigationView.setupWithNavController(navController)
 
     }
