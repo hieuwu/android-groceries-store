c06eb33d3663c9ddba1580782fa8a16d78352633
Hieu Vu <star.hieu.99@gmail.com>
2021-08-28 22:34:28 +0700

    Add total price

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/CartViewModel.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/CartViewModel.kt
@@ -1,10 +1,12 @@
 package com.hieuwu.groceriesstore.presentation.viewmodels
 
 import androidx.lifecycle.*
+import androidx.navigation.fragment.findNavController
 import com.hieuwu.groceriesstore.domain.entities.ProductAndLineItem
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.presentation.fragments.ShopFragmentDirections
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
-import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.core.Flowable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -16,8 +18,14 @@ class CartViewModel @Inject constructor(private val productRepository: ProductRe
     val lineItemList: LiveData<List<ProductAndLineItem>>
         get() = _lineItemList
 
+    private var _totalPrice = MutableLiveData<Double>()
+    val totalPrice: LiveData<Double>
+        get() = _totalPrice
+
+
     init {
         getLineItemFromDatabase()
+        sumPrice()
     }
 
     private fun getLineItemFromDatabase() {
@@ -35,6 +43,17 @@ class CartViewModel @Inject constructor(private val productRepository: ProductRe
     }
 
     fun displayPropertyDetails(it: ProductAndLineItem) {
+
+    }
+
+    public fun sumPrice() {
+        var sum = 0.0
+        if (_lineItemList?.value != null) {
+            for (item in _lineItemList.value!!) {
+                sum = sum?.plus(1.0)
+            }
+        }
+        _totalPrice.value = sum
 
     }
 
