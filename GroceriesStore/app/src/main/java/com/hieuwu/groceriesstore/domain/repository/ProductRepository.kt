6b1393992d3137cc1efae2229ae1c0566a506873
Hieu Vu <star.hieu.99@gmail.com>
2021-09-04 09:46:22 +0700

    Declare method to update line item

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
@@ -1,5 +1,6 @@
 package com.hieuwu.groceriesstore.domain.repository
 
+import com.hieuwu.groceriesstore.domain.entities.LineItem
 import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.entities.ProductAndLineItem
 import com.hieuwu.groceriesstore.domain.models.ProductModel
@@ -11,4 +12,5 @@ interface ProductRepository {
     suspend fun getFromServer()
     suspend fun hasProduct(): Boolean
     suspend fun getProductAndLineItem():Flow<List<ProductAndLineItem>>
+    suspend fun updateProductAndLineItem(lineItem: ProductAndLineItem)
 }
