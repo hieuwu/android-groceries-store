bb79b4b7169d5400b50b5bc6e9252bbbea1ae5dc
Hieu Vu <star.hieu.99@gmail.com>
2021-09-04 10:03:20 +0700

    Refactor entity

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/entities/ProductAndLineItem.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/entities/ProductAndLineItem.kt
@@ -4,7 +4,7 @@ import androidx.room.Embedded
 import androidx.room.Relation
 
 data class ProductAndLineItem(
-    @Embedded val product: Product,
+    @Embedded val product: Product?,
     @Relation(
         parentColumn = "productId",
         entityColumn = "productId"
