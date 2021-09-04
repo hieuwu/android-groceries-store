bb3424d08dc2b466daae4d636b7c2d4bb0f93e47
Hieu Vu <star.hieu.99@gmail.com>
2021-09-04 22:38:44 +0700

    Refactor factory for cart view model

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/CartViewModelFactory.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/CartViewModelFactory.kt
@@ -2,14 +2,17 @@ package com.hieuwu.groceriesstore.presentation.viewmodels.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
 import com.hieuwu.groceriesstore.presentation.viewmodels.CartViewModel
 
-class CartViewModelFactory(private val productRepository: ProductRepository) :
+class CartViewModelFactory(
+    private val productRepository: ProductRepository, private val orderRepository: OrderRepository
+) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CartViewModel::class.java)) {
-            return CartViewModel(productRepository) as T
+            return CartViewModel(productRepository, orderRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
