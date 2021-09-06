510e2def5d68f8de0c04ecaf7ea37999115d25e9
Hieu Vu <star.hieu.99@gmail.com>
2021-09-06 09:16:17 +0700

    Get methods

--- GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/OrderRepository.kt
+++ GroceriesStore/app/src/main/java/com/hieuwu/groceriesstore/domain/repository/OrderRepository.kt
@@ -1,9 +1,11 @@
 package com.hieuwu.groceriesstore.domain.repository
 
+import androidx.lifecycle.LiveData
 import com.hieuwu.groceriesstore.data.utils.OrderStatus
 import com.hieuwu.groceriesstore.domain.entities.LineItem
 import com.hieuwu.groceriesstore.domain.entities.Order
 import com.hieuwu.groceriesstore.domain.entities.OrderWithLineItems
+import com.hieuwu.groceriesstore.domain.entities.ProductAndLineItem
 import kotlinx.coroutines.flow.Flow
 
 interface OrderRepository {
@@ -11,6 +13,7 @@ interface OrderRepository {
     suspend fun insert(order: Order)
     suspend fun addLineItem(lineItem: LineItem)
     fun getOrderById(id: String): Flow<OrderWithLineItems>
-    fun getOrderInCart(status: OrderStatus): Flow<Order>
+    fun getOrderInCart(status: OrderStatus): Order
     fun getOrderWithLineItems(): List<OrderWithLineItems>
+    fun getCurrentCartId(status: OrderStatus): LiveData<String>
 }
