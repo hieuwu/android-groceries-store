971db965de9aa84df1c3df5a8e3977df43aaf5b9
Hieu Vu <star.hieu.99@gmail.com>
2021-09-09 10:10:54 +0700

    Implement view model factory

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/CheckOutViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/CheckOutViewModel.kt
@@ -1,4 +1,43 @@
 package com.hieuwu.groceriesstore.presentation.viewmodels
 
-class CheckOutViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.hieuwu.groceriesstore.data.utils.OrderStatus
+import com.hieuwu.groceriesstore.domain.entities.OrderWithLineItems
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
+import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+
+class CheckOutViewModel @Inject constructor(
+    private val orderId: String,
+    val orderRepository: OrderRepository
+) :
+    ObservableViewModel() {
+
+    private var _order = MutableLiveData<OrderWithLineItems>()
+    val order: LiveData<OrderWithLineItems>
+        get() = _order
+
+    init {
+        getLineItemFromDatabase()
+    }
+
+    private fun getLineItemFromDatabase() {
+        viewModelScope.launch {
+            getLineItemFromLocal()
+        }
+    }
+
+    private suspend fun getLineItemFromLocal() {
+        return withContext(Dispatchers.IO) {
+            _order = orderRepository.getCartWithLineItems(OrderStatus.IN_CART).asLiveData() as MutableLiveData<OrderWithLineItems>
+        }
+    }
+
 }
--- app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/CheckOutViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/viewmodels/factory/CheckOutViewModelFactory.kt
@@ -1,4 +1,18 @@
 package com.hieuwu.groceriesstore.presentation.viewmodels.factory
 
-class CheckOutViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
+import com.hieuwu.groceriesstore.presentation.viewmodels.CartViewModel
+import com.hieuwu.groceriesstore.presentation.viewmodels.CheckOutViewModel
+
+class CheckOutViewModelFactory(
+    private val orderId: String, private val orderRepository: OrderRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(CheckOutViewModel::class.java)) {
+            return CheckOutViewModel(orderId, orderRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
