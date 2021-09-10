9429fdf1452360571369c7f430ee37290c9ec8d8
Hieu Vu <star.hieu.99@gmail.com>
2021-09-10 16:22:47 +0700

    Implement swipe to delete

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/SwipeGestures.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/SwipeGestures.kt
@@ -1,4 +1,80 @@
 package com.hieuwu.groceriesstore.presentation.adapters
 
-class SwipeGestures {
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.hieuwu.groceriesstore.R
+
+class SwipeToDeleteCallback constructor(val adapter: LineListItemAdapter) :
+    ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+    private var _icon: Drawable? =
+        ContextCompat.getDrawable(adapter.context, R.drawable.ic_baseline_delete)
+    private var _background: ColorDrawable = ColorDrawable(Color.RED)
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        val position = viewHolder.adapterPosition;
+        adapter.deleteItemAt(position);
+    }
+
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+        val itemView = viewHolder.itemView;
+        val backgroundCornerOffset = 20;
+        val iconMargin = (itemView.height - _icon?.intrinsicHeight!!) / 2
+        val iconTop = itemView.top + (itemView.height - _icon?.intrinsicHeight!!) / 2
+        val iconBottom = iconTop + _icon?.intrinsicHeight!!
+
+        when {
+            //Swipe to the right
+            dX > 0 -> {
+                val iconLeft = itemView.left + iconMargin + _icon?.intrinsicWidth!!
+                val iconRight = itemView.left + iconMargin
+                _icon!!.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+                _background.setBounds(
+                    itemView.left, itemView.top,
+                    itemView.left + dX.toInt() + backgroundCornerOffset,
+                    itemView.bottom
+                )
+
+            }
+            // Swiping to the left
+            dX < 0 -> {
+                val iconLeft = itemView.right - iconMargin - _icon?.intrinsicWidth!!
+                val iconRight = itemView.right - iconMargin
+                _icon?.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+                _background.setBounds(
+                    itemView.right + dX.toInt() - backgroundCornerOffset,
+                    itemView.top, itemView.right, itemView.bottom
+                )
+            }
+            else -> {
+                _background.setBounds(0, 0, 0, 0)
+            }
+        }
+        _background.draw(c)
+        _icon?.draw(c)
+    }
 }
