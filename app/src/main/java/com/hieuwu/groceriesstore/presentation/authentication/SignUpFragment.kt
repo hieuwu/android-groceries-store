ffdf3c109c12b5a10e514ad2e75332aa22d0fd4e
Hieu Vu <star.hieu.99@gmail.com>
2021-09-10 14:27:13 +0700

    Update directory for each screen

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignUpFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignUpFragment.kt
@@ -1,4 +1,4 @@
-package com.hieuwu.groceriesstore.presentation.fragments
+package com.hieuwu.groceriesstore.presentation.authentication
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,9 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.databinding.FragmentSignUpBinding
-import com.hieuwu.groceriesstore.presentation.viewmodels.SignInViewModel
-import com.hieuwu.groceriesstore.presentation.viewmodels.SignUpViewModel
-import com.hieuwu.groceriesstore.presentation.viewmodels.factory.ViewModelFactory
 
 class SignUpFragment : Fragment() {
 
