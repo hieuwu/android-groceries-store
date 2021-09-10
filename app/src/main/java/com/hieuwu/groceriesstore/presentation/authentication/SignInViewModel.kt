dab2eb723ccb3dcc59dd87ae1e4ab97b387a62aa
Hieu Vu <star.hieu.99@gmail.com>
2021-09-10 14:27:17 +0700

    Update directory for each screen

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInViewModel.kt
@@ -1,4 +1,4 @@
-package com.hieuwu.groceriesstore.presentation.viewmodels
+package com.hieuwu.groceriesstore.presentation.authentication
 
 import androidx.databinding.Bindable
 import com.hieuwu.groceriesstore.BR
