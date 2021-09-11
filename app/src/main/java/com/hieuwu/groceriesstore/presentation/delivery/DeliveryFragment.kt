32a9599195fca6ed66a161c8cd9c729235140250
Hieu Vu <star.hieu.99@gmail.com>
2021-09-11 16:45:38 +0700

    Remove unused params in fragment

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/delivery/DeliveryFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/delivery/DeliveryFragment.kt
@@ -5,56 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.hieuwu.groceriesstore.R
+import com.hieuwu.groceriesstore.databinding.FragmentCheckOutBinding
+import com.hieuwu.groceriesstore.databinding.FragmentDeliveryBinding
+import com.hieuwu.groceriesstore.presentation.shop.ShopFragmentDirections
+import com.hieuwu.groceriesstore.presentation.utils.KeyData
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DeliveryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DeliveryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentDeliveryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_delivery, container, false)
+
+        binding = DataBindingUtil.inflate<FragmentDeliveryBinding>(
+            inflater,
+            R.layout.fragment_delivery,
+            container,
+            false
+        )
+        binding.lifecycleOwner = this
+
+        binding.updateDeliveryBtn.setOnClickListener {
+            parentFragmentManager.setFragmentResult(
+                KeyData.RESULT_KEY, bundleOf(
+                    KeyData.NAME_KEY to binding.receiverName.text.toString(),
+                    KeyData.PHONE_KEY to binding.receiverPhone.text.toString(),
+                    KeyData.STREET_KEY to binding.addressStreet.text.toString(),
+                    KeyData.WARD_KEY to binding.addressWard.text.toString(),
+                    KeyData.CITY_KEY to binding.addressCity.text.toString(),
+                )
+            )
+            findNavController().popBackStack()
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DeliveryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DeliveryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
