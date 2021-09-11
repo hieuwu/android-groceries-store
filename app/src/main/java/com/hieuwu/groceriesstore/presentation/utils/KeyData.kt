e9488a5c6869fdcc343725d9900e1b0f45905d33
Hieu Vu <star.hieu.99@gmail.com>
2021-09-11 16:45:52 +0700

    Declare constant for key data

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/utils/KeyData.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/utils/KeyData.kt
@@ -1,4 +1,10 @@
 package com.hieuwu.groceriesstore.presentation.utils
 
 object KeyData {
+    const val RESULT_KEY = "CHECKOUT_TO_DELIVERY_UPDATE"
+    const val NAME_KEY = "Name"
+    const val PHONE_KEY = "Phone"
+    const val STREET_KEY = "Street"
+    const val WARD_KEY = "Ward"
+    const val CITY_KEY = "City"
 }
