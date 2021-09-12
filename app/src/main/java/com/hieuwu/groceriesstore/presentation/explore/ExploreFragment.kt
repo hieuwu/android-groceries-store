e2cbee72e06b016b88025e5f004bb74d14f76ec4
Hieu Vu <star.hieu.99@gmail.com>
2021-09-12 09:42:47 +0700

    Bind viewmodel

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.databinding.FragmentExploreBinding
+import com.hieuwu.groceriesstore.domain.entities.Category
+import com.hieuwu.groceriesstore.presentation.adapters.CategoryItemAdapter
 import com.hieuwu.groceriesstore.presentation.adapters.GridListItemAdapter
 import timber.log.Timber
 
@@ -26,15 +29,16 @@ class ExploreFragment : Fragment() {
         binding = DataBindingUtil.inflate<FragmentExploreBinding>(
             inflater, R.layout.fragment_explore, container, false
         )
-        setUpRecyclerView()
+        val viewModel = ViewModelProvider(this).get(ExploreViewModel::class.java)
 
+        binding.lifecycleOwner = this
+
+        viewModel.categories.observe(viewLifecycleOwner, {
+        })
+
+        binding.categoryRecyclerview.adapter =
+            CategoryItemAdapter(CategoryItemAdapter.OnClickListener {})
         return binding.root
     }
 
-    private fun setUpRecyclerView() {
-        binding.productRecyclerview.adapter =
-            GridListItemAdapter(GridListItemAdapter.OnClickListener {
-                Timber.d("on item clicked")
-            })
-    }
 }
