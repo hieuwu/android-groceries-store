de246e9dd656c5497dd885eebafc5a3f3e839448
Hieu Vu <star.hieu.99@gmail.com>
2021-09-12 09:43:27 +0700

    Create binding adapter

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/BindingAdapters.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/BindingAdapters.kt
@@ -7,8 +7,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.hieuwu.groceriesstore.R
+import com.hieuwu.groceriesstore.domain.entities.Category
 import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.entities.ProductAndLineItem
+import com.hieuwu.groceriesstore.presentation.adapters.CategoryItemAdapter
 import com.hieuwu.groceriesstore.presentation.adapters.GridListItemAdapter
 import com.hieuwu.groceriesstore.presentation.adapters.LineListItemAdapter
 
@@ -36,5 +38,12 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
 @BindingAdapter("lineListData")
 fun bindRecyclerViewLine(recyclerView: RecyclerView, data: MutableList<ProductAndLineItem>?) {
     val adapter = recyclerView.adapter as LineListItemAdapter
+    adapter.submitList(data)
+}
+
+
+@BindingAdapter("categoryListData")
+fun bindRecyclerViewCategory(recyclerView: RecyclerView, data: MutableList<Category>?) {
+    val adapter = recyclerView.adapter as CategoryItemAdapter
     adapter.submitList(data)
 }
