90710cb0a46d67ef093e5ac360548f5aa87b5fc9
Hieu Vu <star.hieu.99@gmail.com>
2021-09-12 09:43:04 +0700

    Mock data to display category list

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModel.kt
@@ -1,4 +1,24 @@
 package com.hieuwu.groceriesstore.presentation.explore
 
-class ExploreViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.hieuwu.groceriesstore.domain.entities.Category
+import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import javax.inject.Inject
+
+class ExploreViewModel @Inject constructor() : ObservableViewModel() {
+    private var _categories = MutableLiveData<List<Category>>()
+    val categories: MutableLiveData<List<Category>>
+        get() = _categories
+
+    init {
+        val listOfCategories = mutableListOf<Category>()
+        listOfCategories.add(Category("1", "Rice", "213"))
+        listOfCategories.add(Category("2", "Fish", "213"))
+        listOfCategories.add(Category("3", "Oil", "213"))
+        listOfCategories.add(Category("4", "Fruit", "213"))
+        categories.value = listOfCategories
+        _categories.value = listOfCategories
+
+    }
 }
