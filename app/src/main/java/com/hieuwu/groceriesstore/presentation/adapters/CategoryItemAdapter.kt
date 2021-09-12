a10be827f4e37736226ba5a47d12b54c15c4395b
Hieu Vu <star.hieu.99@gmail.com>
2021-09-12 09:43:42 +0700

    Implement adapter for category item

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/CategoryItemAdapter.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/CategoryItemAdapter.kt
@@ -1,4 +1,55 @@
 package com.hieuwu.groceriesstore.presentation.adapters
 
-class CategoryItemAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.hieuwu.groceriesstore.databinding.LayoutCategoryGridItemBinding
+import com.hieuwu.groceriesstore.domain.entities.Category
+
+class CategoryItemAdapter(val onClickListener: OnClickListener) :
+    ListAdapter<Category, CategoryItemAdapter.CategoryItemViewHolder>(DiffCallback) {
+
+    class CategoryItemViewHolder(private var binding: LayoutCategoryGridItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(category: Category) {
+            binding.category = category
+            binding.executePendingBindings()
+        }
+
+    }
+
+    override fun onBindViewHolder(holder: CategoryItemViewHolder, position: Int) {
+        val category = getItem(position)
+        holder.itemView.setOnClickListener {
+            onClickListener.onClick(category)
+        }
+        holder.bind(category)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryItemViewHolder {
+        return CategoryItemViewHolder(
+            LayoutCategoryGridItemBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                )
+            )
+        )
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Category>() {
+        override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+    class OnClickListener(val clickListener: (category: Category) -> Unit) {
+        fun onClick(category: Category) = clickListener(category)
+    }
 }
