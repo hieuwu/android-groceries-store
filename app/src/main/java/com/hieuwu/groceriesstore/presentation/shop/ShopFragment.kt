aed1007c6ffa774174c3c2427be56e0211b7d6bb
Hieu Vu <star.hieu.99@gmail.com>
2021-09-13 15:44:21 +0700

    Remove unused code, handle display snack bar

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/ShopFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/ShopFragment.kt
@@ -9,18 +9,23 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.databinding.FragmentShopBinding
 import com.hieuwu.groceriesstore.di.ProductRepo
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
 import com.hieuwu.groceriesstore.presentation.adapters.GridListItemAdapter
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class ShopFragment : Fragment() {
     private lateinit var binding: FragmentShopBinding
+
+    @Inject
+    lateinit var orderRepository: OrderRepository
+
 
     @ProductRepo
     @Inject
@@ -30,30 +35,25 @@ class ShopFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
         binding = DataBindingUtil.inflate<FragmentShopBinding>(
             inflater, R.layout.fragment_shop, container, false
         )
 
-        val viewModelFactory = ShopViewModelFactory(productRepository)
+        val viewModelFactory = ShopViewModelFactory(productRepository, orderRepository)
         val viewModel = ViewModelProvider(this, viewModelFactory)
             .get(ShopViewModel::class.java)
+
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
-        setUpRecyclerView()
-        binding.exclusiveOfferRecyclerview.adapter =
-            GridListItemAdapter(GridListItemAdapter.OnClickListener {
-                viewModel.displayPropertyDetails(it)
-            })
+        setUpRecyclerView(viewModel)
 
         viewModel.navigateToSelectedProperty.observe(this.viewLifecycleOwner, {
 
             if (null != it) {
                 val direction = ShopFragmentDirections.actionShopFragmentToProductDetailFragment(
-                        it.id
-                    )
+                    it.id
+                )
                 findNavController().navigate(direction)
                 viewModel.displayPropertyDetailsComplete()
             }
@@ -63,24 +63,55 @@ class ShopFragment : Fragment() {
         return binding.root
     }
 
-    private fun setUpRecyclerView() {
+    private fun showSnackBar(productName: String?) {
+        Snackbar.make(
+            requireActivity().findViewById(android.R.id.content),
+            "Added $productName",
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
 
+    private fun setUpRecyclerView(viewModel: ShopViewModel) {
+        binding.exclusiveOfferRecyclerview.adapter =
+            GridListItemAdapter(
+                GridListItemAdapter.OnClickListener(
+                    clickListener = {
+                        viewModel.displayPropertyDetails(it)
+                    },
+                    addToCartListener = {
+                        viewModel.addToCart(it)
+                        showSnackBar(it.name)
+                    },
+                )
+            )
 
         binding.exclusiveOfferRecyclerview.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
         binding.bestSellingRecyclerview.adapter =
-            GridListItemAdapter(GridListItemAdapter.OnClickListener {
-                Timber.d("on item clicked")
-            })
+            GridListItemAdapter(
+                GridListItemAdapter.OnClickListener(
+                    clickListener = { viewModel.displayPropertyDetails(it) },
+                    addToCartListener = {
+                        viewModel.addToCart(it)
+                        showSnackBar(it.name)
+                    },
+                )
+            )
 
         binding.bestSellingRecyclerview.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
         binding.recommendedRecyclerview.adapter =
-            GridListItemAdapter(GridListItemAdapter.OnClickListener {
-                Timber.d("on item clicked")
-            })
+            GridListItemAdapter(
+                GridListItemAdapter.OnClickListener(
+                    clickListener = { viewModel.displayPropertyDetails(it) },
+                    addToCartListener = {
+                        viewModel.addToCart(it)
+                        showSnackBar(it.name)
+                    },
+                )
+            )
 
         binding.recommendedRecyclerview.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
