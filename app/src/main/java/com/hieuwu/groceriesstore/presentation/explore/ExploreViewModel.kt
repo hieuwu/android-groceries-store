2476e41ebb6b00125ac5b3fc6712577ca67b5d19
Hieu Vu <star.hieu.99@gmail.com>
2021-09-13 21:38:11 +0700

    Inject repo to viewmodel

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModel.kt
@@ -3,15 +3,19 @@ package com.hieuwu.groceriesstore.presentation.explore
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.hieuwu.groceriesstore.domain.entities.Category
+import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
 import javax.inject.Inject
 
-class ExploreViewModel @Inject constructor() : ObservableViewModel() {
+class ExploreViewModel @Inject constructor(private val categoryRepository: CategoryRepository) :
+    ObservableViewModel() {
     private var _categories = MutableLiveData<List<Category>>()
     val categories: MutableLiveData<List<Category>>
         get() = _categories
 
     init {
+        categoryRepository.getFromServer()
+
         val listOfCategories = mutableListOf<Category>()
         listOfCategories.add(Category("1", "Rice", "213"))
         listOfCategories.add(Category("2", "Fish", "213"))
