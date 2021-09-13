00860521835bf6aeeab860d5eae11b209a4c4153
Hieu Vu <star.hieu.99@gmail.com>
2021-09-13 21:40:25 +0700

    Declare upsert and getall

--- app/src/main/java/com/hieuwu/groceriesstore/data/dao/CategoryDao.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/dao/CategoryDao.kt
@@ -1,4 +1,19 @@
 package com.hieuwu.groceriesstore.data.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.hieuwu.groceriesstore.data.utils.DataConstant
+import com.hieuwu.groceriesstore.domain.entities.Category
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface CategoryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(category: Category)
+
+    @Query("SELECT * FROM `${DataConstant.CATEGORY_TABLE}`")
+    fun getAll(): Flow<List<Category>>
 }
+
