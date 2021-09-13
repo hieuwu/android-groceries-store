fc696b69ef141652c666f770feab400fea65989c
Hieu Vu <star.hieu.99@gmail.com>
2021-09-13 21:39:44 +0700

    Add category to entities

--- app/src/main/java/com/hieuwu/groceriesstore/data/GroceriesStoreDatabase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/GroceriesStoreDatabase.kt
@@ -2,15 +2,17 @@ package com.hieuwu.groceriesstore.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.hieuwu.groceriesstore.data.dao.CategoryDao
 import com.hieuwu.groceriesstore.data.dao.LineItemDao
 import com.hieuwu.groceriesstore.data.dao.OrderDao
 import com.hieuwu.groceriesstore.data.dao.ProductDao
+import com.hieuwu.groceriesstore.domain.entities.Category
 import com.hieuwu.groceriesstore.domain.entities.LineItem
 import com.hieuwu.groceriesstore.domain.entities.Order
 import com.hieuwu.groceriesstore.domain.entities.Product
 
 @Database(
-    entities = [Product::class, LineItem::class, Order::class],
+    entities = [Product::class, LineItem::class, Order::class, Category::class],
     version = 1,
     exportSchema = false
 )
@@ -18,4 +20,5 @@ abstract class GroceriesStoreDatabase : RoomDatabase() {
     abstract fun productDao(): ProductDao
     abstract fun lineItemDao(): LineItemDao
     abstract fun orderDao(): OrderDao
+    abstract fun categoryDao(): CategoryDao
 }
