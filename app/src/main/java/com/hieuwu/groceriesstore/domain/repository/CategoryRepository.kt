56607971d71fe16553b8c3e3839499dc4bd506dd
Hieu Vu <star.hieu.99@gmail.com>
2021-09-13 21:38:38 +0700

    Declare methods in repository

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/CategoryRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/CategoryRepository.kt
@@ -1,4 +1,9 @@
 package com.hieuwu.groceriesstore.domain.repository
 
+import com.hieuwu.groceriesstore.domain.entities.Category
+import kotlinx.coroutines.flow.Flow
+
 interface CategoryRepository {
+    fun getFromServer()
+    fun getFromLocal(): Flow<List<Category>>
 }
