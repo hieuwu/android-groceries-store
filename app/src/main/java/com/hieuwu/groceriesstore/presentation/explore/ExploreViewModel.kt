4280db1762e4b13ef810fe8eeac2f4111d852f71
Hieu Vu <star.hieu.99@gmail.com>
2021-09-14 09:40:46 +0700

    Get data from db for category

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModel.kt
@@ -2,6 +2,7 @@ package com.hieuwu.groceriesstore.presentation.explore
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
 import com.hieuwu.groceriesstore.domain.entities.Category
 import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
@@ -9,20 +10,13 @@ import javax.inject.Inject
 
 class ExploreViewModel @Inject constructor(private val categoryRepository: CategoryRepository) :
     ObservableViewModel() {
-    private var _categories = MutableLiveData<List<Category>>()
+    private var _categories: MutableLiveData<List<Category>> =
+        categoryRepository.getFromLocal().asLiveData() as MutableLiveData<List<Category>>
+
     val categories: MutableLiveData<List<Category>>
         get() = _categories
 
     init {
         categoryRepository.getFromServer()
-
-        val listOfCategories = mutableListOf<Category>()
-        listOfCategories.add(Category("1", "Rice", "213"))
-        listOfCategories.add(Category("2", "Fish", "213"))
-        listOfCategories.add(Category("3", "Oil", "213"))
-        listOfCategories.add(Category("4", "Fruit", "213"))
-        categories.value = listOfCategories
-        _categories.value = listOfCategories
-
     }
 }
