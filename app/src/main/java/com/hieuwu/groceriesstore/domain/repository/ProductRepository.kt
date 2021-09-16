60970851ea1469b391bb798e60d22c2f8e5501ef
Hieu Vu <star.hieu.99@gmail.com>
2021-09-16 20:36:15 +0700

    Optimize imports

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
@@ -1,11 +1,8 @@
 package com.hieuwu.groceriesstore.domain.repository
 
-import com.hieuwu.groceriesstore.data.utils.OrderStatus
 import com.hieuwu.groceriesstore.domain.entities.LineItem
-import com.hieuwu.groceriesstore.domain.entities.Order
 import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.entities.ProductAndLineItem
-import com.hieuwu.groceriesstore.domain.models.ProductModel
 import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
