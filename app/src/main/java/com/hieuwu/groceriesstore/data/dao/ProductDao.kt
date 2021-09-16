564dfc56564d0cdc39c624e873ee5d5ce5705963
Hieu Vu <star.hieu.99@gmail.com>
2021-09-16 20:36:10 +0700

    Optimize imports

--- app/src/main/java/com/hieuwu/groceriesstore/data/dao/ProductDao.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/dao/ProductDao.kt
@@ -2,9 +2,7 @@ package com.hieuwu.groceriesstore.data.dao
 
 import androidx.room.*
 import com.hieuwu.groceriesstore.data.utils.DataConstant
-import com.hieuwu.groceriesstore.domain.entities.LineItem
 import com.hieuwu.groceriesstore.domain.entities.Product
-import com.hieuwu.groceriesstore.domain.entities.ProductAndLineItem
 import kotlinx.coroutines.flow.Flow
 
 @Dao
