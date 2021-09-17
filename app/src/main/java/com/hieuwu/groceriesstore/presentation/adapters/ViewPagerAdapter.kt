4b88f5b64a4d9b80fe2ffcd267827524bb2c0db2
Hieu Vu <star.hieu.99@gmail.com>
2021-09-17 11:38:02 +0700

    Implement adapter for image slide

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/ViewPagerAdapter.kt
@@ -1,4 +1,39 @@
 package com.hieuwu.groceriesstore.presentation.adapters
 
-class ViewPagerAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.viewpager.widget.PagerAdapter
+import com.hieuwu.groceriesstore.R
+
+class ViewPagerAdapter(val context: Context) : PagerAdapter() {
+
+    private val images = arrayOf(
+        R.drawable.banner,
+        R.drawable.banner,
+        R.drawable.banner
+    )
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val layoutInflater =
+            context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = layoutInflater.inflate(R.layout.layout_image_layout, null);
+        val imageView = view.findViewById<ImageView>(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+        container.addView(view, 0);
+        return view
+    }
+
+    override fun getCount() = images.size
+
+
+    override fun isViewFromObject(view: View, `object`: Any) = view == `object`
+
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
 }
