35ccf0bbf77d3c81e0c8a0c8897fb412dcc9534b
Hieu Vu <star.hieu.99@gmail.com>
2021-09-21 21:29:45 +0700

    Call table name from global util

--- app/src/main/java/com/hieuwu/groceriesstore/data/dao/OrderDao.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/dao/OrderDao.kt
@@ -2,9 +2,9 @@ package com.hieuwu.groceriesstore.data.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.hieuwu.groceriesstore.data.utils.DataConstant
 import com.hieuwu.groceriesstore.domain.entities.Order
 import com.hieuwu.groceriesstore.domain.entities.OrderWithLineItems
+import com.hieuwu.groceriesstore.utilities.ORDER_TABLE
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -16,24 +16,24 @@ interface OrderDao {
     fun update(order: Order)
 
     @Transaction
-    @Query("SELECT * FROM `${DataConstant.ORDER_TABLE}`")
+    @Query("SELECT * FROM `$ORDER_TABLE`")
     fun getOrderWithLineItems(): Flow<List<OrderWithLineItems>>
 
     @Transaction
-    @Query("SELECT * FROM `${DataConstant.ORDER_TABLE}` WHERE orderId = :id")
+    @Query("SELECT * FROM `$ORDER_TABLE` WHERE orderId = :id")
     fun getById(id: String): OrderWithLineItems
 
-    @Query("SELECT EXISTS(SELECT 1 FROM `${DataConstant.ORDER_TABLE}` WHERE status = :status LIMIT 1)")
+    @Query("SELECT EXISTS(SELECT 1 FROM `${ORDER_TABLE}` WHERE status = :status LIMIT 1)")
     fun isCartExisted(status: String): Flow<Boolean>
 
-    @Query("SELECT * FROM `${DataConstant.ORDER_TABLE}` WHERE status = :status LIMIT 1")
+    @Query("SELECT * FROM `${ORDER_TABLE}` WHERE status = :status LIMIT 1")
     fun getOrderInCart(status: String): Flow<Order>
 
-    @Query("SELECT orderId FROM `${DataConstant.ORDER_TABLE}` WHERE status = :status LIMIT 1")
+    @Query("SELECT orderId FROM `${ORDER_TABLE}` WHERE status = :status LIMIT 1")
     fun getCurrentCartId(status: String): LiveData<String>
 
 
-    @Query("SELECT * FROM `${DataConstant.ORDER_TABLE}` WHERE status = :status  LIMIT 1 ")
+    @Query("SELECT * FROM `${ORDER_TABLE}` WHERE status = :status  LIMIT 1 ")
     fun getCart(status: String): Flow<Order>
 
     @Transaction
