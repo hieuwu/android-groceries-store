5039cdc12c5ea03df8cb536e92e9d81d6ece4df9
Hieu Vu <star.hieu.99@gmail.com>
2021-09-21 21:29:42 +0700

    Call table name from global util

--- app/src/main/java/com/hieuwu/groceriesstore/data/dao/ProductDao.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/dao/ProductDao.kt
@@ -1,8 +1,8 @@
 package com.hieuwu.groceriesstore.data.dao
 
 import androidx.room.*
-import com.hieuwu.groceriesstore.data.utils.DataConstant
 import com.hieuwu.groceriesstore.domain.entities.Product
+import com.hieuwu.groceriesstore.utilities.PRODUCT_TABLE
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,18 +13,18 @@ interface ProductDao {
     @Update
     fun update(product: Product)
 
-    @Query("SELECT * FROM ${DataConstant.PRODUCT_TABLE} WHERE productId = :id")
+    @Query("SELECT * FROM $PRODUCT_TABLE WHERE productId = :id")
     fun getById(id: String): Flow<Product>
 
-    @Query("DELETE FROM ${DataConstant.PRODUCT_TABLE}")
+    @Query("DELETE FROM $PRODUCT_TABLE")
     fun clear()
 
-    @Query("SELECT * FROM ${DataConstant.PRODUCT_TABLE} LIMIT 10")
+    @Query("SELECT * FROM $PRODUCT_TABLE LIMIT 10")
     fun getAll(): Flow<List<Product>>
 
-    @Query("SELECT * FROM ${DataConstant.PRODUCT_TABLE} Where category =:categoryId LIMIT 20")
+    @Query("SELECT * FROM $PRODUCT_TABLE Where category =:categoryId LIMIT 20")
     fun getAllByCategory(categoryId: String): Flow<List<Product>>
 
-    @Query("SELECT * FROM ${DataConstant.PRODUCT_TABLE} LIMIT 1")
+    @Query("SELECT * FROM $PRODUCT_TABLE LIMIT 1")
     fun hasProduct(): Product?
 }
