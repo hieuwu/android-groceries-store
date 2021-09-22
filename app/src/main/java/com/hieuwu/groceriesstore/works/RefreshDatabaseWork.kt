b8151768901aaf600e0fd8b45c1bdce115597636
Hieu Vu <star.hieu.99@gmail.com>
2021-09-22 21:18:24 +0700

    Implement refresh data work

--- app/src/main/java/com/hieuwu/groceriesstore/works/RefreshDatabaseWork.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/works/RefreshDatabaseWork.kt
@@ -1,4 +1,23 @@
 package com.hieuwu.groceriesstore.works
 
-class RefreshDatabaseWork {
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.hieuwu.groceriesstore.di.ProductRepo
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import javax.inject.Inject
+
+class RefreshDatabaseWork(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+    @ProductRepo
+    @Inject
+    lateinit var productRepository: ProductRepository
+    override suspend fun doWork(): Result {
+        return try {
+            Result.success()
+        } catch (e: Exception) {
+            Result.retry()
+        }
+    }
+
 }
