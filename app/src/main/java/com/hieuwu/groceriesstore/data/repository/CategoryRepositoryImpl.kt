0283e65b07b8baa335d6637e05a7970aad67e795
Hieu Vu <star.hieu.99@gmail.com>
2021-09-25 15:10:34 +0700

    Refactor get from server and insert all data

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/CategoryRepositoryImpl.kt
@@ -1,10 +1,14 @@
 package com.hieuwu.groceriesstore.data.repository
 
+import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.hieuwu.groceriesstore.data.dao.CategoryDao
 import com.hieuwu.groceriesstore.domain.entities.Category
+import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -14,23 +18,28 @@ import javax.inject.Singleton
 @Singleton
 class CategoryRepositoryImpl @Inject constructor(private val categoryDao: CategoryDao) :
     CategoryRepository {
-    private val executorService: ExecutorService = Executors.newFixedThreadPool(4)
 
-    override fun getFromServer() {
+    override suspend fun getFromServer() {
+        val categoriesList = mutableListOf<Category>();
         val fireStore = Firebase.firestore
         fireStore.collection("categories").get().addOnSuccessListener { result ->
             for (document in result) {
-                val id = document.id
-                val name: String = document.data["name"] as String
-                val image: String = document.data["image"] as String
-                executorService.execute {
-                    categoryDao.insert(Category(id, name, image))
-                }
+                categoriesList.add(getCategoryEntityFromDocument(document))
             }
         }
             .addOnFailureListener { exception ->
                 Timber.w("Error getting documents.${exception}")
             }
+        withContext(Dispatchers.IO) {
+            categoryDao.insertAll(categoriesList)
+        }
+    }
+
+    private fun getCategoryEntityFromDocument(document: QueryDocumentSnapshot): Category {
+        val id = document.id
+        val name: String = document.data["name"] as String
+        val image: String = document.data["image"] as String
+        return Category(id, name, image)
     }
 
     override fun getFromLocal() = categoryDao.getAll()
