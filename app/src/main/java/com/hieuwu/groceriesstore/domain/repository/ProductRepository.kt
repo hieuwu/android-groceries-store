0f0b1b88f16a59b2198e8e851f407583b09e3217
Hieu Vu <star.hieu.99@gmail.com>
2021-09-25 14:51:31 +0700

    Refactor method call in repository

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
@@ -6,13 +6,11 @@ import com.hieuwu.groceriesstore.domain.entities.ProductAndLineItem
 import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
-    fun getById(id: String): Flow<Product>
-    suspend fun getAllProducts(): Flow<List<Product>>
+    fun getProductById(productId: String): Flow<Product>
+    fun getAllProducts(): Flow<List<Product>>
     suspend fun getAllProductsByCategory(categoryId: String): Flow<List<Product>>
     suspend fun getFromServer()
     suspend fun hasProduct(): Boolean
-    suspend fun updateProductAndLineItem(lineItem: ProductAndLineItem)
-    suspend fun removeProductAndLineItem(lineItem: LineItem)
-    suspend fun getAllLineItem(): Flow<List<ProductAndLineItem>>
-    suspend fun getLineItemInOrder(orderId:String): Flow<List<ProductAndLineItem>>
+    suspend fun updateLineItem(lineItem: LineItem)
+    suspend fun removeLineItem(lineItem: LineItem)
 }
