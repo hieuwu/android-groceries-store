2d5dc4bb7b051cd4e9146bf1d59548ca60de05ac
Hieu Vu <star.hieu.99@gmail.com>
2021-09-26 20:47:23 +0700

    Change return type

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
@@ -13,5 +13,5 @@ interface ProductRepository {
     suspend fun hasProduct(): Boolean
     suspend fun updateLineItem(lineItem: LineItem)
     suspend fun removeLineItem(lineItem: LineItem)
-    suspend fun searchProductsListByName(name: String) : Flow<List<Product>>
+    suspend fun searchProductsListByName(name: String) : List<Product>
 }
