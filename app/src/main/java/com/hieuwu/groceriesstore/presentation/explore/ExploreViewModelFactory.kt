cab728b12e8dfd50d560bb9f3b16024d31437890
Hieu Vu <star.hieu.99@gmail.com>
2021-09-26 13:42:15 +0700

    Update view model factory with product repo

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModelFactory.kt
@@ -3,13 +3,15 @@ package com.hieuwu.groceriesstore.presentation.explore
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
 
 class ExploreViewModelFactory(
-    private val categoryRepository: CategoryRepository
-) : ViewModelProvider.Factory {
+    private val categoryRepository: CategoryRepository,
+    private val productRepo: ProductRepository,
+    ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ExploreViewModel::class.java)) {
-            return ExploreViewModel(categoryRepository) as T
+            return ExploreViewModel(categoryRepository, productRepo) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
