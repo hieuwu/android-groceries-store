2d54a142e9e4c8c5856bab64abaed5f1e9cba03c
Hieu Vu <star.hieu.99@gmail.com>
2021-09-26 13:41:53 +0700

    Add product list for search result

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModel.kt
@@ -1,23 +1,34 @@
 package com.hieuwu.groceriesstore.presentation.explore
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.hieuwu.groceriesstore.domain.entities.Category
+import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class ExploreViewModel @Inject constructor(private val categoryRepository: CategoryRepository) :
+class ExploreViewModel @Inject constructor(private val categoryRepository: CategoryRepository,
+                                           private val productRepository: ProductRepository) :
     ObservableViewModel() {
     private var _categories: MutableLiveData<List<Category>> =
         categoryRepository.getFromLocal().asLiveData() as MutableLiveData<List<Category>>
 
     val categories: MutableLiveData<List<Category>>
         get() = _categories
+
+    private var _productList:MutableLiveData<List<Product>> = productRepository.getAllProducts().asLiveData()
+            as MutableLiveData<List<Product>>
+
+    val productList: LiveData<List<Product>>
+        get() = _productList
+
 
     init {
         viewModelScope.launch {
