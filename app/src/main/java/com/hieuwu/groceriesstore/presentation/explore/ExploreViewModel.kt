680e26a79d5300de34492547039a784e76a45824
Hieu Vu <star.hieu.99@gmail.com>
2021-09-29 21:47:18 +0700

    Mock view detail and add to cart method

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModel.kt
@@ -2,11 +2,15 @@ package com.hieuwu.groceriesstore.presentation.explore
 
 import androidx.lifecycle.*
 import com.hieuwu.groceriesstore.domain.entities.Category
+import com.hieuwu.groceriesstore.domain.entities.LineItem
+import com.hieuwu.groceriesstore.domain.entities.Order
 import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import com.hieuwu.groceriesstore.utilities.OrderStatus
 import kotlinx.coroutines.*
+import java.util.*
 import javax.inject.Inject
 
 class ExploreViewModel @Inject constructor(
@@ -42,6 +46,31 @@ class ExploreViewModel @Inject constructor(
         withContext(Dispatchers.IO) {
             categoryRepository.getFromServer()
         }
+    }
+    fun displayPropertyDetails(marsProperty: Product) {
+    }
+
+    fun addToCart(product: Product) {
+//        if (CurrentCart.value != null) {
+//            //Add to cart
+//            val cartId = CurrentCart.value!!.id
+//            uiScope.launch {
+//                val lineItem = LineItem(
+//                    product.id, cartId, 1, product.price!!
+//                )
+//                orderRepository.addLineItem(lineItem)
+//            }
+//        } else {
+//            val id = UUID.randomUUID().toString()
+//            val newOrder = Order(id, OrderStatus.IN_CART.value, null)
+//            uiScope.launch {
+//                orderRepository.insert(newOrder)
+//                val lineItem = LineItem(
+//                    product.id, id, 1, product.price!!
+//                )
+//                orderRepository.addLineItem(lineItem)
+//            }
+//        }
     }
 
 }
