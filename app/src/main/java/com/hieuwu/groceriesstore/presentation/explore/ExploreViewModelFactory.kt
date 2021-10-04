1448e26245044b7172671e03515f9f04deea8488
Hieu Vu <star.hieu.99@gmail.com>
2021-10-04 20:24:14 +0700

    Refactor view model factory with order repository

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/explore/ExploreViewModelFactory.kt
@@ -3,15 +3,17 @@ package com.hieuwu.groceriesstore.presentation.explore
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
 
 class ExploreViewModelFactory(
-    private val categoryRepository: CategoryRepository,
+    private val categoryRepo: CategoryRepository,
     private val productRepo: ProductRepository,
-    ) : ViewModelProvider.Factory {
+    private val orderRepo: OrderRepository
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ExploreViewModel::class.java)) {
-            return ExploreViewModel(categoryRepository, productRepo) as T
+            return ExploreViewModel(categoryRepo, productRepo, orderRepo) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
