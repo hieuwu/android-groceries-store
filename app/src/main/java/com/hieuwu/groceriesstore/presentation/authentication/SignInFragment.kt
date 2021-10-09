5a258175eb7f9b33af5c96a9d932521c99814d8a
Hieu Vu <star.hieu.99@gmail.com>
2021-10-09 20:35:25 +0700

    Add mock method to create account and go back after login

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInFragment.kt
@@ -5,24 +5,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.hieuwu.groceriesstore.MainActivity
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.databinding.FragmentSigninBinding
+import timber.log.Timber
 
 
 class SignInFragment : Fragment() {
 
     private lateinit var binding: FragmentSigninBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         binding = DataBindingUtil.inflate<FragmentSigninBinding>(
             inflater, R.layout.fragment_signin, container, false
         )
@@ -41,11 +46,27 @@ class SignInFragment : Fragment() {
         }
 
         binding.signinButton.setOnClickListener{
-            var i = Intent(this.context, MainActivity::class.java)
-            startActivity(i)
+            //If sign in successful, go back
+//            createAccount(signInViewModel.email, signInViewModel.password)
+//            val i = Intent(this.context, MainActivity::class.java)
+//            startActivity(i)
         }
 
         return binding.root
     }
+//    private fun createAccount(email: String, password: String) {
+//        // [START create_user_with_email]
+//        auth.createUserWithEmailAndPassword(email, password)
+//            .addOnCompleteListener { task ->
+//                if (task.isSuccessful) {
+//                    Timber.d("signInWithEmail:success")
+//                    val user = auth.currentUser
+//
+//                } else {
+//                    Toast.makeText(context, "Authentication failed.",
+//                        Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//    }
 
 }
