b7e1b3f0be5c8ad14a22ec6bc335eba75e8e01fc
Hieu Vu <star.hieu.99@gmail.com>
2021-10-10 11:46:37 +0700

    Remove unused imports

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInFragment.kt
@@ -1,7 +1,6 @@
 package com.hieuwu.groceriesstore.presentation.authentication
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
