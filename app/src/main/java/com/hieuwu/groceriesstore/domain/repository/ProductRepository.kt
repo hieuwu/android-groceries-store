27c889f3f0125b10d4173781b568f92203c29e67
Hieu Vu <star.hieu.99@gmail.com>
2021-10-14 21:30:16 +0700

    Declare property for repository

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
@@ -1,11 +1,14 @@
 package com.hieuwu.groceriesstore.domain.repository
 
+import androidx.lifecycle.LiveData
 import com.hieuwu.groceriesstore.domain.entities.LineItem
 import com.hieuwu.groceriesstore.domain.entities.Product
 import com.hieuwu.groceriesstore.domain.entities.ProductAndLineItem
+import com.hieuwu.groceriesstore.domain.models.ProductModel
 import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
+    val products: LiveData<List<ProductModel>>
     fun getProductById(productId: String): Flow<Product>
     fun getAllProducts(): Flow<List<Product>>
     suspend fun getAllProductsByCategory(categoryId: String): Flow<List<Product>>
