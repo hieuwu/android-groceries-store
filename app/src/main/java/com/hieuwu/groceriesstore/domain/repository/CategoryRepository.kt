40b683b03160c4c518d0f95bf384debbbeaf4907
Hieu Vu <star.hieu.99@gmail.com>
2021-10-18 21:23:50 +0700

    Update repository to get data with domain model

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/CategoryRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/CategoryRepository.kt
@@ -1,9 +1,11 @@
 package com.hieuwu.groceriesstore.domain.repository
 
+import androidx.lifecycle.LiveData
 import com.hieuwu.groceriesstore.domain.entities.Category
+import com.hieuwu.groceriesstore.domain.models.CategoryModel
 import kotlinx.coroutines.flow.Flow
 
 interface CategoryRepository {
     suspend fun getFromServer()
-    fun getFromLocal(): Flow<List<Category>>
+    fun getFromLocal(): LiveData<List<CategoryModel>>
 }
