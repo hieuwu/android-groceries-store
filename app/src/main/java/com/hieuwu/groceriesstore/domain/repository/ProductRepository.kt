ee1fe460fd89a9155ab53caeb2e00cdd073375d9
Hieu Vu <star.hieu.99@gmail.com>
2021-10-18 21:24:03 +0700

    Refactor getting data with domain model

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
@@ -3,18 +3,17 @@ package com.hieuwu.groceriesstore.domain.repository
 import androidx.lifecycle.LiveData
 import com.hieuwu.groceriesstore.domain.entities.LineItem
 import com.hieuwu.groceriesstore.domain.entities.Product
-import com.hieuwu.groceriesstore.domain.entities.ProductAndLineItem
 import com.hieuwu.groceriesstore.domain.models.ProductModel
 import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
     val products: LiveData<List<ProductModel>>
     fun getProductById(productId: String): Flow<Product>
-    fun getAllProducts(): Flow<List<Product>>
-    suspend fun getAllProductsByCategory(categoryId: String): Flow<List<Product>>
+    fun getAllProducts(): LiveData<List<ProductModel>>
+    suspend fun getAllProductsByCategory(categoryId: String):LiveData<List<ProductModel>>
     suspend fun getFromServer()
     suspend fun hasProduct(): Boolean
     suspend fun updateLineItem(lineItem: LineItem)
     suspend fun removeLineItem(lineItem: LineItem)
-    fun searchProductsListByName(name: String?) : Flow<List<Product>>
+    fun searchProductsListByName(name: String?) : LiveData<List<ProductModel>>
 }
