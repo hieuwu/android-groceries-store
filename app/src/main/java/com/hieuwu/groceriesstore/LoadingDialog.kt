0ff94048b6437d6fd7edae2ae94091dc77b17bff
Hieu Vu <star.hieu.99@gmail.com>
2021-10-19 22:09:34 +0700

    Implement loading dialog code behind

--- app/src/main/java/com/hieuwu/groceriesstore/LoadingDialog.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/LoadingDialog.kt
@@ -1,4 +1,32 @@
 package com.hieuwu.groceriesstore
 
+import android.app.Activity
+import androidx.appcompat.app.AlertDialog
+
 class LoadingDialog {
+    private lateinit var myActivity: Activity
+    private lateinit var dialog: AlertDialog
+
+    constructor(activity: Activity) {
+        myActivity = activity
+    }
+
+    fun showLoadingDialog() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(myActivity)
+
+        val inflater = myActivity.layoutInflater
+        val view = inflater.inflate(R.layout.loading_layout, null)
+
+        builder.setView(view)
+        builder.setCancelable(false)
+
+        dialog = builder.create()
+        dialog.show()
+    }
+
+    fun dismissLoadingDialog() {
+        dialog.dismiss()
+    }
+
+
 }
