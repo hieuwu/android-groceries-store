ee72c053ab5e97ed676ff89e75954afce742b2f0
Hieu Vu <star.hieu.99@gmail.com>
2021-10-21 21:55:51 +0700

    Handle create user then insert user data to fire store

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignUpFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignUpFragment.kt
@@ -9,23 +9,30 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.hieuwu.groceriesstore.LoadingDialog
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.databinding.FragmentSignUpBinding
+import com.hieuwu.groceriesstore.domain.models.UserModel
 import timber.log.Timber
 
 class SignUpFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var loadingDialog: LoadingDialog
-    private lateinit var binding: FragmentSignUpBinding;
+    private lateinit var binding: FragmentSignUpBinding
+    private lateinit var database: DatabaseReference
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         auth = FirebaseAuth.getInstance()
-
+        database = Firebase.database.reference
 
         binding = DataBindingUtil.inflate<FragmentSignUpBinding>(
             inflater, R.layout.fragment_sign_up, container, false
@@ -41,7 +48,12 @@ class SignUpFragment : Fragment() {
 
         binding.signupBtn.setOnClickListener {
             //If sign in successful, go back
-            createAccount(signUpViewModel.email!!, signUpViewModel.password!!)
+            createAccount(
+                signUpViewModel.email!!,
+                signUpViewModel.password!!,
+                signUpViewModel.name!!
+            )
+
 //            val i = Intent(this.context, MainActivity::class.java)
 //            startActivity(i)
         }
@@ -50,14 +62,24 @@ class SignUpFragment : Fragment() {
         return binding.root
     }
 
-    private fun createAccount(email: String, password: String) {
+    private fun createAccount(email: String, password: String, name: String) {
         loadingDialog.showLoadingDialog()
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(requireActivity()) { task ->
                 Timber.d(task.exception)
                 if (task.isSuccessful) {
                     // Create account success, update UI with the signed-in user's information
-                    val user = auth.currentUser
+                    val userId = auth.currentUser!!.uid
+                    val newUser = hashMapOf(
+                        "name" to name,
+                        "email" to email,
+                    )
+                    val db = Firebase.firestore
+                    db.collection("users").document(userId)
+                        .set(newUser)
+                        .addOnSuccessListener { Timber.d("DocumentSnapshot successfully written!") }
+                        .addOnFailureListener { e -> Timber.d("Error writing document%s", e) }
+
                 } else {
                     // If sign in fails, display a message to the user.
                     Toast.makeText(
@@ -67,6 +89,8 @@ class SignUpFragment : Fragment() {
                 }
 
             }.addOnFailureListener { Exception -> Timber.d(Exception) }
+
+        loadingDialog.dismissLoadingDialog()
     }
 
 }
