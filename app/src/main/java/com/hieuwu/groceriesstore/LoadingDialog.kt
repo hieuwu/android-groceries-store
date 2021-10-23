bb2fa18780857fb944522eb5dfa040b36ccbc1af
Hieu Vu <star.hieu.99@gmail.com>
2021-10-23 21:51:03 +0700

    Extends  dialog instead of create new class

--- app/src/main/java/com/hieuwu/groceriesstore/LoadingDialog.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/LoadingDialog.kt
@@ -1,32 +1,13 @@
 package com.hieuwu.groceriesstore
 
-import android.app.Activity
-import androidx.appcompat.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.view.LayoutInflater
 
-class LoadingDialog {
-    private lateinit var myActivity: Activity
-    private lateinit var dialog: AlertDialog
-
-    constructor(activity: Activity) {
-        myActivity = activity
+class LoadingDialog(context: Context) : Dialog(context) {
+    init {
+        val view = LayoutInflater.from(context).inflate(R.layout.loading_layout, null)
+        setContentView(view)
+        setCancelable(false)
     }
-
-    fun showLoadingDialog() {
-        val builder: AlertDialog.Builder = AlertDialog.Builder(myActivity)
-
-        val inflater = myActivity.layoutInflater
-        val view = inflater.inflate(R.layout.loading_layout, null)
-
-        builder.setView(view)
-        builder.setCancelable(false)
-
-        dialog = builder.create()
-        dialog.show()
-    }
-
-    fun dismissLoadingDialog() {
-        dialog.dismiss()
-    }
-
-
 }
