342c3ea0cec74f0450a80a633f8ac0092df80e64
Hieu Vu <star.hieu.99@gmail.com>
2021-10-24 21:49:49 +0700

    Create user dao to access data

--- app/src/main/java/com/hieuwu/groceriesstore/data/dao/UserDao.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/dao/UserDao.kt
@@ -1,4 +1,19 @@
 package com.hieuwu.groceriesstore.data.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.hieuwu.groceriesstore.domain.entities.User
+import com.hieuwu.groceriesstore.utilities.USER_TABLE
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(user: User)
+
+    @Query("SELECT * FROM $USER_TABLE WHERE id = :id")
+    fun getById(id: String): Flow<User>
 }
