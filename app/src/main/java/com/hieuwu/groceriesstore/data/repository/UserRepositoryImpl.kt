997919f3aa2f968a3a203368726a4f866ddfcf3b
Hieu Vu <star.hieu.99@gmail.com>
2021-10-24 21:49:35 +0700

    Inject user dao to user repository

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/UserRepositoryImpl.kt
@@ -1,4 +1,16 @@
 package com.hieuwu.groceriesstore.data.repository
 
-class UserRepositoryImpl {
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.asLiveData
+import com.hieuwu.groceriesstore.data.dao.UserDao
+import com.hieuwu.groceriesstore.domain.entities.asDomainModel
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserRepositoryImpl @Inject constructor(private val userDao: UserDao) {
+
+    fun getUser(id: String) = Transformations.map(userDao.getById(id).asLiveData()) {
+        it.asDomainModel()
+    }
 }
