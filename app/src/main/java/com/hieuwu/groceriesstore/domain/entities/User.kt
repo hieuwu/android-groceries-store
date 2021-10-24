6155d4fb18d6f9ab178c7fdeda89b89a94fdadd2
Hieu Vu <star.hieu.99@gmail.com>
2021-10-24 21:49:15 +0700

    Implement user entity

--- app/src/main/java/com/hieuwu/groceriesstore/domain/entities/User.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/entities/User.kt
@@ -1,4 +1,34 @@
 package com.hieuwu.groceriesstore.domain.entities
 
-class User {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.hieuwu.groceriesstore.domain.models.UserModel
+
+@Entity
+
+class User(
+    @PrimaryKey
+    var id: String,
+
+    @ColumnInfo(name = "name")
+    var name: String,
+
+    @ColumnInfo(name = "email")
+    var email: String,
+
+    @ColumnInfo(name = "address")
+    var address: String?,
+
+    @ColumnInfo(name = "phone")
+    var phone: String?,
+)
+
+fun User.asDomainModel(): UserModel {
+    return UserModel(
+        id = this.id,
+        name = this.name,
+        phone = this.phone ?: "",
+        email = this.email
+    )
 }
