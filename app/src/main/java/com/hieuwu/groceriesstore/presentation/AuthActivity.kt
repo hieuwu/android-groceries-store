dbfffe87bda5262bcb994dfc9f6e67ebeeb4dd73
Hieu Vu <star.hieu.99@gmail.com>
2021-10-26 21:44:12 +0700

    Add entry point annotation for injection

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/AuthActivity.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/AuthActivity.kt
@@ -3,7 +3,9 @@ package com.hieuwu.groceriesstore.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hieuwu.groceriesstore.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
