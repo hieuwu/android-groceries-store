e2c1680178421f87f95294e496f7e2ed292d5f67
Hieu Vu <star.hieu.99@gmail.com>
2021-10-26 21:45:36 +0700

    Implement user repository

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
@@ -1,4 +1,8 @@
 package com.hieuwu.groceriesstore.domain.repository
 
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.domain.entities.User
+
 interface UserRepository {
+    suspend fun createAccount(email: String, password: String, name: String): Boolean
 }
