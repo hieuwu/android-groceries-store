5e906ecb95474e74ff3f0d9bc6eb1cfe3d192661
Hieu Vu <star.hieu.99@gmail.com>
2021-10-26 21:46:14 +0700

    Bind impl with interface

--- app/src/main/java/com/hieuwu/groceriesstore/di/RepositoryModule.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/di/RepositoryModule.kt
@@ -3,9 +3,11 @@ package com.hieuwu.groceriesstore.di
 import com.hieuwu.groceriesstore.data.repository.CategoryRepositoryImpl
 import com.hieuwu.groceriesstore.data.repository.OrderRepositoryImpl
 import com.hieuwu.groceriesstore.data.repository.ProductRepositoryImpl
+import com.hieuwu.groceriesstore.data.repository.UserRepositoryImpl
 import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
 import com.hieuwu.groceriesstore.domain.repository.OrderRepository
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -32,4 +34,8 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun bindCategoryRepository(impl: CategoryRepositoryImpl): CategoryRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindUserRepository(impl: UserRepositoryImpl): UserRepository
 }
