3e8c65f441d3ac495e11a9c34f07d7ec71439035
Hieu Vu <star.hieu.99@gmail.com>
2021-10-28 20:19:37 +0700

    Remove unused imports

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
@@ -1,8 +1,5 @@
 package com.hieuwu.groceriesstore.domain.repository
 
-import androidx.lifecycle.LiveData
-import com.hieuwu.groceriesstore.domain.entities.User
-
 interface UserRepository {
     suspend fun createAccount(email: String, password: String, name: String): Boolean
 }
