d6ff5c0be2a5c52daef171f789e92db5de568275
Hieu Vu <star.hieu.99@gmail.com>
2021-10-28 20:19:28 +0700

    Await for result

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/CategoryRepositoryImpl.kt
@@ -11,6 +11,7 @@ import com.hieuwu.groceriesstore.domain.entities.Category
 import com.hieuwu.groceriesstore.domain.entities.asDomainModel
 import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
@@ -29,12 +30,12 @@ class CategoryRepositoryImpl @Inject constructor(private val categoryDao: Catego
             }
         }.addOnFailureListener { exception ->
             Timber.w("Error getting documents.${exception}")
-        }
+        }.await()
+
         withContext(Dispatchers.IO) {
             categoryDao.insertAll(categoriesList)
         }
     }
-
     private fun getCategoryEntityFromDocument(document: QueryDocumentSnapshot): Category {
         val id = document.id
         val name: String = document.data["name"] as String
