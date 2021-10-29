abffc8b9225ff513eb5779ebcbc8a6400a5793e8
Hieu Vu <star.hieu.99@gmail.com>
2021-10-29 20:19:59 +0700

    Move authenticate to repository

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
@@ -2,4 +2,6 @@ package com.hieuwu.groceriesstore.domain.repository
 
 interface UserRepository {
     suspend fun createAccount(email: String, password: String, name: String): Boolean
+    suspend fun authenticate(email: String, password: String): Boolean
+
 }
