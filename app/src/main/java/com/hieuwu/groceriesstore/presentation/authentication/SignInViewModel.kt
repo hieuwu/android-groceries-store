43ff21543f1a936400bcfd88bd4ea57bf187e630
Hieu Vu <star.hieu.99@gmail.com>
2021-10-29 20:19:32 +0700

    Handle sign in method

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInViewModel.kt
@@ -1,11 +1,17 @@
 package com.hieuwu.groceriesstore.presentation.authentication
 
 import androidx.databinding.Bindable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.hieuwu.groceriesstore.BR
+import com.hieuwu.groceriesstore.domain.repository.UserRepository
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
-class SignInViewModel : ObservableViewModel() {
+class SignInViewModel @Inject constructor(private val userRepository: UserRepository) : ObservableViewModel() {
 
     private var _email: String? = null
     var email: String?
@@ -28,4 +34,12 @@ class SignInViewModel : ObservableViewModel() {
             _password = value
             notifyPropertyChanged(BR.password)
         }
-}
+    private val _isSignUpSuccessful = MutableLiveData<Boolean?>()
+    val isSignUpSuccessful: LiveData<Boolean?>
+        get() = _isSignUpSuccessful
+
+    public fun signIn(email: String, password: String) {
+        viewModelScope.launch {
+            _isSignUpSuccessful.value = userRepository!!.authenticate(_email!!, _password!!)
+        }
+    }}
