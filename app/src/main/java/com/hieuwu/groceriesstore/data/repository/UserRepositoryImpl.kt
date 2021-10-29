68a6ba5db8a23aec34d7c935c428a97834a1ae8b
Hieu Vu <star.hieu.99@gmail.com>
2021-10-29 20:20:11 +0700

    Handle authenticate in repository

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/UserRepositoryImpl.kt
@@ -1,13 +1,10 @@
 package com.hieuwu.groceriesstore.data.repository
 
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.asLiveData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.hieuwu.groceriesstore.data.dao.UserDao
 import com.hieuwu.groceriesstore.domain.entities.User
-import com.hieuwu.groceriesstore.domain.entities.asDomainModel
 import com.hieuwu.groceriesstore.domain.repository.UserRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
@@ -54,5 +51,15 @@ class UserRepositoryImpl @Inject constructor(private val userDao: UserDao) : Use
             return true
         } else return false
         return true
+    }
+
+    override suspend fun authenticate(email: String, password: String): Boolean {
+        var isSucess = false
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                Timber.d(task.exception)
+                isSucess = task.isSuccessful
+            }.addOnFailureListener { Exception -> Timber.d(Exception) }.await()
+        return isSucess
     }
 }
