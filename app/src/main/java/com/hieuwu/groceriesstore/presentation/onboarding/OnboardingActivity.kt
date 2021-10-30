b30027b2d3f30baddc49cf53d1c4ed71e044fe88
Hieu Vu <star.hieu.99@gmail.com>
2021-10-30 22:28:04 +0700

    Remove unused fragment

---
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingActivity.kt
@@ -0,0 +1,11 @@
+package com.hieuwu.groceriesstore.presentation.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
