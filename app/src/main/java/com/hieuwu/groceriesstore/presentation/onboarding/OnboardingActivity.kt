c2577bafeecc5195e05fe6349c081b5adc0d8521
Hieu Vu <star.hieu.99@gmail.com>
2021-10-30 22:28:59 +0700

    Create onboarding activity to sync data

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingActivity.kt
@@ -1,11 +1,44 @@
 package com.hieuwu.groceriesstore.presentation.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.groceriesstore.MainActivity
+import com.hieuwu.groceriesstore.R
+import com.hieuwu.groceriesstore.databinding.ActivityOnboardingBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class OnboardingActivity : AppCompatActivity() {
+    lateinit var binding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_onboarding)
+        binding.viewModel = ViewModelProvider(this).get(OnboardingViewModel::class.java)
+
+        binding.viewModel.productSyncStatus.observe(this) {
+            if (it!!) {
+                //Handle sync product successful
+            }
+            else {
+               //Handle sync product failed
+            }
+        }
+
+        binding.viewModel.categorySyncStatus.observe(this) {
+            if (it!!) {
+                //Handle sync category successful
+            }
+            else {
+                //Handle sync category failed
+            }
+        }
+        binding.getStartedButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
