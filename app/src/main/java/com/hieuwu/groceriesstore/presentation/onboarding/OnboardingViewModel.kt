e0a80f309d1bcccecf3956eca1bd62d6f7cd4924
Hieu Vu <star.hieu.99@gmail.com>
2021-10-30 22:29:12 +0700

    Create onboarding viewmodel with sync status

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingViewModel.kt
@@ -1,4 +1,24 @@
 package com.hieuwu.groceriesstore.presentation.onboarding
 
-class OnboardingViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+
+class OnboardingViewModel: ObservableViewModel() {
+    init {
+        //Sync product
+
+
+
+        //Sync category
+    }
+
+    private val _productSyncStatus = MutableLiveData<Boolean?>()
+    val productSyncStatus: LiveData<Boolean?>
+        get() = _productSyncStatus
+
+    private val _categorySyncStatus = MutableLiveData<Boolean?>()
+    val categorySyncStatus: LiveData<Boolean?>
+        get() = _categorySyncStatus
+
 }
