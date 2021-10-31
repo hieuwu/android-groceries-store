225419c6cf0141afa91d386320a59d1986f2dcb9
Hieu Vu <star.hieu.99@gmail.com>
2021-10-31 20:38:04 +0700

    Temporary lock sync status update

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingViewModel.kt
@@ -1,24 +1,46 @@
 package com.hieuwu.groceriesstore.presentation.onboarding
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class OnboardingViewModel: ObservableViewModel() {
+class OnboardingViewModel @Inject constructor(
+    private val productRepository: ProductRepository,
+    private val categoryRepository: CategoryRepository
+) : ObservableViewModel() {
+
     init {
-        //Sync product
+        viewModelScope.launch {
+            getCategories()
+            productRepository.getFromServer()
+        }
+        onSyncDone()
+//        _productSyncStatus.value = true
+//        categorySyncStatus.value = true
 
-
-
-        //Sync category
     }
 
+    private fun onSyncDone() {
+        _productSyncStatus?.value = true
+        _categorySyncStatus?.value = true
+    }
     private val _productSyncStatus = MutableLiveData<Boolean?>()
-    val productSyncStatus: LiveData<Boolean?>
+    val productSyncStatus: MutableLiveData<Boolean?>
         get() = _productSyncStatus
 
     private val _categorySyncStatus = MutableLiveData<Boolean?>()
-    val categorySyncStatus: LiveData<Boolean?>
+    val categorySyncStatus: MutableLiveData<Boolean?>
         get() = _categorySyncStatus
 
+    private suspend fun getCategories() {
+        withContext(Dispatchers.IO) {
+            categoryRepository.getFromServer()
+        }
+    }
 }
