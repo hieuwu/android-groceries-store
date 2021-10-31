1e7766ef0f7998e574591a545ad8613bdbfb1f4a
Hieu Vu <star.hieu.99@gmail.com>
2021-10-31 20:37:45 +0700

    Implement viewmodel factory

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingViewModelFactory.kt
@@ -1,4 +1,19 @@
 package com.hieuwu.groceriesstore.presentation.onboarding
 
-class OnboardingViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.presentation.cart.CartViewModel
+
+class OnboardingViewModelFactory(
+    private val productRepository: ProductRepository, private val categoryRepository: CategoryRepository
+) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(OnboardingViewModel::class.java)) {
+            return OnboardingViewModel(productRepository, categoryRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
