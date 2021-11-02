f0f75c4f516bdaaa5c487f113fe7e9b9908bf90d
Hieu Vu <star.hieu.99@gmail.com>
2021-11-02 21:35:59 +0700

    Update sync status when sync complete

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingViewModel.kt
@@ -1,5 +1,6 @@
 package com.hieuwu.groceriesstore.presentation.onboarding
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
@@ -15,28 +16,26 @@ class OnboardingViewModel @Inject constructor(
     private val categoryRepository: CategoryRepository
 ) : ObservableViewModel() {
 
+    private val _productSyncStatus = MutableLiveData<Boolean?>(false)
+    val productSyncStatus: LiveData<Boolean?>
+        get() = _productSyncStatus
+
+    private val _categorySyncStatus = MutableLiveData<Boolean>(false)
+    val categorySyncStatus: MutableLiveData<Boolean>
+        get() = _categorySyncStatus
     init {
         viewModelScope.launch {
             getCategories()
             productRepository.getFromServer()
         }
-        onSyncDone()
-//        _productSyncStatus.value = true
-//        categorySyncStatus.value = true
-
+        updateSyncStatus(true)
     }
 
-    private fun onSyncDone() {
-        _productSyncStatus?.value = true
-        _categorySyncStatus?.value = true
-    }
-    private val _productSyncStatus = MutableLiveData<Boolean?>()
-    val productSyncStatus: MutableLiveData<Boolean?>
-        get() = _productSyncStatus
+    fun updateSyncStatus(status: Boolean) {
 
-    private val _categorySyncStatus = MutableLiveData<Boolean?>()
-    val categorySyncStatus: MutableLiveData<Boolean?>
-        get() = _categorySyncStatus
+        _productSyncStatus.value = status
+        _categorySyncStatus.value = status
+    }
 
     private suspend fun getCategories() {
         withContext(Dispatchers.IO) {
