a731df1768d87fdc296900dff48f3eab6af14638
Hieu Vu <star.hieu.99@gmail.com>
2021-11-03 21:44:51 +0700

    Implement view model factory

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/updateprofile/UpdateProfileViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/updateprofile/UpdateProfileViewModelFactory.kt
@@ -1,4 +1,16 @@
 package com.hieuwu.groceriesstore.presentation.updateprofile
 
-class UpdateProfileViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.groceriesstore.domain.repository.UserRepository
+
+class UpdateProfileViewModelFactory(private val userRepository: UserRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(UpdateProfileViewModel::class.java)) {
+            return UpdateProfileViewModel(userRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
 }
