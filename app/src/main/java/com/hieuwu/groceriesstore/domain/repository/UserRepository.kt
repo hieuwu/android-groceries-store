7b7887d634b1d64dc8c26c938a40747d11f24c1c
Hieu Vu <star.hieu.99@gmail.com>
2021-11-03 21:57:52 +0700

    Get user data

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
@@ -1,7 +1,11 @@
 package com.hieuwu.groceriesstore.domain.repository
 
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.domain.models.UserModel
+
 interface UserRepository {
     suspend fun createAccount(email: String, password: String, name: String): Boolean
     suspend fun authenticate(email: String, password: String): Boolean
+    fun getCurrentUser(): LiveData<UserModel>
 
 }
