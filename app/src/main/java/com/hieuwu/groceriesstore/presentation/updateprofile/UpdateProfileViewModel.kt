a0d9db20c8290669e769083285f414120177b353
Hieu Vu <star.hieu.99@gmail.com>
2021-11-03 21:45:02 +0700

    Create view model and inject

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/updateprofile/UpdateProfileViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/updateprofile/UpdateProfileViewModel.kt
@@ -1,4 +1,10 @@
 package com.hieuwu.groceriesstore.presentation.updateprofile
 
-class UpdateProfileViewModel {
+import com.hieuwu.groceriesstore.domain.repository.UserRepository
+import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import javax.inject.Inject
+
+class UpdateProfileViewModel @Inject constructor(private val userRepository: UserRepository) :
+    ObservableViewModel() {
+
 }
