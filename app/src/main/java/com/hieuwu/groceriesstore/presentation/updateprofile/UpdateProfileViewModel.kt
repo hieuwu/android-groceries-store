93a0fce79999ccf5c9cf8e9faf1b14068dc6af29
Hieu Vu <star.hieu.99@gmail.com>
2021-11-04 21:46:48 +0700

    Display message when update profile done

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/updateprofile/UpdateProfileViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/updateprofile/UpdateProfileViewModel.kt
@@ -3,13 +3,16 @@ package com.hieuwu.groceriesstore.presentation.updateprofile
 import androidx.databinding.Bindable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.hieuwu.groceriesstore.BR
 import com.hieuwu.groceriesstore.domain.models.UserModel
 import com.hieuwu.groceriesstore.domain.repository.UserRepository
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import kotlinx.coroutines.launch
+import java.lang.Exception
 import javax.inject.Inject
 
-class UpdateProfileViewModel @Inject constructor(userRepository: UserRepository) :
+class UpdateProfileViewModel @Inject constructor(private val userRepository: UserRepository) :
     ObservableViewModel() {
     private val _user =
         userRepository.getCurrentUser() as MutableLiveData<UserModel>
@@ -48,6 +51,20 @@ class UpdateProfileViewModel @Inject constructor(userRepository: UserRepository)
             notifyPropertyChanged(BR.phoneNumber)
         }
 
+    private val _isDoneUpdate = MutableLiveData<Boolean>(null)
+    val isDoneUpdate: LiveData<Boolean?>
+        get() = _isDoneUpdate
 
+    fun updateUserProfile() {
+        var id = _user.value!!.id
+        try {
+            viewModelScope.launch {
+                userRepository.updateUserProfile(id, _name!!, email!!, email!!)
+            }
+            _isDoneUpdate.value = true
+        } catch (e: Exception) {
+            _isDoneUpdate.value = false
+        }
+    }
 
 }
