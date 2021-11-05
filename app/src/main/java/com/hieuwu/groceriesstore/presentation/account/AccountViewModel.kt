842ecb41e62df5589e654a19c92cd73ac5d2e54f
Hieu Vu <star.hieu.99@gmail.com>
2021-11-05 20:19:37 +0700

    Implement view model and display data

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/account/AccountViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/account/AccountViewModel.kt
@@ -1,4 +1,20 @@
 package com.hieuwu.groceriesstore.presentation.account
 
-class AccountViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.hieuwu.groceriesstore.domain.models.UserModel
+import com.hieuwu.groceriesstore.domain.repository.UserRepository
+import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import javax.inject.Inject
+
+class AccountViewModel @Inject constructor(private val userRepository: UserRepository) :
+    ObservableViewModel() {
+
+    private val _user =
+        userRepository.getCurrentUser() as MutableLiveData<UserModel>
+    val user: LiveData<UserModel?>
+        get() = _user
+
+
+
 }
