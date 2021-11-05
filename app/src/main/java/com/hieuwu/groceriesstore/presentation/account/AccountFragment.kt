1e1cc3476acd2b9c9ca7b5b92d0d530a28768317
Hieu Vu <star.hieu.99@gmail.com>
2021-11-05 20:29:46 +0700

    Update viewmodel in ui controller

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/account/AccountFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/account/AccountFragment.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.databinding.FragmentAccountBinding
 import com.hieuwu.groceriesstore.domain.repository.UserRepository
+import com.hieuwu.groceriesstore.presentation.updateprofile.UpdateProfileViewModelFactory
 import javax.inject.Inject
 
 class AccountFragment : Fragment() {
@@ -26,6 +28,11 @@ class AccountFragment : Fragment() {
         binding = DataBindingUtil.inflate<FragmentAccountBinding>(
             inflater, R.layout.fragment_account, container, false
         )
+
+        val viewModelFactory = UpdateProfileViewModelFactory(userRepository)
+        val viewModel =
+            ViewModelProvider(this, viewModelFactory).get(AccountViewModel::class.java)
+        binding.viewModel = viewModel
 
         binding.lifecycleOwner = this
         binding.profileLayout.setOnClickListener {
