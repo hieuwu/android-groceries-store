431d3445bacaa7fa30cc7fd2dc4a601f861e1082
Hieu Vu <star.hieu.99@gmail.com>
2021-11-07 20:00:46 +0700

    Declare method for clear user data

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/UserRepositoryImpl.kt
@@ -87,6 +87,10 @@ class UserRepositoryImpl @Inject constructor(private val userDao: UserDao) : Use
         }
     }
 
+    override suspend fun clearUser() {
+        TODO("Not yet implemented")
+    }
+
     override fun getCurrentUser() =
         Transformations.map(userDao.getCurrentUser().asLiveData()) {
             it.asDomainModel()
