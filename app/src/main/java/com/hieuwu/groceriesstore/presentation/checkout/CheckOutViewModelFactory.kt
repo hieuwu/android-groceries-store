7e6d82a06e620a641050d9f1db85b8952f297936
Hieu Vu <star.hieu.99@gmail.com>
2021-11-08 21:25:39 +0700

    Add user repository to factory

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/checkout/CheckOutViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/checkout/CheckOutViewModelFactory.kt
@@ -3,13 +3,15 @@ package com.hieuwu.groceriesstore.presentation.checkout
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.hieuwu.groceriesstore.domain.repository.OrderRepository
+import com.hieuwu.groceriesstore.domain.repository.UserRepository
 
 class CheckOutViewModelFactory(
-    private val orderId: String, private val orderRepository: OrderRepository
+    private val orderId: String, private val orderRepository: OrderRepository, private val userRepository: UserRepository
+
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CheckOutViewModel::class.java)) {
-            return CheckOutViewModel(orderId, orderRepository) as T
+            return CheckOutViewModel(orderId, orderRepository, userRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
