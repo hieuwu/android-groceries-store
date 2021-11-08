f60fe5168086f9334db5b6ab821da4c3d3278a04
Hieu Vu <star.hieu.99@gmail.com>
2021-11-08 21:09:31 +0700

    Update user profile with address

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
@@ -7,6 +7,6 @@ interface UserRepository {
     suspend fun createAccount(email: String, password: String, name: String): Boolean
     suspend fun authenticate(email: String, password: String): Boolean
     fun getCurrentUser(): LiveData<UserModel>
-    suspend fun updateUserProfile(userId: String, name: String, email: String, phone: String)
+    suspend fun updateUserProfile(userId: String, name: String, email: String, phone: String, address: String)
     suspend fun clearUser()
 }
