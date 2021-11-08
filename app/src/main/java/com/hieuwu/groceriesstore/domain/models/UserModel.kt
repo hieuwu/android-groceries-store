dc610a888dfacb40097d0ad5a9f479210c5043e3
Hieu Vu <star.hieu.99@gmail.com>
2021-11-08 21:09:42 +0700

    Add address to user model

--- app/src/main/java/com/hieuwu/groceriesstore/domain/models/UserModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/models/UserModel.kt
@@ -1,5 +1,4 @@
 package com.hieuwu.groceriesstore.domain.models
 
-import com.google.firebase.firestore.IgnoreExtraProperties
 
-data class UserModel(val id: String, val name: String? = null, val email: String?, val phone: String)
+data class UserModel(val id: String, val name: String? = null, val email: String?, val phone: String, val address: String)
