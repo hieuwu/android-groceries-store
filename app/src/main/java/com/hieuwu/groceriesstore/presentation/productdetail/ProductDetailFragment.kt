a9d6a6b760fb7e182010e0e29419386f7d742be6
Hieu Vu <star.hieu.99@gmail.com>
2021-11-09 21:41:44 +0700

    Remove unused imports

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/productdetail/ProductDetailFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/productdetail/ProductDetailFragment.kt
@@ -1,13 +1,12 @@
 package com.hieuwu.groceriesstore.presentation.productdetail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.NestedScrollView
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
