908f26655a99a6ef1d947d596c6a4fbe2976e92c
Hieu Vu <star.hieu.99@gmail.com>
2021-11-09 21:41:49 +0700

    Remove unused imports

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/updateprofile/UpdateProfileFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/updateprofile/UpdateProfileFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
