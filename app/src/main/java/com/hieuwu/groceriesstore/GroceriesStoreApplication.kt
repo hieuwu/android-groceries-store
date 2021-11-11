9ce5135eeeda477d2b371fa9d028b131a7ab595d
Hieu Vu <star.hieu.99@gmail.com>
2021-11-11 21:06:05 +0700

    Setup daily refresh data

--- app/src/main/java/com/hieuwu/groceriesstore/GroceriesStoreApplication.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/GroceriesStoreApplication.kt
@@ -1,13 +1,36 @@
 package com.hieuwu.groceriesstore
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.hieuwu.groceriesstore.works.RefreshDatabaseWork
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 @HiltAndroidApp
-class GroceriesStoreApplication: Application() {
+class GroceriesStoreApplication : Application() {
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+
+        delayedInit()
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            val repeatingRequest =
+                PeriodicWorkRequestBuilder<RefreshDatabaseWork>(1, TimeUnit.DAYS).build()
+            WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+                RefreshDatabaseWork.WORK_NAME,
+                ExistingPeriodicWorkPolicy.KEEP, repeatingRequest
+            )
+
+        }
     }
 }
