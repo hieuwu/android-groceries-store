d0dd352c8cd77e172b359c5ed905191910edcdd2
Hieu Vu <star.hieu.99@gmail.com>
2021-11-12 21:24:25 +0700

    Remove unused imports

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/CategoryItemAdapter.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/CategoryItemAdapter.kt
@@ -6,7 +6,6 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.hieuwu.groceriesstore.databinding.LayoutCategoryGridItemBinding
-import com.hieuwu.groceriesstore.domain.entities.Category
 import com.hieuwu.groceriesstore.domain.models.CategoryModel
 
 class CategoryItemAdapter(val onClickListener: OnClickListener) :
