3ab496a948b114d2fecf1a11c8f6e7d144acc75e
Hieu Vu <star.hieu.99@gmail.com>
2021-11-12 21:24:29 +0700

    Remove unused imports

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/CategoryRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/CategoryRepository.kt
@@ -1,9 +1,7 @@
 package com.hieuwu.groceriesstore.domain.repository
 
 import androidx.lifecycle.LiveData
-import com.hieuwu.groceriesstore.domain.entities.Category
 import com.hieuwu.groceriesstore.domain.models.CategoryModel
-import kotlinx.coroutines.flow.Flow
 
 interface CategoryRepository {
     suspend fun getFromServer()
