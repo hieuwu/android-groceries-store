14b2a6fb088b2c34c1b4d55dca5ca830983369dd
Hieu Vu <star.hieu.99@gmail.com>
2021-11-14 21:16:07 +0700

    Change return type of get user data

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/UserRepository.kt
@@ -6,7 +6,7 @@ import com.hieuwu.groceriesstore.domain.models.UserModel
 interface UserRepository {
     suspend fun createAccount(email: String, password: String, name: String): Boolean
     suspend fun authenticate(email: String, password: String): Boolean
-    fun getCurrentUser(): LiveData<UserModel>
+    fun getCurrentUser(): LiveData<UserModel?>
     suspend fun updateUserProfile(userId: String, name: String, email: String, phone: String, address: String)
     suspend fun clearUser()
 }
