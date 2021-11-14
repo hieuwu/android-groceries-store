c126e8c6aae328071a6ef5fd6ff7087a1c09b267
Hieu Vu <star.hieu.99@gmail.com>
2021-11-14 21:36:45 +0700

    Replace resource with image url

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/ViewPagerAdapter.kt
@@ -1,19 +1,21 @@
 package com.hieuwu.groceriesstore.presentation.adapters
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.viewpager.widget.PagerAdapter
+import com.bumptech.glide.Glide
 import com.hieuwu.groceriesstore.R
 
 class ViewPagerAdapter(val context: Context) : PagerAdapter() {
 
     private val images = arrayOf(
-        R.drawable.banner,
-        R.drawable.banner,
-        R.drawable.banner
+        "gs://groceries-store-ad0eb.appspot.com/slider-1.jpg",
+        "gs://groceries-store-ad0eb.appspot.com/slider-1.jpg",
+        "gs://groceries-store-ad0eb.appspot.com/slider-1.jpg",
     )
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
@@ -21,9 +23,11 @@ class ViewPagerAdapter(val context: Context) : PagerAdapter() {
             context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = layoutInflater.inflate(R.layout.layout_image_layout, null);
         val imageView = view.findViewById<ImageView>(R.id.imageView);
-        imageView.setImageResource(images[position]);
-
-        container.addView(view, 0);
+//        imageView.setImageResource(images[position]);
+        Glide.with(view)
+            .load(Uri.parse(images[position]))
+            .into(imageView);
+        container.addView(view, 0)
         return view
     }
 
