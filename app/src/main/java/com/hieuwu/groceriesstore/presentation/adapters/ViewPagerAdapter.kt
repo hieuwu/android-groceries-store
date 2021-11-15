607d6cee44956194fcad92518cd22687cc78565e
Hieu Vu <star.hieu.99@gmail.com>
2021-11-15 21:14:01 +0700

    Bind image from uri instead of resource

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/adapters/ViewPagerAdapter.kt
@@ -1,33 +1,30 @@
 package com.hieuwu.groceriesstore.presentation.adapters
 
 import android.content.Context
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.viewpager.widget.PagerAdapter
-import com.bumptech.glide.Glide
 import com.hieuwu.groceriesstore.R
+import com.hieuwu.groceriesstore.presentation.bindImage
 
 class ViewPagerAdapter(val context: Context) : PagerAdapter() {
 
     private val images = arrayOf(
-        "gs://groceries-store-ad0eb.appspot.com/slider-1.jpg",
-        "gs://groceries-store-ad0eb.appspot.com/slider-1.jpg",
-        "gs://groceries-store-ad0eb.appspot.com/slider-1.jpg",
+        "https://firebasestorage.googleapis.com/v0/b/groceries-store-ad0eb.appspot.com/o/slider-1.jpg?alt=media&token=468896eb-8083-4a79-ba18-6af6a7c3e3eb",
+        "https://firebasestorage.googleapis.com/v0/b/groceries-store-ad0eb.appspot.com/o/slider-3.jpg?alt=media&token=94a991e9-75a1-45ae-9439-3dc94522eb9f",
+        "https://firebasestorage.googleapis.com/v0/b/groceries-store-ad0eb.appspot.com/o/slider-2.jpg?alt=media&token=c7fcd8de-3bb6-48e2-9a63-f393d4b45c98",
     )
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
         val layoutInflater =
             context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = layoutInflater.inflate(R.layout.layout_image_layout, null);
-        val imageView = view.findViewById<ImageView>(R.id.imageView);
-//        imageView.setImageResource(images[position]);
-        Glide.with(view)
-            .load(Uri.parse(images[position]))
-            .into(imageView);
-        container.addView(view, 0)
+        val imageView = view.findViewById<ImageView>(R.id.imageView)
+        bindImage(imageView, images[position])
+        container.addView(view, 0);
+
         return view
     }
 
@@ -35,7 +32,6 @@ class ViewPagerAdapter(val context: Context) : PagerAdapter() {
 
 
     override fun isViewFromObject(view: View, `object`: Any) = view == `object`
-
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
         container.removeView(`object` as View)
