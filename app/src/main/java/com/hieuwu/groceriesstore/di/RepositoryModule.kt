cfdd123a81ef4d1a09071b6b652526c3a6b874cc
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 13:56:55 +0700

    Add di module for recipe

--- app/src/main/java/com/hieuwu/groceriesstore/di/RepositoryModule.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/di/RepositoryModule.kt
@@ -1,13 +1,7 @@
 package com.hieuwu.groceriesstore.di
 
-import com.hieuwu.groceriesstore.data.repository.CategoryRepositoryImpl
-import com.hieuwu.groceriesstore.data.repository.OrderRepositoryImpl
-import com.hieuwu.groceriesstore.data.repository.ProductRepositoryImpl
-import com.hieuwu.groceriesstore.data.repository.UserRepositoryImpl
-import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
-import com.hieuwu.groceriesstore.domain.repository.OrderRepository
-import com.hieuwu.groceriesstore.domain.repository.ProductRepository
-import com.hieuwu.groceriesstore.domain.repository.UserRepository
+import com.hieuwu.groceriesstore.data.repository.*
+import com.hieuwu.groceriesstore.domain.repository.*
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -38,4 +32,8 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun bindUserRepository(impl: UserRepositoryImpl): UserRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindRecipeRepository(impl: RecipeRepositoryImpl): RecipeRepository
 }
