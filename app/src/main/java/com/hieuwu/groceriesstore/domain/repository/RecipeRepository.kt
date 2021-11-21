41e5b1048be9ebccd885364a68b92eff84b74ef6
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 13:55:48 +0700

    Add get from local method in recipe repo

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/RecipeRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/RecipeRepository.kt
@@ -2,8 +2,9 @@ package com.hieuwu.groceriesstore.domain.repository
 
 import androidx.lifecycle.LiveData
 import com.hieuwu.groceriesstore.domain.models.CategoryModel
+import com.hieuwu.groceriesstore.domain.models.RecipeModel
 
 interface RecipeRepository {
     suspend fun refreshDatabase()
-    fun getFromLocal(): LiveData<List<CategoryModel>>
+    fun getFromLocal(): LiveData<List<RecipeModel>>
 }
