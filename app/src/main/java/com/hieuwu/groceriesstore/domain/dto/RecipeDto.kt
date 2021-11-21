393b7943e8bf05a0941a09537b52389458b83d35
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 13:56:22 +0700

    Add dto and mapper

--- app/src/main/java/com/hieuwu/groceriesstore/domain/dto/RecipeDto.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/dto/RecipeDto.kt
@@ -1,9 +1,20 @@
 package com.hieuwu.groceriesstore.domain.dto
 
+import com.hieuwu.groceriesstore.domain.entities.Recipe
 import com.squareup.moshi.Json
 
-data class Recipe(
+data class RecipeDto(
     @Json(name = "id") val id: String,
     @Json(name = "thumbnail_url") val image: String,
     @Json(name = "name") val name: String,
 )
+
+fun List<RecipeDto>.asEntity(): List<Recipe> {
+    return map {
+        Recipe(
+            id = it.id,
+            name = it.name,
+            image = it.image
+        )
+    }
+}
