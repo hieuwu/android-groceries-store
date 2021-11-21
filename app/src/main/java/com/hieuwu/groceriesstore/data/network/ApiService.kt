4a2bbc226f22d761d7fa7a912a7c93ca04129db9
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 09:41:55 +0700

    Implement recipes api service

--- app/src/main/java/com/hieuwu/groceriesstore/data/network/ApiService.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/network/ApiService.kt
@@ -1,6 +1,7 @@
 package com.hieuwu.groceriesstore.data.network
 
-import com.hieuwu.groceriesstore.domain.entities.Product
+import com.hieuwu.groceriesstore.domain.dto.Recipe
+import com.hieuwu.groceriesstore.domain.dto.RecipeListResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -8,8 +9,10 @@ import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
 
-private const val BASE_URL = "https://example.com/"
+private const val BASE_URL = "https://tasty.p.rapidapi.com/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -18,17 +21,28 @@ private val moshi = Moshi.Builder()
 private val retrofit = Retrofit.Builder()
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .addConverterFactory(MoshiConverterFactory.create(moshi))
+
     .baseUrl(BASE_URL)
     .build()
 
-interface MarsApiService {
-    @GET("products")
-    fun getProducts():
-            Deferred<List<Product>>
+var rapidApiHost = "tasty.p.rapidapi.com"
+var rapidApiKey = "25623d5122mshdf7d5b25fc85d92p16d42bjsn846c1ccf5e93"
+
+interface RecipesApiService {
+    @Headers(
+        "x-rapidapi-host: tasty.p.rapidapi.com",
+        "x-rapidapi-key: 25623d5122mshdf7d5b25fc85d92p16d42bjsn846c1ccf5e93"
+    )
+    @GET("recipes/list")
+    fun getRecipesList(
+        @Query("from") from: Int = 0,
+        @Query("size") size: Int = 30,
+        @Query("tags") tags: String = "under_30_minutes",
+    ): Deferred<RecipeListResponse>
 }
 
 object Api {
-    val retrofitService: MarsApiService by lazy {
-        retrofit.create(MarsApiService::class.java)
+    val retrofitService: RecipesApiService by lazy {
+        retrofit.create(RecipesApiService::class.java)
     }
 }
