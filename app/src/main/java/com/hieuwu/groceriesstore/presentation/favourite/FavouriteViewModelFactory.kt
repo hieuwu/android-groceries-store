412b6857174102944488705ebc62d831e5b1cb52
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 16:48:32 +0700

    Create viewmodel factory for favourite

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/favourite/FavouriteViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/favourite/FavouriteViewModelFactory.kt
@@ -1,4 +1,18 @@
 package com.hieuwu.groceriesstore.presentation.favourite
 
-class FavouriteViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.groceriesstore.domain.repository.RecipeRepository
+
+
+class FavouriteViewModelFactory(
+    private val recipeRepository: RecipeRepository
+) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(FavouriteViewModel::class.java)) {
+            return FavouriteViewModel(recipeRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
