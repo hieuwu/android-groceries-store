db84726e886e369cab6498eaa14d14451683d034
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 11:53:14 +0700

    Add recipe entity

--- app/src/main/java/com/hieuwu/groceriesstore/data/GroceriesStoreDatabase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/GroceriesStoreDatabase.kt
@@ -6,7 +6,7 @@ import com.hieuwu.groceriesstore.data.dao.*
 import com.hieuwu.groceriesstore.domain.entities.*
 
 @Database(
-    entities = [Product::class, LineItem::class, Order::class, Category::class, User::class],
+    entities = [Product::class, LineItem::class, Order::class, Category::class, User::class, Recipe::class],
     version = 1,
     exportSchema = false
 )
@@ -16,5 +16,6 @@ abstract class GroceriesStoreDatabase : RoomDatabase() {
     abstract fun orderDao(): OrderDao
     abstract fun categoryDao(): CategoryDao
     abstract fun userDao(): UserDao
+    abstract fun recipeDao(): RecipeDao
 
 }
