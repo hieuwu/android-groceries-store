3c12ad0fa9985eb73916a44bcbf2c4b1f0ffdd3a
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 16:48:20 +0700

    Get data, handle item click in view model

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/favourite/FavouriteViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/favourite/FavouriteViewModel.kt
@@ -1,5 +1,9 @@
 package com.hieuwu.groceriesstore.presentation.favourite
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.hieuwu.groceriesstore.domain.models.ProductModel
+import com.hieuwu.groceriesstore.domain.models.RecipeModel
 import com.hieuwu.groceriesstore.domain.repository.RecipeRepository
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
 import javax.inject.Inject
@@ -7,5 +11,18 @@ import javax.inject.Inject
 class FavouriteViewModel @Inject constructor(private val recipeRepository: RecipeRepository) :
     ObservableViewModel() {
 
+    private var _recipesList: MutableLiveData<List<RecipeModel>> = recipeRepository.getFromLocal()
+            as MutableLiveData<List<RecipeModel>>
+    val recipesList: LiveData<List<RecipeModel>>
+        get() = _recipesList
+
+
+    private val _navigateToSelectedRecipe = MutableLiveData<RecipeModel?>()
+    val navigateToSelectedRecipe: LiveData<RecipeModel?>
+        get() = _navigateToSelectedRecipe
+
+    public fun displayRecipeDetails(recipe: RecipeModel) {
+        _navigateToSelectedRecipe.value = recipe
+    }
 
 }
