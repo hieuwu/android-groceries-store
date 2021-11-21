c988578f81b9e35aa758192d8a58b79b1a19faab
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 11:56:03 +0700

    Implement recipe repository

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/RecipeRepositoryImpl.kt
@@ -1,4 +1,27 @@
 package com.hieuwu.groceriesstore.data.repository
 
-class RecipeRepositoryImpl {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.hieuwu.groceriesstore.data.network.Api
+import com.hieuwu.groceriesstore.domain.models.CategoryModel
+import com.hieuwu.groceriesstore.domain.repository.RecipeRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class RecipeRepositoryImpl : RecipeRepository {
+    override suspend fun refreshDatabase() {
+        withContext(Dispatchers.IO) {
+            val getRecipeDeferred = Api.retrofitService.getRecipesList()
+            try {
+                var listResult = getRecipeDeferred.await().recipesList
+            } catch (t: Throwable) {
+                var message = t.message
+            }
+        }
+    }
+
+    override fun getFromLocal(): LiveData<List<CategoryModel>> {
+        //TODO NOT
+    }
 }
