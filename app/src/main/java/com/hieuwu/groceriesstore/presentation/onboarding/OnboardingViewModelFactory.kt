15e110bb69fde40286a8b6c063cd3ba45b052e97
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 11:55:45 +0700

    Add recipe repository

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/onboarding/OnboardingViewModelFactory.kt
@@ -4,14 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.domain.repository.RecipeRepository
 
 class OnboardingViewModelFactory(
-    private val productRepository: ProductRepository, private val categoryRepository: CategoryRepository
+    private val productRepository: ProductRepository, private val categoryRepository: CategoryRepository,
+    private val recipeRepository: RecipeRepository
 ) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(OnboardingViewModel::class.java)) {
-            return OnboardingViewModel(productRepository, categoryRepository) as T
+            return OnboardingViewModel(productRepository, categoryRepository, recipeRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
