8dcf51e7c9ec88eaa4e82baf9fb983a20370b259
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 13:55:16 +0700

    Create viewmodel for favourite

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/favourite/FavouriteViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/favourite/FavouriteViewModel.kt
@@ -1,7 +1,11 @@
 package com.hieuwu.groceriesstore.presentation.favourite
 
+import com.hieuwu.groceriesstore.domain.repository.RecipeRepository
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import javax.inject.Inject
 
-class FavouriteViewModel: ObservableViewModel {
+class FavouriteViewModel @Inject constructor(private val recipeRepository: RecipeRepository) :
+    ObservableViewModel() {
+
 
 }
