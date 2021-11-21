49537f575295a865dcdda0881d646f7da7b4628a
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 11:53:25 +0700

    Create entity for recipe

--- app/src/main/java/com/hieuwu/groceriesstore/domain/entities/Recipe.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/entities/Recipe.kt
@@ -1,4 +1,15 @@
 package com.hieuwu.groceriesstore.domain.entities
 
-class Recipe {
-}
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+
+data class Recipe(
+    @PrimaryKey
+    var id: String,
+
+    @ColumnInfo(name = "image")
+    val image: String,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+)
