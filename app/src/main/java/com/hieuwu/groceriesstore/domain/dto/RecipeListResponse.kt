92e3ce5af614d40805d3cc339d5908eafd2dd5c7
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 09:41:26 +0700

    Implement recipe response

--- app/src/main/java/com/hieuwu/groceriesstore/domain/dto/RecipeListResponse.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/dto/RecipeListResponse.kt
@@ -1,4 +1,8 @@
 package com.hieuwu.groceriesstore.domain.dto
 
-class RecipeListResponse {
-}
+import com.squareup.moshi.Json
+
+data class RecipeListResponse(
+    @Json(name = "count") val count: String,
+    @Json(name = "results") var recipesList: MutableList<Recipe> = mutableListOf(),
+)
