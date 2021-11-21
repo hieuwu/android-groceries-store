f530e0f06adb9007b12bbca2053640d595b8911c
Hieu Vu <star.hieu.99@gmail.com>
2021-11-21 09:41:42 +0700

    Implement recipe data class

--- app/src/main/java/com/hieuwu/groceriesstore/domain/dto/Recipe.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/dto/Recipe.kt
@@ -1,4 +1,9 @@
 package com.hieuwu.groceriesstore.domain.dto
 
-class Recipe {
-}
+import com.squareup.moshi.Json
+
+data class Recipe(
+    @Json(name = "id") val id: String,
+    @Json(name = "thumbnail_url") val image: String,
+    @Json(name = "name") val name: String,
+)
