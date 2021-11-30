51092db727dc251a49abbd351b57315164bc6ab0
Hieu Vu <star.hieu.99@gmail.com>
2021-11-30 21:37:26 +0700

    Extract data convert method to helper

--- app/src/main/java/com/hieuwu/groceriesstore/utilities/FirestoreHelper.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/utilities/FirestoreHelper.kt
@@ -1,10 +1,16 @@
 package com.hieuwu.groceriesstore.utilities
 
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.QueryDocumentSnapshot
-import com.hieuwu.groceriesstore.domain.entities.Category
-import com.hieuwu.groceriesstore.domain.entities.OrderWithLineItems
-import com.hieuwu.groceriesstore.domain.entities.Product
-import com.hieuwu.groceriesstore.domain.entities.ProductAndLineItem
+import com.hieuwu.groceriesstore.domain.entities.*
+
+
+object CollectionNames {
+    const val products = "products"
+    const val categories = "categories"
+    const val orders = "orders"
+    const val users = "users"
+}
 
 fun convertItemEntityToDocument(lineItem: ProductAndLineItem): HashMap<String, Any> {
     val document = HashMap<String, Any>()
@@ -46,4 +52,22 @@ fun convertCategoryDocumentToEntity(document: QueryDocumentSnapshot): Category {
     val name: String = document.data["name"] as String
     val image: String = document.data["image"] as String
     return Category(id, name, image)
+}
+
+fun convertUserDocumentToEntity(id: String, document: DocumentSnapshot): User {
+    val name: String = document.data?.get("name")!! as String
+    val phone: String = document.data!!["phone"]!! as String
+    val address: String = document.data!!["address"]!! as String
+    val email: String = document.data!!["email"]!! as String
+    return User(id, name, email, address, phone)
+}
+
+fun convertUserEntityToDocument(user: User): HashMap<String, String?> {
+    val document = hashMapOf(
+        "name" to user.name,
+        "email" to user.email,
+        "phone" to user.phone,
+        "address" to user.address,
+    )
+    return document
 }
