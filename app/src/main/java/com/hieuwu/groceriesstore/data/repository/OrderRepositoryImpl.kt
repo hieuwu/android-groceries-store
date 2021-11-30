9ea7c3e294301d1058625ce7773d0bf935506bf4
Hieu Vu <star.hieu.99@gmail.com>
2021-11-30 16:18:30 +0700

    Send current order to server

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/OrderRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/OrderRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.hieuwu.groceriesstore.data.repository
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.hieuwu.groceriesstore.data.dao.LineItemDao
 import com.hieuwu.groceriesstore.data.dao.OrderDao
 import com.hieuwu.groceriesstore.domain.entities.LineItem
@@ -8,6 +10,8 @@ import com.hieuwu.groceriesstore.domain.entities.OrderWithLineItems
 import com.hieuwu.groceriesstore.domain.repository.OrderRepository
 import com.hieuwu.groceriesstore.utilities.OrderStatus
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.tasks.await
+import timber.log.Timber
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import javax.inject.Inject
@@ -48,5 +52,22 @@ class OrderRepositoryImpl @Inject constructor(
     override fun getCurrentCartId(status: OrderStatus) = orderDao.getCurrentCartId(status.value)
 
     override fun getCart(status: OrderStatus) = orderDao.getCart(status.value)
-    override suspend fun getCartWithLineItems(status: OrderStatus) = orderDao.getCartWithLineItems(status.value)
+    override suspend fun getCartWithLineItems(status: OrderStatus) =
+        orderDao.getCartWithLineItems(status.value)
+
+    override suspend fun sendOrderToServer(order: OrderWithLineItems) {
+        //Convert order with line items to order on firebase
+        var orderMap = HashMap<String, Any>()
+        orderMap["address"] = order.order.address
+
+        val db = Firebase.firestore
+        db.collection("orders").document(order.order.id)
+            .set(orderMap)
+            .addOnSuccessListener {
+
+
+            }
+            .addOnFailureListener { e -> Timber.d("Error writing document%s", e) }
+            .await()
+    }
 }
