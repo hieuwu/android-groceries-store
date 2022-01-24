490e36c4e54ec01287da398cf7a419a82af0376c
Hieu Vu <star.hieu.99@gmail.com>
2022-01-24 08:38:47 +0700

    Refactor imports from domain to data

--- app/src/main/java/com/hieuwu/groceriesstore/data/entities/LineItem.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/entities/LineItem.kt
@@ -1,4 +1,4 @@
-package com.hieuwu.groceriesstore.domain.entities
+package com.hieuwu.groceriesstore.data.entities
 
 import androidx.room.*
 import com.hieuwu.groceriesstore.utilities.LINE_ITEM_TABLE
