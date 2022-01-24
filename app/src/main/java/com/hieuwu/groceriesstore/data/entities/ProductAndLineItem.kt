fc3ae37eb4d73c8047e09515098526b319a3de42
Hieu Vu <star.hieu.99@gmail.com>
2022-01-24 20:42:01 +0700

    Create object mapper for lineitem

--- app/src/main/java/com/hieuwu/groceriesstore/data/entities/ProductAndLineItem.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/entities/ProductAndLineItem.kt
@@ -2,6 +2,7 @@ package com.hieuwu.groceriesstore.data.entities
 
 import androidx.room.Embedded
 import androidx.room.Relation
+import com.hieuwu.groceriesstore.domain.models.LineItemModel
 
 
 data class ProductAndLineItem(
@@ -13,3 +14,14 @@ data class ProductAndLineItem(
     )
     val product: Product?
 )
+
+fun ProductAndLineItem.asDomainModel(): LineItemModel {
+    return LineItemModel(
+        id = null,
+        name = this.product?.name,
+        image = this.product?.image,
+        productId = this.product?.id,
+        quantity = this.lineItem?.quantity,
+        subtotal = this.lineItem?.subtotal,
+        )
+}
