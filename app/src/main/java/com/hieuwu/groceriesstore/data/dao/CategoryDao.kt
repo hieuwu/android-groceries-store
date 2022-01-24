77ab03f35ceec307a5ef86b7607ee7855726b54f
Hieu Vu <star.hieu.99@gmail.com>
2022-01-24 08:38:40 +0700

    Refactor imports from domain to data

--- app/src/main/java/com/hieuwu/groceriesstore/data/dao/CategoryDao.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/dao/CategoryDao.kt
@@ -4,7 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.hieuwu.groceriesstore.domain.entities.Category
+import com.hieuwu.groceriesstore.data.entities.Category
 import com.hieuwu.groceriesstore.utilities.CATEGORY_TABLE
 import kotlinx.coroutines.flow.Flow
 
