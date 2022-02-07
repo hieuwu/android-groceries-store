7f7ba620a04114c192969454d0185f79d076299a
Hieu Vu <star.hieu.99@gmail.com>
2022-02-07 21:52:47 +0700

    Add price to line item model

--- app/src/main/java/com/hieuwu/groceriesstore/domain/models/LineItemModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/models/LineItemModel.kt
@@ -3,6 +3,7 @@ package com.hieuwu.groceriesstore.domain.models
 data class LineItemModel(
     var id: String? = null,
     var name: String? = null,
+    var price: Double? = null,
     var image: String? = null,
     var productId: String? = null,
     var quantity: Int? = null,
