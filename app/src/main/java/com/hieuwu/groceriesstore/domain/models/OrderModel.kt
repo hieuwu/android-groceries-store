cce19af4b10affcca86cc0782ecd104bd0dfb703
Hieu Vu <star.hieu.99@gmail.com>
2022-02-07 22:10:58 +0700

    Use domain model instead of entity

--- app/src/main/java/com/hieuwu/groceriesstore/domain/models/OrderModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/models/OrderModel.kt
@@ -1,6 +1,7 @@
 package com.hieuwu.groceriesstore.domain.models
 
 data class OrderModel(
+    var id: String = "",
     var status: String? = null,
     var address: String? = null,
     val lineItemList: MutableList<LineItemModel>
