e4a3226dddde022570ba42266a65067b0d8a26e0
Hieu Vu <star.hieu.99@gmail.com>
2022-02-16 21:49:22 +0700

    Update id type in line item

--- app/src/main/java/com/hieuwu/groceriesstore/domain/models/LineItemModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/models/LineItemModel.kt
@@ -1,7 +1,7 @@
 package com.hieuwu.groceriesstore.domain.models
 
 data class LineItemModel(
-    var id: String? = null,
+    var id: Long? = null,
     var name: String? = null,
     var price: Double? = null,
     var image: String? = null,
