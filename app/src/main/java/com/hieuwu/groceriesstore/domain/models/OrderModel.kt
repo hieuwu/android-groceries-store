c1733586fe30d092a49fad19fe990cba51fafbb1
Hieu Vu <star.hieu.99@gmail.com>
2022-02-17 21:29:10 +0700

    Move calculate total to order model

--- app/src/main/java/com/hieuwu/groceriesstore/domain/models/OrderModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/models/OrderModel.kt
@@ -4,5 +4,17 @@ data class OrderModel(
     var id: String = "",
     var status: String? = null,
     var address: String? = null,
-    val lineItemList: MutableList<LineItemModel>
-)
+    val lineItemList: MutableList<LineItemModel>,
+) {
+    val total: Double
+        get() = calculateTotal()
+
+    private fun calculateTotal(): Double {
+        var sum = 0.0
+        for (item in lineItemList) {
+            val sub = item?.subtotal ?: 0.0
+            sum = sum.plus(sub)
+        }
+        return sum
+    }
+}
