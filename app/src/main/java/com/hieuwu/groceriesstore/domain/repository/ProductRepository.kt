f5412b2c25d873a95a4ae3984a40265b54c5484f
Hieu Vu <star.hieu.99@gmail.com>
2022-02-27 17:30:01 +0700

    Rename method

--- app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/repository/ProductRepository.kt
@@ -1,17 +1,16 @@
 package com.hieuwu.groceriesstore.domain.repository
 
 import androidx.lifecycle.LiveData
-import com.hieuwu.groceriesstore.data.entities.LineItem
 import com.hieuwu.groceriesstore.data.entities.Product
 import com.hieuwu.groceriesstore.domain.models.ProductModel
 import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
     val products: LiveData<List<ProductModel>>
-    fun getProductById(productId: String): Flow<Product>
+    fun getProductById(productId: String): LiveData<ProductModel>
     fun getAllProducts(): LiveData<List<ProductModel>>
     suspend fun getAllProductsByCategory(categoryId: String):LiveData<List<ProductModel>>
-    suspend fun getFromServer()
+    suspend fun refreshDatabase()
     suspend fun updateLineItemQuantityById(quantity: Int, id: Long)
     suspend fun removeLineItemById(id: Long)
     fun searchProductsListByName(name: String?) : LiveData<List<ProductModel>>
