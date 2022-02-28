42292ddfc6a3aa79a4be61a8d0ba12c63cbc5e3c
Hieu Vu <star.hieu.99@gmail.com>
2022-02-28 21:19:38 +0700

    Implement product detail use case

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProductDetailUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProductDetailUseCase.kt
@@ -1,4 +1,8 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.domain.models.ProductModel
+
 interface GetProductDetailUseCase {
+    fun getProductDetail(productId: String): LiveData<ProductModel>
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProductDetailUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProductDetailUseCaseImpl.kt
@@ -1,4 +1,10 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-class GetProductDetailUseCaseImpl : GetProductDetailUseCase {
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import javax.inject.Inject
+
+class GetProductDetailUseCaseImpl @Inject constructor(
+    private val productRepository: ProductRepository,
+) : GetProductDetailUseCase{
+    override fun getProductDetail(productId: String) = productRepository.getProductById(productId)
 }
