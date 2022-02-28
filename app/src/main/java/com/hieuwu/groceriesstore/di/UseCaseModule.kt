943e5b3a5985bc1c1f039e96e95ca45248a29858
Hieu Vu <star.hieu.99@gmail.com>
2022-02-28 21:19:16 +0700

    Implement use case module for product list and detail

--- app/src/main/java/com/hieuwu/groceriesstore/di/UseCaseModule.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/di/UseCaseModule.kt
@@ -1,4 +1,25 @@
 package com.hieuwu.groceriesstore.di
 
-class UseCaseModule {
+import com.hieuwu.groceriesstore.domain.usecases.GetProductDetailUseCase
+import com.hieuwu.groceriesstore.domain.usecases.GetProductDetailUseCaseImpl
+import com.hieuwu.groceriesstore.domain.usecases.GetProductListUseCase
+import com.hieuwu.groceriesstore.domain.usecases.GetProductListUseCaseImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class UseCaseModule {
+
+    @Singleton
+    @Binds
+    abstract fun bindGetProductListUseCase(impl: GetProductListUseCaseImpl): GetProductListUseCase
+
+    @Singleton
+    @Binds
+    abstract fun bindGetProductDetailUseCase(impl: GetProductDetailUseCaseImpl): GetProductDetailUseCase
 }
