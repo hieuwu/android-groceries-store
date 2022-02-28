75b005749ed2ab105b6024c33a85039927760015
Hieu Vu <star.hieu.99@gmail.com>
2022-02-28 21:20:59 +0700

    Remove unused imports

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/productdetail/ProductDetailViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/productdetail/ProductDetailViewModelFactory.kt
@@ -4,15 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.hieuwu.groceriesstore.domain.repository.OrderRepository
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.domain.usecases.GetProductDetailUseCase
 
 class ProductDetailViewModelFactory(
     private val id: String,
-    private val productRepository: ProductRepository,
+    private val getProductDetailUseCase: GetProductDetailUseCase,
     private val orderRepository: OrderRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ProductDetailViewModel::class.java)) {
-            return ProductDetailViewModel(id, productRepository, orderRepository) as T
+            return ProductDetailViewModel(id, getProductDetailUseCase, orderRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
