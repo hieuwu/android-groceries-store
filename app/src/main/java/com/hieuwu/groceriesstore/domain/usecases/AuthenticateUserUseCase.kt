c7a318c81bd2e2355af63791b6880e4aaa37ead3
Hieu Vu <star.hieu.99@gmail.com>
2022-03-10 23:22:17 +0700

    Implement use case for authenticate user

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/AuthenticateUserUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/AuthenticateUserUseCase.kt
@@ -1,4 +1,11 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.domain.models.UserModel
+
 interface AuthenticateUserUseCase {
+    suspend fun signIn(email: String, password: String)
+    suspend fun signOut()
+    fun getCurrentUser() : LiveData<UserModel?>
+    suspend fun updateUserProfile(userId: String, name: String, email: String, phone: String, address: String)
 }
