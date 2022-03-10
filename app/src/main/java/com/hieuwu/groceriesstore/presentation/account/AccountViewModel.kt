a39fc727f5e0053c97d101d245af3e12ef85fabd
Hieu Vu <star.hieu.99@gmail.com>
2022-03-10 23:22:36 +0700

    Replace repository with usecase

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/account/AccountViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/account/AccountViewModel.kt
@@ -4,23 +4,21 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.hieuwu.groceriesstore.domain.models.UserModel
-import com.hieuwu.groceriesstore.domain.repository.UserRepository
+import com.hieuwu.groceriesstore.domain.usecases.AuthenticateUserUseCase
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class AccountViewModel @Inject constructor(private val userRepository: UserRepository) :
+class AccountViewModel @Inject constructor(private val authenticateUserUseCase: AuthenticateUserUseCase) :
     ObservableViewModel() {
 
-    private val _user =
-        userRepository.getCurrentUser() as MutableLiveData<UserModel>
+    private val _user = authenticateUserUseCase.getCurrentUser() as MutableLiveData<UserModel?>
     val user: LiveData<UserModel?>
         get() = _user
 
-
     fun signOut() {
         viewModelScope.launch {
-            userRepository.clearUser()
+            authenticateUserUseCase.signOut()
         }
     }
 
