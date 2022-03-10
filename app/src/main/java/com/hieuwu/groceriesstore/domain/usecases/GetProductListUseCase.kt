0df1049d39b04742d47d607994d37199a43dd7b0
Hieu Vu <star.hieu.99@gmail.com>
2022-03-10 23:04:40 +0700

    Implement use case for shopping screen

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProductListUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProductListUseCase.kt
@@ -1,4 +1,14 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.data.entities.LineItem
+import com.hieuwu.groceriesstore.data.entities.Order
+import com.hieuwu.groceriesstore.domain.models.OrderModel
+import com.hieuwu.groceriesstore.domain.models.ProductModel
+
 interface GetProductListUseCase {
+    fun getProductList(): LiveData<List<ProductModel>>
+    fun getCurrentCart(): LiveData<OrderModel>?
+    suspend fun addToCart(lineItem: LineItem)
+    suspend fun createNewOrder(order: Order)
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProductListUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProductListUseCaseImpl.kt
@@ -1,4 +1,34 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-class GetProductListUseCaseImpl : GetProductListUseCase {
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.data.entities.LineItem
+import com.hieuwu.groceriesstore.data.entities.Order
+import com.hieuwu.groceriesstore.domain.models.OrderModel
+import com.hieuwu.groceriesstore.domain.models.ProductModel
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.utilities.OrderStatus
+import javax.inject.Inject
+
+class GetProductListUseCaseImpl @Inject constructor(
+    private val productRepository: ProductRepository,
+    private val orderRepository: OrderRepository
+) : GetProductListUseCase {
+
+    override fun getProductList(): LiveData<List<ProductModel>> {
+        return productRepository.products
+    }
+
+    override fun getCurrentCart(): LiveData<OrderModel>? {
+        return orderRepository.getOneOrderByStatus(OrderStatus.IN_CART)
+    }
+
+    override suspend fun addToCart(lineItem: LineItem) {
+        orderRepository.addLineItem(lineItem)
+    }
+
+    override suspend fun createNewOrder(order: Order) {
+        orderRepository.createOrUpdate(order)
+    }
+
 }
