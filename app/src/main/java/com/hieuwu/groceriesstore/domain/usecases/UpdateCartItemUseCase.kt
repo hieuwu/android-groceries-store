1922e9618fe2312195f8fed4cfb823c7dbe21dc9
Hieu Vu <star.hieu.99@gmail.com>
2022-03-11 23:40:14 +0700

    Implement update cart item use case

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/UpdateCartItemUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/UpdateCartItemUseCase.kt
@@ -1,4 +1,11 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.domain.models.LineItemModel
+import com.hieuwu.groceriesstore.domain.models.OrderModel
+
 interface UpdateCartItemUseCase {
+    suspend fun updateLineItem(lineItemModel: LineItemModel)
+    suspend fun removeLineItem(lineItemModel: LineItemModel)
+    fun getCurrentCart(): LiveData<OrderModel>?
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/UpdateCartItemUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/UpdateCartItemUseCaseImpl.kt
@@ -1,4 +1,25 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-class UpdateCartItemUseCaseImpl {
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.domain.models.LineItemModel
+import com.hieuwu.groceriesstore.domain.models.OrderModel
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.utilities.OrderStatus
+import javax.inject.Inject
+
+class UpdateCartItemUseCaseImpl @Inject constructor(
+    private val productRepository: ProductRepository,
+    private val orderRepository: OrderRepository
+) : UpdateCartItemUseCase {
+    override suspend fun updateLineItem(lineItemModel: LineItemModel) {
+        productRepository.updateLineItemQuantityById(lineItemModel.quantity!!, lineItemModel.id!!)
+    }
+
+    override suspend fun removeLineItem(lineItemModel: LineItemModel) {
+        productRepository.removeLineItemById(lineItemModel.id!!)
+    }
+    override fun getCurrentCart(): LiveData<OrderModel>? {
+        return orderRepository.getOneOrderByStatus(OrderStatus.IN_CART)
+    }
 }
