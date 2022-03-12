2376bbcfba51bb48e078bf90bff6426193e16f4d
Hieu Vu <star.hieu.99@gmail.com>
2022-03-12 13:22:17 +0700

    Implement create order use case

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/CreateOrderUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/CreateOrderUseCase.kt
@@ -1,4 +1,12 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.domain.models.OrderModel
+import com.hieuwu.groceriesstore.domain.models.UserModel
+
 interface CreateOrderUseCase {
+    suspend fun sendOrderToServer(order: OrderModel): Boolean
+    fun getCurrentUser(): LiveData<UserModel?>
+    fun setOrderAddress()
+    fun getCurrentCart(): LiveData<OrderModel>?
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/CreateOrderUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/CreateOrderUseCaseImpl.kt
@@ -1,4 +1,23 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-class CreateOrderUseCase {
+import com.hieuwu.groceriesstore.domain.models.OrderModel
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
+import com.hieuwu.groceriesstore.domain.repository.UserRepository
+import com.hieuwu.groceriesstore.utilities.OrderStatus
+import javax.inject.Inject
+
+class CreateOrderUseCaseImpl @Inject constructor(
+    private val orderRepository: OrderRepository,
+    private val userRepository: UserRepository
+) : CreateOrderUseCase {
+
+    override suspend fun sendOrderToServer(order: OrderModel) = orderRepository.sendOrderToServer(order)
+
+    override fun getCurrentUser() = userRepository.getCurrentUser()
+
+    override fun setOrderAddress() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCurrentCart() = orderRepository.getOneOrderByStatus(OrderStatus.IN_CART)
 }
