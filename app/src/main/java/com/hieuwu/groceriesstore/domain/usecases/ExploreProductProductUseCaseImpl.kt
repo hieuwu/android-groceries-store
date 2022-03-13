fa603d97d7ffc7a9b0473ef7813420999c158603
Hieu Vu <star.hieu.99@gmail.com>
2022-03-13 23:02:39 +0700

    Implement explore use case

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/ExploreProductProductUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/ExploreProductProductUseCaseImpl.kt
@@ -1,4 +1,40 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-class ExploreProductProductUseCaseImpl {
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.data.entities.LineItem
+import com.hieuwu.groceriesstore.data.entities.Order
+import com.hieuwu.groceriesstore.domain.models.CategoryModel
+import com.hieuwu.groceriesstore.domain.models.OrderModel
+import com.hieuwu.groceriesstore.domain.models.ProductModel
+import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.utilities.OrderStatus
+import javax.inject.Inject
+
+class ExploreProductProductUseCaseImpl @Inject constructor(
+    private val productRepository: ProductRepository,
+    private val orderRepository: OrderRepository,
+    private val categoryRepository: CategoryRepository
+) : ExploreProductUseCase {
+    override fun getCategoryList(): LiveData<List<CategoryModel>> =
+        categoryRepository.getFromLocal()
+
+
+    override fun searchProductByName(name: String?): LiveData<List<ProductModel>> =
+        productRepository.searchProductsListByName(name)
+
+
+    override fun getCurrentCart(): LiveData<OrderModel>? {
+        return orderRepository.getOneOrderByStatus(OrderStatus.IN_CART)
+    }
+
+    override suspend fun addToCart(lineItem: LineItem) {
+        orderRepository.addLineItem(lineItem)
+    }
+
+    override suspend fun createNewOrder(order: Order) {
+        orderRepository.createOrUpdate(order)
+    }
+
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/ExploreProductUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/ExploreProductUseCase.kt
@@ -1,4 +1,16 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.data.entities.LineItem
+import com.hieuwu.groceriesstore.data.entities.Order
+import com.hieuwu.groceriesstore.domain.models.CategoryModel
+import com.hieuwu.groceriesstore.domain.models.OrderModel
+import com.hieuwu.groceriesstore.domain.models.ProductModel
+
 interface ExploreProductUseCase {
+    fun getCategoryList(): LiveData<List<CategoryModel>>
+    fun searchProductByName(name: String?) : LiveData<List<ProductModel>>
+    fun getCurrentCart(): LiveData<OrderModel>?
+    suspend fun addToCart(lineItem: LineItem)
+    suspend fun createNewOrder(order: Order)
 }
