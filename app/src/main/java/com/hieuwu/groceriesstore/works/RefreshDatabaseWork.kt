f43d609913289e19e2ac516a5708b01434ad8361
Hieu Vu <star.hieu.99@gmail.com>
2022-03-14 23:13:06 +0700

    Replace repository with use case

--- app/src/main/java/com/hieuwu/groceriesstore/works/RefreshDatabaseWork.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/works/RefreshDatabaseWork.kt
@@ -3,23 +3,19 @@ package com.hieuwu.groceriesstore.works
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
-import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.domain.usecases.RefreshAppDataUseCase
 import timber.log.Timber
 import javax.inject.Inject
 
 class RefreshDatabaseWork(appContext: Context, params: WorkerParameters) :
     CoroutineWorker(appContext, params) {
     @Inject
-    lateinit var productRepository: ProductRepository
+    lateinit var refreshAppDataUseCase: RefreshAppDataUseCase
 
-    @Inject
-    lateinit var categoryRepository: CategoryRepository
     override suspend fun doWork(): Result {
         return try {
             Timber.d("Refreshing database")
-            productRepository.refreshDatabase()
-            categoryRepository.refreshDatabase()
+            refreshAppDataUseCase.refreshAppData()
             Result.success()
         } catch (e: Exception) {
             Result.retry()
