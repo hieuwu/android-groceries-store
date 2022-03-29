590432d65aeee53340b306d2f56f6674f14b4b5f
Hieu Vu <star.hieu.99@gmail.com>
2022-03-29 21:56:53 +0700

    Extract show message method in fragment

--- app/src/main/java/com/hieuwu/groceriesstore/utilities/FragmentExt.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/utilities/FragmentExt.kt
@@ -1,4 +1,12 @@
 package com.hieuwu.groceriesstore.utilities
 
-class FragmentExt {
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+
+fun Fragment.showMessageSnackBar(message: String) {
+    Snackbar.make(
+        requireActivity().findViewById(android.R.id.content),
+        message,
+        Snackbar.LENGTH_LONG
+    ).show()
 }
