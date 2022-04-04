aff517c0497e90db20d1ae8054b491bda9819c28
Hieu Vu <star.hieu.99@gmail.com>
2022-04-04 22:24:30 +0700

    Format code with klint

--- app/src/main/java/com/hieuwu/groceriesstore/data/GroceriesStoreDatabase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/GroceriesStoreDatabase.kt
@@ -2,8 +2,18 @@ package com.hieuwu.groceriesstore.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.hieuwu.groceriesstore.data.dao.*
-import com.hieuwu.groceriesstore.data.entities.*
+import com.hieuwu.groceriesstore.data.dao.CategoryDao
+import com.hieuwu.groceriesstore.data.dao.LineItemDao
+import com.hieuwu.groceriesstore.data.dao.OrderDao
+import com.hieuwu.groceriesstore.data.dao.ProductDao
+import com.hieuwu.groceriesstore.data.dao.RecipeDao
+import com.hieuwu.groceriesstore.data.dao.UserDao
+import com.hieuwu.groceriesstore.data.entities.Category
+import com.hieuwu.groceriesstore.data.entities.LineItem
+import com.hieuwu.groceriesstore.data.entities.Order
+import com.hieuwu.groceriesstore.data.entities.Product
+import com.hieuwu.groceriesstore.data.entities.Recipe
+import com.hieuwu.groceriesstore.data.entities.User
 
 @Database(
     entities = [Product::class, LineItem::class, Order::class, Category::class, User::class, Recipe::class],
@@ -17,5 +27,4 @@ abstract class GroceriesStoreDatabase : RoomDatabase() {
     abstract fun categoryDao(): CategoryDao
     abstract fun userDao(): UserDao
     abstract fun recipeDao(): RecipeDao
-
 }
