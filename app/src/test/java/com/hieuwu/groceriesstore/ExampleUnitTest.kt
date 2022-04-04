07c5e2a7bdd069de586e530a8ff3f56ab1ac7e10
Hieu Vu <star.hieu.99@gmail.com>
2022-04-04 22:25:57 +0700

    Format code with klint

--- app/src/test/java/com/hieuwu/groceriesstore/ExampleUnitTest.kt
+++ app/src/test/java/com/hieuwu/groceriesstore/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.hieuwu.groceriesstore
 
+import junit.framework.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
