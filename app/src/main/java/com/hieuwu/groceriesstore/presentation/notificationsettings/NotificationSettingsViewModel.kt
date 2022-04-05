76ea2f9999e1f0adb5782236c915eb428376fa82
Hieu Vu <star.hieu.99@gmail.com>
2022-04-05 21:56:58 +0700

    Inject use case and put it in viewmodel factory

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/notificationsettings/NotificationSettingsViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/notificationsettings/NotificationSettingsViewModel.kt
@@ -1,7 +1,10 @@
 package com.hieuwu.groceriesstore.presentation.notificationsettings
 
+import com.hieuwu.groceriesstore.domain.usecases.UserSettingsUseCase
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
+import javax.inject.Inject
 
-class NotificationSettingsViewModel: ObservableViewModel() {
+class NotificationSettingsViewModel @Inject constructor(private val userSettingsUseCase: UserSettingsUseCase) :
+    ObservableViewModel() {
 
 }
--- app/src/main/java/com/hieuwu/groceriesstore/presentation/notificationsettings/NotificationSettingsViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/notificationsettings/NotificationSettingsViewModelFactory.kt
@@ -2,16 +2,16 @@ package com.hieuwu.groceriesstore.presentation.notificationsettings
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.hieuwu.groceriesstore.domain.usecases.RefreshAppDataUseCase
+import com.hieuwu.groceriesstore.domain.usecases.UserSettingsUseCase
 import com.hieuwu.groceriesstore.presentation.onboarding.OnboardingViewModel
 
 class NotificationSettingsViewModelFactory(
-    private val refreshAppDataUseCase: RefreshAppDataUseCase
+    private val userSettingsUseCase: UserSettingsUseCase
 ) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(OnboardingViewModel::class.java)) {
-            return NotificationSettingsViewModel() as T
+            return NotificationSettingsViewModel(userSettingsUseCase) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
