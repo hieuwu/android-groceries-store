f11e0fca32a3d3515531d431370d1071bc0e5dbb
Hieu Vu <star.hieu.99@gmail.com>
2022-04-06 22:22:57 +0700

    Update fields for user model

--- app/src/main/java/com/hieuwu/groceriesstore/domain/models/UserModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/models/UserModel.kt
@@ -1,3 +1,12 @@
 package com.hieuwu.groceriesstore.domain.models
 
-data class UserModel(val id: String, val name: String? = null, val email: String?, val phone: String, val address: String)
+data class UserModel(
+    val id: String,
+    val name: String? = null,
+    val email: String?,
+    val phone: String,
+    val address: String,
+    var isOrderCreatedNotiEnabled: Boolean,
+    var isPromotionNotiEnabled: Boolean,
+    var isDataRefreshedNotiEnabled: Boolean
+)
