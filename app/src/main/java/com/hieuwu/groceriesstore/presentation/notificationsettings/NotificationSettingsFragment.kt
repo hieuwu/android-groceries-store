0e92584fe737599d425ded70c311ffad8b3ac3b0
Hieu Vu <star.hieu.99@gmail.com>
2022-04-16 23:31:37 +0700

    Add event listener

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/notificationsettings/NotificationSettingsFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/notificationsettings/NotificationSettingsFragment.kt
@@ -14,36 +14,15 @@ import com.hieuwu.groceriesstore.domain.usecases.UserSettingsUseCase
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NotificationSettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 @AndroidEntryPoint
 class NotificationSettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private lateinit var binding: FragmentNotificationSettingsBinding
     private lateinit var viewModel: NotificationSettingsViewModel
 
-
     @Inject
     lateinit var userSettingsUseCase: UserSettingsUseCase
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -65,6 +44,18 @@ class NotificationSettingsFragment : Fragment() {
     private fun setEventListeners() {
         binding.toolbar.setNavigationOnClickListener {
             findNavController().navigateUp()
+        }
+
+        binding.switchDatabaseRefreshDone.setOnCheckedChangeListener { _, isChecked ->
+            viewModel.isDatabaseRefreshedNotiEnabled = isChecked
+        }
+
+        binding.switchOrderCreated.setOnCheckedChangeListener { _, isChecked ->
+            viewModel.isOrderCreatedNotiEnabled = isChecked
+        }
+
+        binding.switchPromotion.setOnCheckedChangeListener { _, isChecked ->
+            viewModel.isPromotionNotiEnabled = isChecked
         }
 
         binding.toolbar.setOnMenuItemClickListener { item ->
