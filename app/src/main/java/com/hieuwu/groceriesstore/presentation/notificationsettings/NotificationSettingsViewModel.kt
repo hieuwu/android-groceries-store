579f0232f028ddc84da98fa53f2f7fe125058871
Hieu Vu <star.hieu.99@gmail.com>
2022-04-16 23:31:30 +0700

    Remove bindings

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/notificationsettings/NotificationSettingsViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/notificationsettings/NotificationSettingsViewModel.kt
@@ -4,55 +4,49 @@ import androidx.databinding.Bindable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.hieuwu.groceriesstore.BR
 import com.hieuwu.groceriesstore.domain.models.UserModel
 import com.hieuwu.groceriesstore.domain.usecases.AuthenticateUserUseCase
 import com.hieuwu.groceriesstore.domain.usecases.UserSettingsUseCase
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 class NotificationSettingsViewModel @Inject constructor(private val userSettingsUseCase: UserSettingsUseCase) :
     ObservableViewModel() {
-    @Inject
-    lateinit var authenticateUserUseCase: AuthenticateUserUseCase
-
-    private val _user =
-        authenticateUserUseCase.getCurrentUser() as MutableLiveData<UserModel?>
-    val user: LiveData<UserModel?>
-        get() = _user
+//    @Inject
+//    lateinit var authenticateUserUseCase: AuthenticateUserUseCase
+//
+//    private val _user =
+//        authenticateUserUseCase.getCurrentUser() as MutableLiveData<UserModel?>
+//    val user: LiveData<UserModel?>
+//        get() = _user
 
     private var _isDatabaseRefreshedNotiEnabled: Boolean? = null
     var isDatabaseRefreshedNotiEnabled: Boolean?
-        @Bindable
         get() {
             return _isDatabaseRefreshedNotiEnabled
         }
         set(value) {
             _isDatabaseRefreshedNotiEnabled = value
-            notifyPropertyChanged(BR.isDatabaseRefreshedNotiEnabled)
         }
 
     private var _isOrderCreatedNotiEnabled: Boolean? = null
     var isOrderCreatedNotiEnabled: Boolean?
-        @Bindable
         get() {
             return _isOrderCreatedNotiEnabled
         }
         set(value) {
             _isOrderCreatedNotiEnabled = value
-            notifyPropertyChanged(BR.isOrderCreatedNotiEnabled)
         }
 
     private var _isPromotionNotiEnabled: Boolean? = null
     var isPromotionNotiEnabled: Boolean?
-        @Bindable
         get() {
             return _isPromotionNotiEnabled
         }
         set(value) {
             _isPromotionNotiEnabled = value
-            notifyPropertyChanged(BR.isPromotionNotiEnabled)
         }
 
     init {
@@ -62,12 +56,13 @@ class NotificationSettingsViewModel @Inject constructor(private val userSettings
     }
 
     fun updateNotificationSettings() {
+        Timber.d("promo: $isPromotionNotiEnabled, ord: $isOrderCreatedNotiEnabled, db: $isDatabaseRefreshedNotiEnabled")
         viewModelScope.launch {
-            userSettingsUseCase.updateUserSettings(
-                user.value?.id!!,
-                isOrderCreatedNotiEnabled!!, isDatabaseRefreshedNotiEnabled
-                !!, isPromotionNotiEnabled!!
-            )
+//            userSettingsUseCase.updateUserSettings(
+//                user.value?.id!!,
+//                isOrderCreatedNotiEnabled!!, isDatabaseRefreshedNotiEnabled
+//                !!, isPromotionNotiEnabled!!
+//            )
         }
     }
 
