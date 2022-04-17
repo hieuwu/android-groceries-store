302b06e024684f4de1c4c71eb2e6331641dfcace
Hieu Vu <star.hieu.99@gmail.com>
2022-04-17 23:05:14 +0700

    Add auth use case

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/notificationsettings/NotificationSettingsViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/notificationsettings/NotificationSettingsViewModelFactory.kt
@@ -2,15 +2,17 @@ package com.hieuwu.groceriesstore.presentation.notificationsettings
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.groceriesstore.domain.usecases.AuthenticateUserUseCase
 import com.hieuwu.groceriesstore.domain.usecases.UserSettingsUseCase
 
 class NotificationSettingsViewModelFactory(
-    private val userSettingsUseCase: UserSettingsUseCase
+    private val userSettingsUseCase: UserSettingsUseCase,
+    private val authenticateUserUseCase: AuthenticateUserUseCase
 ) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(NotificationSettingsViewModel::class.java)) {
-            return NotificationSettingsViewModel(userSettingsUseCase) as T
+            return NotificationSettingsViewModel(userSettingsUseCase, authenticateUserUseCase) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
