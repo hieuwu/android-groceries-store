d9874775d2775fae9d31a5c497dc3e77c3121703
Hieu Vu <star.hieu.99@gmail.com>
2022-04-30 18:44:24 +0700

    Implement show notification by style

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/notification/NotificationUtils.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/notification/NotificationUtils.kt
@@ -1,4 +1,62 @@
 package com.hieuwu.groceriesstore.presentation.notification
 
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
+import androidx.core.app.NotificationCompat
+import com.hieuwu.groceriesstore.R
+
+enum class NotificationType {
+    ORDER_CREATED,
+    DATABASE_SYNCED,
+    PROMOTION_SENT,
+}
+
+private val NOTIFICATION_ID = 0
+
+
 class NotificationUtils {
+    private var notificationBuilder: NotificationCompat.Builder? = null
+
+    private fun createStyleByType(
+        type: NotificationType, applicationContext: Context
+    ): NotificationCompat.Style {
+
+        when (type) {
+            NotificationType.PROMOTION_SENT -> {
+                val bannerImage =
+                    BitmapFactory.decodeResource(applicationContext.resources, R.drawable.banner)
+
+                return NotificationCompat.BigPictureStyle().bigPicture(bannerImage)
+                    .bigLargeIcon(null)
+            }
+            NotificationType.DATABASE_SYNCED -> {
+                return NotificationCompat.BigTextStyle().bigText("Some big text")
+                    .setSummaryText("This is a very log text summary for notification")
+            }
+            NotificationType.ORDER_CREATED -> {
+                return NotificationCompat.InboxStyle().addLine("Line")
+                    .setBigContentTitle("big conent tittle")
+                    .setSummaryText("Summary text")
+            }
+        }
+    }
+
+    private fun NotificationManager.buildNotification(
+        type: NotificationType,
+        messageBody: String,
+        applicationContext: Context
+    ) {
+        val style = createStyleByType(type, applicationContext)
+        notificationBuilder = NotificationCompat.Builder(
+            applicationContext,
+            applicationContext.getString(R.string.order_created_notification_channel_id)
+        )
+            .setSmallIcon(R.drawable.ic_app_notification)
+            .setContentText(applicationContext.getString(R.string.notification_title))
+            .setContentText(messageBody)
+            .setStyle(style)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        notify(NOTIFICATION_ID, notificationBuilder?.build())
+    }
 }
