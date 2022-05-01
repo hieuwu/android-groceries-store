49e45477f3e251c45643caa54b71d150bed22a87
Hieu Vu <star.hieu.99@gmail.com>
2022-05-01 13:07:40 +0700

    Reformat code

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/notification/NotificationUtils.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/notification/NotificationUtils.kt
@@ -14,49 +14,44 @@ enum class NotificationType {
 
 private val NOTIFICATION_ID = 0
 
+private fun createStyleByType(
+    type: NotificationType, applicationContext: Context
+): NotificationCompat.Style {
 
-class NotificationUtils {
-    private var notificationBuilder: NotificationCompat.Builder? = null
+    when (type) {
+        NotificationType.PROMOTION_SENT -> {
+            val bannerImage =
+                BitmapFactory.decodeResource(applicationContext.resources, R.drawable.banner)
 
-    private fun createStyleByType(
-        type: NotificationType, applicationContext: Context
-    ): NotificationCompat.Style {
-
-        when (type) {
-            NotificationType.PROMOTION_SENT -> {
-                val bannerImage =
-                    BitmapFactory.decodeResource(applicationContext.resources, R.drawable.banner)
-
-                return NotificationCompat.BigPictureStyle().bigPicture(bannerImage)
-                    .bigLargeIcon(null)
-            }
-            NotificationType.DATABASE_SYNCED -> {
-                return NotificationCompat.BigTextStyle().bigText("Some big text")
-                    .setSummaryText("This is a very log text summary for notification")
-            }
-            NotificationType.ORDER_CREATED -> {
-                return NotificationCompat.InboxStyle().addLine("Line")
-                    .setBigContentTitle("big conent tittle")
-                    .setSummaryText("Summary text")
-            }
+            return NotificationCompat.BigPictureStyle().bigPicture(bannerImage)
+                .bigLargeIcon(null)
+        }
+        NotificationType.DATABASE_SYNCED -> {
+            return NotificationCompat.BigTextStyle().bigText("Some big text")
+                .setSummaryText("This is a very log text summary for notification")
+        }
+        NotificationType.ORDER_CREATED -> {
+            return NotificationCompat.InboxStyle().addLine("Line")
+                .setBigContentTitle("big conent tittle")
+                .setSummaryText("Summary text")
         }
     }
+}
 
-    private fun NotificationManager.buildNotification(
-        type: NotificationType,
-        messageBody: String,
-        applicationContext: Context
-    ) {
-        val style = createStyleByType(type, applicationContext)
-        notificationBuilder = NotificationCompat.Builder(
-            applicationContext,
-            applicationContext.getString(R.string.order_created_notification_channel_id)
-        )
-            .setSmallIcon(R.drawable.ic_app_notification)
-            .setContentText(applicationContext.getString(R.string.notification_title))
-            .setContentText(messageBody)
-            .setStyle(style)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-        notify(NOTIFICATION_ID, notificationBuilder?.build())
-    }
+fun NotificationManager.buildNotification(
+    type: NotificationType,
+    messageBody: String,
+    applicationContext: Context
+) {
+    val style = createStyleByType(type, applicationContext)
+    val notificationBuilder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.order_created_notification_channel_id)
+    )
+        .setSmallIcon(R.drawable.ic_app_notification)
+        .setContentText(applicationContext.getString(R.string.notification_title))
+        .setContentText(messageBody)
+        .setStyle(style)
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+    notify(NOTIFICATION_ID, notificationBuilder.build())
 }
