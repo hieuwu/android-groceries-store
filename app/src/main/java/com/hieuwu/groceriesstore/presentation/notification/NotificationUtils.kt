e160acb6c531daeee640f60bf733577731facf91
Hieu Vu <star.hieu.99@gmail.com>
2022-05-02 15:29:29 +0700

    Refactor method

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/notification/NotificationUtils.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/notification/NotificationUtils.kt
@@ -1,8 +1,11 @@
 package com.hieuwu.groceriesstore.presentation.notification
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.hieuwu.groceriesstore.R
 
@@ -37,7 +40,7 @@ private fun createStyleByType(
     }
 }
 
-private fun getChannelIdByType(type: NotificationType): Int = when (type) {
+private fun getChannelIdByType(type: NotificationType) = when (type) {
     NotificationType.PROMOTION_SENT -> {
         R.string.promotion_sent_notification_channel_id
     }
@@ -48,6 +51,87 @@ private fun getChannelIdByType(type: NotificationType): Int = when (type) {
         R.string.order_created_notification_channel_id
     }
 }
+
+
+private fun getChannelNameByType(type: NotificationType) = when (type) {
+    NotificationType.PROMOTION_SENT -> {
+        R.string.promotion_sent_notification_channel_name
+    }
+    NotificationType.DATABASE_SYNCED -> {
+        R.string.database_synced_notification_channel_name
+    }
+    NotificationType.ORDER_CREATED -> {
+        R.string.order_created_notification_channel_name
+    }
+}
+
+private fun createChannelByType(
+    type: NotificationType,
+    applicationContext: Context
+): NotificationChannel? {
+    val channelId = applicationContext.getString(getChannelIdByType(type))
+    val channelName = applicationContext.getString(getChannelNameByType(type))
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        when (type) {
+            NotificationType.PROMOTION_SENT -> {
+                val notificationChannel = NotificationChannel(
+                    channelId, channelName,
+                    NotificationManager.IMPORTANCE_HIGH
+                )
+                with(notificationChannel) {
+                    description = "Time for breakfast"
+                    lightColor = Color.RED
+                    enableVibration(true)
+                    enableLights(true)
+                    setShowBadge(false)
+                }
+                return notificationChannel
+            }
+            NotificationType.DATABASE_SYNCED -> {
+                val notificationChannel = NotificationChannel(
+                    channelId, channelName,
+                    NotificationManager.IMPORTANCE_HIGH
+                )
+                with(notificationChannel) {
+                    description = "Time for breakfast"
+                    lightColor = Color.RED
+                    enableVibration(true)
+                    enableLights(true)
+                    setShowBadge(false)
+                }
+                return notificationChannel
+
+            }
+            NotificationType.ORDER_CREATED -> {
+                val notificationChannel = NotificationChannel(
+                    channelId, channelName,
+                    NotificationManager.IMPORTANCE_HIGH
+                )
+                with(notificationChannel) {
+                    description = "Time for breakfast"
+                    lightColor = Color.RED
+                    enableVibration(true)
+                    enableLights(true)
+                    setShowBadge(false)
+                }
+                return notificationChannel
+            }
+        }
+    }
+    return null
+}
+
+fun createNotificationChannel(type: NotificationType, applicationContext: Context) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channel = createChannelByType(type, applicationContext)
+        channel?.let {
+            val notificationManager =
+                applicationContext.getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(channel)
+        }
+    }
+}
+
 
 fun NotificationManager.buildNotification(
     type: NotificationType,
