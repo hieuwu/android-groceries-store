b054f432763a93a441fa73a94ee93786f9c25bce
Hieu Vu <star.hieu.99@gmail.com>
2022-10-14 15:36:12 +0700

    Implement get profile use case

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProfileUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProfileUseCase.kt
@@ -1,4 +1,9 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-interface GetProfileUseCase {
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.domain.models.UserModel
+
+interface GetProfileUseCase:UseCase<GetProfileUseCase.Input, GetProfileUseCase.Output> {
+    class Input
+    open class Output(val result: LiveData<UserModel?>)
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProfileUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProfileUseCaseImpl.kt
@@ -1,4 +1,12 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-class GetProfileUseCaseImpl {
+import com.hieuwu.groceriesstore.domain.repository.UserRepository
+import javax.inject.Inject
+
+class GetProfileUseCaseImpl @Inject constructor(private val userRepository: UserRepository) :
+    GetProfileUseCase {
+    override suspend fun execute(input: GetProfileUseCase.Input): GetProfileUseCase.Output {
+        val user = userRepository.getCurrentUser()
+        return GetProfileUseCase.Output(user)
+    }
 }
