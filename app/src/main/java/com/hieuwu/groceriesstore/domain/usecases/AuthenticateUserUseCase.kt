bf1ff23ec35c94ddc27aa59dd7d5ebfa67faa0d7
Hieu Vu <star.hieu.99@gmail.com>
2022-10-14 15:36:02 +0700

    Comment out unused code

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/AuthenticateUserUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/AuthenticateUserUseCase.kt
@@ -5,7 +5,7 @@ import com.hieuwu.groceriesstore.domain.models.UserModel
 
 interface AuthenticateUserUseCase {
     suspend fun signIn(email: String, password: String)
-    suspend fun signOut()
-    fun getCurrentUser(): LiveData<UserModel?>
+//    suspend fun signOut()
+//    fun getCurrentUser(): LiveData<UserModel?>
     suspend fun updateUserProfile(userId: String, name: String, email: String, phone: String, address: String)
 }
