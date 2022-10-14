15afa3f8f2ea90236136f63f476ddd27599723ec
Hieu Vu <star.hieu.99@gmail.com>
2022-10-14 17:31:08 +0700

    Implement submit order use case

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SubmitOrderUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SubmitOrderUseCase.kt
@@ -1,4 +1,8 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-interface SubmitOrderUseCase {
+import com.hieuwu.groceriesstore.domain.models.OrderModel
+
+interface SubmitOrderUseCase : UseCase<SubmitOrderUseCase.Input, SubmitOrderUseCase.Output> {
+    class Input(val order: OrderModel)
+    data class Output(val result: Boolean)
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SubmitOrderUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SubmitOrderUseCaseImpl.kt
@@ -1,4 +1,13 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-class SubmitOrderUseCaseImpl {
+import com.hieuwu.groceriesstore.domain.repository.OrderRepository
+import javax.inject.Inject
+
+class SubmitOrderUseCaseImpl @Inject constructor(
+    private val orderRepository: OrderRepository,
+) : SubmitOrderUseCase {
+    override suspend fun execute(input: SubmitOrderUseCase.Input): SubmitOrderUseCase.Output {
+        val result = orderRepository.sendOrderToServer(input.order)
+        return SubmitOrderUseCase.Output(result)
+    }
 }
