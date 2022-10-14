3a9aedeed2194d58c8eac3e3d5a2465d75734d8f
Hieu Vu <star.hieu.99@gmail.com>
2022-10-14 18:18:56 +0700

    Implement get categories use case

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetCategoriesListUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetCategoriesListUseCase.kt
@@ -1,4 +1,11 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-interface GetCategoriesList {
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.domain.models.CategoryModel
+
+interface GetCategoriesListUseCase :
+    UseCase<GetCategoriesListUseCase.Input, GetCategoriesListUseCase.Output> {
+    class Input
+
+    class Output(val result: LiveData<List<CategoryModel>>)
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetCategoriesListUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetCategoriesListUseCaseImpl.kt
@@ -1,4 +1,13 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-class GetCategoriesListUseCaseImpl {
+import com.hieuwu.groceriesstore.domain.repository.CategoryRepository
+import javax.inject.Inject
+
+class GetCategoriesListUseCaseImpl @Inject constructor(
+    private val categoryRepository: CategoryRepository
+) : GetCategoriesListUseCase {
+    override suspend fun execute(input: GetCategoriesListUseCase.Input): GetCategoriesListUseCase.Output {
+        val result = categoryRepository.getFromLocal()
+        return GetCategoriesListUseCase.Output(result)
+    }
 }
