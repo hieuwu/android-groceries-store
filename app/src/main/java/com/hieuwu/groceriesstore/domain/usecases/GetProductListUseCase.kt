28d40ee96de402e62799a4ab6fe057f25f7f36d9
Hieu Vu <star.hieu.99@gmail.com>
2022-10-14 21:29:49 +0700

    Mark as deprecated

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProductListUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetProductListUseCase.kt
@@ -6,10 +6,20 @@ import com.hieuwu.groceriesstore.domain.models.OrderModel
 import com.hieuwu.groceriesstore.domain.models.ProductModel
 import kotlinx.coroutines.flow.Flow
 
+@Deprecated("Use use case")
 interface GetProductListUseCase {
+    @Deprecated("Use GetProductsListUseCase")
     fun getProductList(): Flow<List<ProductModel>>
+
+    @Deprecated("Use GetCurrentCartUseCase")
     fun getCurrentCart(): Flow<OrderModel?>
+
+    @Deprecated("Use AddToCardUseCase")
     suspend fun addToCart(lineItem: LineItem)
+
+    @Deprecated("Use CreateNewOrderUseCase")
     suspend fun createNewOrder(order: Order)
+
+    @Deprecated("Use GetProductsByCategoryUseCase")
     fun getAllProductsByCategory(categoryId: String): Flow<List<ProductModel>>
 }
