3cae42e97149c275c36976b03c7bd6f03979d75f
Hieu Vu <star.hieu.99@gmail.com>
2022-10-14 15:36:31 +0700

    Apply new signout use case

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/account/AccountViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/account/AccountViewModel.kt
@@ -5,13 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.hieuwu.groceriesstore.domain.models.UserModel
 import com.hieuwu.groceriesstore.domain.usecases.AuthenticateUserUseCase
+import com.hieuwu.groceriesstore.domain.usecases.SignOutUseCase
 import com.hieuwu.groceriesstore.presentation.utils.ObservableViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.launch
 
 @HiltViewModel
-class AccountViewModel @Inject constructor(private val authenticateUserUseCase: AuthenticateUserUseCase) :
+class AccountViewModel @Inject constructor(private val authenticateUserUseCase: AuthenticateUserUseCase,
+private val signOutUseCase: SignOutUseCase) :
     ObservableViewModel() {
 
     private val _user = authenticateUserUseCase.getCurrentUser() as MutableLiveData<UserModel?>
@@ -20,7 +22,7 @@ class AccountViewModel @Inject constructor(private val authenticateUserUseCase: 
 
     fun signOut() {
         viewModelScope.launch {
-            authenticateUserUseCase.signOut()
+            signOutUseCase.execute(SignOutUseCase.Input())
         }
     }
 }
