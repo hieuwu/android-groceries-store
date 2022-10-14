634a64d9f7272a4df7ff2b893c8be94df7eaf785
Hieu Vu <star.hieu.99@gmail.com>
2022-10-14 16:06:39 +0700

    Implement update profile use case

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/UpdateProfileUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/UpdateProfileUseCaseImpl.kt
@@ -1,4 +1,14 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-class UpdateProfileUseCaseImpl {
+import com.hieuwu.groceriesstore.domain.repository.UserRepository
+import javax.inject.Inject
+
+class UpdateProfileUseCaseImpl @Inject constructor(private val userRepository: UserRepository) :
+    UpdateProfileUseCase {
+    override suspend fun execute(input: UpdateProfileUseCase.Input): UpdateProfileUseCase.Output {
+        userRepository.updateUserProfile(
+            input.userId, input.name, input.email, input.phone, input.address
+        )
+        return UpdateProfileUseCase.Output()
+    }
 }
