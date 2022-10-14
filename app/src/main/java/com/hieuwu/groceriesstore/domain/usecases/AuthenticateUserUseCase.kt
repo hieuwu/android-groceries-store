834de1db328d3880f81c1f8229f0144b02fd985e
Hieu Vu <star.hieu.99@gmail.com>
2022-10-14 16:07:34 +0700

    Mark as derpecated

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/AuthenticateUserUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/AuthenticateUserUseCase.kt
@@ -3,9 +3,10 @@ package com.hieuwu.groceriesstore.domain.usecases
 import androidx.lifecycle.LiveData
 import com.hieuwu.groceriesstore.domain.models.UserModel
 
+@Deprecated("Use simple usecase instead")
 interface AuthenticateUserUseCase {
     suspend fun signIn(email: String, password: String)
 //    suspend fun signOut()
-//    fun getCurrentUser(): LiveData<UserModel?>
+    fun getCurrentUser(): LiveData<UserModel?>
     suspend fun updateUserProfile(userId: String, name: String, email: String, phone: String, address: String)
 }
