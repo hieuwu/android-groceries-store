be69431fc00be66d2f0c5619b67c4533978a3ecc
Hieu Vu <star.hieu.99@gmail.com>
2022-10-14 18:20:14 +0700

    Implement search product use cas

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SearchProductUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SearchProductUseCase.kt
@@ -1,4 +1,9 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-interface SearchProductUseCase {
+import androidx.lifecycle.LiveData
+import com.hieuwu.groceriesstore.domain.models.ProductModel
+
+interface SearchProductUseCase : UseCase<SearchProductUseCase.Input, SearchProductUseCase.Output> {
+    class Input(val name: String? = null)
+    class Output(result: LiveData<List<ProductModel>>)
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SearchProductUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SearchProductUseCaseImpl.kt
@@ -1,4 +1,12 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-class SearchProductUseCaseImpl {
+import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import javax.inject.Inject
+
+class SearchProductUseCaseImpl @Inject constructor(private val productRepository: ProductRepository) :
+    SearchProductUseCase {
+    override suspend fun execute(input: SearchProductUseCase.Input): SearchProductUseCase.Output {
+        val result = productRepository.searchProductsListByName(input.name)
+        return SearchProductUseCase.Output(result)
+    }
 }
