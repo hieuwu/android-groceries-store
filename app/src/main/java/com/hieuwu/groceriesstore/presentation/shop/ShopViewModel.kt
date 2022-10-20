d9f889dd7fc4bf19ffaf185c206f503503ee7180
Hieu Vu <star.hieu.99@gmail.com>
2022-10-20 21:28:23 +0700

    Migrate to new use cases

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/ShopViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/ShopViewModel.kt
@@ -6,32 +6,46 @@ import com.hieuwu.groceriesstore.data.entities.LineItem
 import com.hieuwu.groceriesstore.data.entities.Order
 import com.hieuwu.groceriesstore.domain.models.OrderModel
 import com.hieuwu.groceriesstore.domain.models.ProductModel
+import com.hieuwu.groceriesstore.domain.usecases.AddToCartUseCase
+import com.hieuwu.groceriesstore.domain.usecases.CreateNewOrderUseCase
+import com.hieuwu.groceriesstore.domain.usecases.GetCurrentCartUseCase
 import com.hieuwu.groceriesstore.domain.usecases.GetProductListUseCase
+import com.hieuwu.groceriesstore.domain.usecases.GetProductsListUseCase
 import com.hieuwu.groceriesstore.utilities.OrderStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
-import java.util.UUID
+import java.util.*
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.stateIn
-import javax.inject.Inject
 import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+private const val STOP_TIMEOUT = 5000L
 
 @HiltViewModel
 class ShopViewModel @Inject constructor(
-    private val getProductListUseCase: GetProductListUseCase
+    private val getProductListUseCase: GetProductListUseCase,
+    private val getProductsListUseCase: GetProductsListUseCase,
+    private val getCurrentCartUseCase: GetCurrentCartUseCase,
+    private val addToCartUseCase: AddToCartUseCase,
+    private val createNewOrderUseCase: CreateNewOrderUseCase
 ) : ViewModel() {
     val productList: StateFlow<List<ProductModel>> =
-        getProductListUseCase.getProductList()
-            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+        getProductLists()?.stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(STOP_TIMEOUT),
+            emptyList()
+        )!!
 
     private val _navigateToSelectedProperty = MutableStateFlow<ProductModel?>(null)
     val navigateToSelectedProperty: StateFlow<ProductModel?>
         get() = _navigateToSelectedProperty.asStateFlow()
 
-    var currentCart: StateFlow<OrderModel?> = getProductListUseCase.getCurrentCart()
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), null)
+    var currentCart: StateFlow<OrderModel?> = getCurrentCart()
+        ?.stateIn(viewModelScope, SharingStarted.WhileSubscribed(STOP_TIMEOUT), null)!!
 
     fun displayPropertyDetails(marsProperty: ProductModel) {
         _navigateToSelectedProperty.value = marsProperty
@@ -41,19 +55,55 @@ class ShopViewModel @Inject constructor(
         _navigateToSelectedProperty.value = null
     }
 
+    private fun getProductLists(): Flow<List<ProductModel>>? {
+        var res: Flow<List<ProductModel>>? = null
+        viewModelScope.launch {
+            res = getProductsListUseCase.execute(GetProductsListUseCase.Input()).result
+        }
+        return res
+    }
+
+    private fun getCurrentCart(): Flow<OrderModel?>? {
+        var res: Flow<OrderModel?>? = null
+        viewModelScope.launch {
+            res = getCurrentCartUseCase.execute(GetCurrentCartUseCase.Input()).result
+        }
+        return res
+    }
+
     fun addToCart(product: ProductModel) {
         if (currentCart.value != null) {
             // Add to cart
             val cartId = currentCart.value!!.id
             viewModelScope.launch {
-                getProductListUseCase
-                    .addToCart(LineItem(product.id, cartId, 1, product.price!!))
+                addToCartUseCase.execute(
+                    AddToCartUseCase.Input(
+                        LineItem(
+                            product.id,
+                            cartId,
+                            1,
+                            product.price!!
+                        )
+                    )
+                )
             }
         } else {
             val id = UUID.randomUUID().toString()
             viewModelScope.launch {
-                getProductListUseCase.createNewOrder(Order(id, OrderStatus.IN_CART.value, ""))
-                getProductListUseCase.addToCart(LineItem(product.id, id, 1, product.price!!))
+                createNewOrderUseCase.execute(
+                    CreateNewOrderUseCase.Input(
+                        Order(
+                            id,
+                            OrderStatus.IN_CART.value,
+                            ""
+                        )
+                    )
+                )
+                addToCartUseCase.execute(
+                    AddToCartUseCase.Input(
+                        LineItem(product.id, id, 1, product.price!!)
+                    )
+                )
             }
         }
     }
