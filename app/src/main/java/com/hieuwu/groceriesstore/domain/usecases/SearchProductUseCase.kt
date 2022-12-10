66e898230b7734d1082c408531682d81b28d1513
Hieu Vu <star.hieu.99@gmail.com>
2022-12-10 22:01:52 +0700

    Update property

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SearchProductUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SearchProductUseCase.kt
@@ -5,5 +5,5 @@ import com.hieuwu.groceriesstore.domain.models.ProductModel
 
 interface SearchProductUseCase : UseCase<SearchProductUseCase.Input, SearchProductUseCase.Output> {
     class Input(val name: String? = null)
-    class Output(result: LiveData<List<ProductModel>>)
+    class Output(val result: LiveData<List<ProductModel>>)
 }
