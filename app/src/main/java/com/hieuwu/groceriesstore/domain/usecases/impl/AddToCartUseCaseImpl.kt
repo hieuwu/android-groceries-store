3dfab487a8dbb7f1789eca6ee8387ee08c160b61
Hieu Vu <star.hieu.99@gmail.com>
2022-12-16 22:44:25 +0700

    Move to impl

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/AddToCartUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/AddToCartUseCaseImpl.kt
@@ -1,6 +1,7 @@
-package com.hieuwu.groceriesstore.domain.usecases
+package com.hieuwu.groceriesstore.domain.usecases.impl
 
 import com.hieuwu.groceriesstore.domain.repository.OrderRepository
+import com.hieuwu.groceriesstore.domain.usecases.AddToCartUseCase
 import javax.inject.Inject
 
 class AddToCartUseCaseImpl @Inject constructor(private val orderRepository: OrderRepository) :
