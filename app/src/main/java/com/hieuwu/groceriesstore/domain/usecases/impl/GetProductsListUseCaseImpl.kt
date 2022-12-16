c3f2925f2c61a5ccaf04f1ade94e327543c61934
Hieu Vu <star.hieu.99@gmail.com>
2022-12-16 22:44:10 +0700

    Move to impl

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetProductsListUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetProductsListUseCaseImpl.kt
@@ -1,6 +1,7 @@
-package com.hieuwu.groceriesstore.domain.usecases
+package com.hieuwu.groceriesstore.domain.usecases.impl
 
 import com.hieuwu.groceriesstore.domain.repository.ProductRepository
+import com.hieuwu.groceriesstore.domain.usecases.GetProductsListUseCase
 import javax.inject.Inject
 
 class GetProductsListUseCaseImpl @Inject constructor(private val productRepository: ProductRepository) :
