2d28f15f93e04af8880d71140876c1e058266b81
Hieu Vu <star.hieu.99@gmail.com>
2022-12-20 22:43:14 +0700

    Handle error

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SignInUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/SignInUseCase.kt
@@ -2,5 +2,8 @@ package com.hieuwu.groceriesstore.domain.usecases
 
 interface SignInUseCase : UseCase<SignInUseCase.Input, SignInUseCase.Output> {
     data class Input(val email: String, val password: String)
-    open class Output(val result: Boolean)
+    open class Output(val result: Boolean) {
+        sealed class Error : Output(false)
+        object AccountNotExistedError : Output(false)
+    }
 }
