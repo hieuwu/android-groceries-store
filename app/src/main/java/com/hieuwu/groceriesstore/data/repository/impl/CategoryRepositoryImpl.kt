f5eb70cdeedf173e58824c67288337305509a87c
Hieu Vu <star.hieu.99@gmail.com>
2022-12-21 23:06:44 +0700

    Replace live data with flow

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/CategoryRepositoryImpl.kt
@@ -1,7 +1,5 @@
 package com.hieuwu.groceriesstore.data.repository.impl
 
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.asLiveData
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.hieuwu.groceriesstore.data.database.dao.CategoryDao
@@ -10,12 +8,13 @@ import com.hieuwu.groceriesstore.data.database.entities.asDomainModel
 import com.hieuwu.groceriesstore.data.repository.CategoryRepository
 import com.hieuwu.groceriesstore.utilities.CollectionNames
 import com.hieuwu.groceriesstore.utilities.convertCategoryDocumentToEntity
-import javax.inject.Inject
-import javax.inject.Singleton
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @Singleton
 class CategoryRepositoryImpl @Inject constructor(private val categoryDao: CategoryDao) :
@@ -37,7 +36,6 @@ class CategoryRepositoryImpl @Inject constructor(private val categoryDao: Catego
         }
     }
 
-    override fun getFromLocal() = Transformations
-        .map(categoryDao.getAll().asLiveData()) { it.asDomainModel()
-    }
+    override fun getFromLocal() = categoryDao.getAll().map { it.asDomainModel() }
+
 }
