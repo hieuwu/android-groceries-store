e70972b416e046359a3135d59f13968018161ed3
Hieu Vu <star.hieu.99@gmail.com>
2022-12-22 22:39:08 +0700

    Remove unused method

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/ProductRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/ProductRepository.kt
@@ -1,13 +1,11 @@
 package com.hieuwu.groceriesstore.data.repository
 
-import androidx.lifecycle.LiveData
 import com.hieuwu.groceriesstore.domain.models.ProductModel
 import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
     val products: Flow<List<ProductModel>>
     fun getProductById(productId: String): Flow<ProductModel>
-    fun getAllProducts(): LiveData<List<ProductModel>>
     fun getAllProductsByCategory(categoryId: String): Flow<List<ProductModel>>
     suspend fun refreshDatabase()
     suspend fun updateLineItemQuantityById(quantity: Int, id: Long)
