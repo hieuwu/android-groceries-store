ba3812004f252867385f250663ee8f05b55827d6
Hieu Vu <star.hieu.99@gmail.com>
2023-04-05 23:27:16 +0700

    Provide dependencies

--- app/src/main/java/com/hieuwu/groceriesstore/di/SupabaseModule.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/di/SupabaseModule.kt
@@ -1,17 +1,13 @@
 package com.hieuwu.groceriesstore.di
 
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.hieuwu.groceriesstore.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.plugins.standaloneSupabaseModule
 import io.github.jan.supabase.postgrest.Postgrest
-import io.github.jan.supabase.postgrest.postgrest
+import io.ktor.client.plugins.*
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -21,14 +17,13 @@ object SupabaseModule {
     @Provides
     @Singleton
     fun provideFirebaseAuth(): Postgrest {
-        val client = createSupabaseClient(
-            supabaseUrl = BuildConfig.SUPABASE_URL,
-            supabaseKey = BuildConfig.SECRET
-        ) {
-            install(Postgrest) {
-            }
-        }
-        return client.postgrest
+        val postgrest = standaloneSupabaseModule(
+            Postgrest,
+            url = BuildConfig.SUPABASE_URL,
+            apiKey = BuildConfig.API_KEY
+        )
+
+        return postgrest
     }
 
 }
