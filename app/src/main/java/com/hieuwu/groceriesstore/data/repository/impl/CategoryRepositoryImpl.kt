471852d88a157e174c1db139092aa82eeae42fc6
Hieu Vu <star.hieu.99@gmail.com>
2023-04-07 22:15:06 +0700

    Query data from database

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/CategoryRepositoryImpl.kt
@@ -5,10 +5,12 @@ import com.google.firebase.ktx.Firebase
 import com.hieuwu.groceriesstore.data.database.dao.CategoryDao
 import com.hieuwu.groceriesstore.data.database.entities.Category
 import com.hieuwu.groceriesstore.data.database.entities.asDomainModel
+import com.hieuwu.groceriesstore.data.network.dto.CategoriesDto
 import com.hieuwu.groceriesstore.data.repository.CategoryRepository
 import com.hieuwu.groceriesstore.utilities.CollectionNames
 import com.hieuwu.groceriesstore.utilities.convertCategoryDocumentToEntity
 import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.query.Count
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.tasks.await
@@ -28,6 +30,9 @@ class CategoryRepositoryImpl @Inject constructor(
     override suspend fun refreshDatabase() {
         val categoriesList = mutableListOf<Category>()
         val fireStore = Firebase.firestore
+        val result = supabasePostgrest[CollectionNames.categories]
+            .select()
+        print(result.decodeList<CategoriesDto>())
         fireStore.collection(CollectionNames.categories).get().addOnSuccessListener { result ->
             for (document in result) {
                 categoriesList.add(convertCategoryDocumentToEntity(document))
