0c7ab2362599e78beb32320bec1665a49e7a566f
Hieu Vu <star.hieu.99@gmail.com>
2023-04-08 14:49:52 +0700

    Create product dto

---
+++ app/src/main/java/com/hieuwu/groceriesstore/data/network/dto/ProductDto.kt
@@ -0,0 +1,24 @@
+package com.hieuwu.groceriesstore.data.network.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ProductDto(
+    @SerialName("productId")
+    val productId: String,
+    @SerialName("name")
+    val name: String,
+    @SerialName("description")
+    val description: String,
+    @SerialName("price")
+    val price: Double,
+    @SerialName("image")
+    val image: String,
+    @SerialName("category")
+    val category: String,
+    @SerialName("nutrition")
+    val nutrition: String,
+    @SerialName("_id")
+    val _id: Int,
+)
