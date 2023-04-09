d3fce3d15ff96761f671bc2d57afb4fc115eea8b
Hieu Vu <star.hieu.99@gmail.com>
2023-04-09 23:58:07 +0700

    Remove unused import

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/CategoryRepositoryImpl.kt
@@ -1,25 +1,15 @@
 package com.hieuwu.groceriesstore.data.repository.impl
 
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.hieuwu.groceriesstore.data.database.dao.CategoryDao
-import com.hieuwu.groceriesstore.data.database.entities.Category
 import com.hieuwu.groceriesstore.data.database.entities.asDomainModel
 import com.hieuwu.groceriesstore.data.network.dto.CategoriesDto
 import com.hieuwu.groceriesstore.data.repository.CategoryRepository
 import com.hieuwu.groceriesstore.utilities.CollectionNames
 import com.hieuwu.groceriesstore.utilities.SupabaseMapper
-import com.hieuwu.groceriesstore.utilities.convertCategoryDocumentToEntity
 import io.github.jan.supabase.postgrest.Postgrest
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
-import timber.log.Timber
 import javax.inject.Inject
-import javax.inject.Singleton
 
-//@Singleton
 class CategoryRepositoryImpl @Inject constructor(
 
     private val categoryDao: CategoryDao,
