b7909f36bceaab3bca193a02933323d454db583a
Hieu Vu <star.hieu.99@gmail.com>
2023-04-13 23:23:15 +0700

    Provide GoTrue

--- app/src/main/java/com/hieuwu/groceriesstore/di/SupabaseModule.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/di/SupabaseModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.github.jan.supabase.gotrue.GoTrue
 import io.github.jan.supabase.plugins.standaloneSupabaseModule
 import io.github.jan.supabase.postgrest.Postgrest
 import io.ktor.client.plugins.*
@@ -19,10 +20,21 @@ object SupabaseModule {
     fun provideSupabaseDatabase(): Postgrest {
         val postgrest = standaloneSupabaseModule(
             Postgrest,
-            url = BuildConfig.SUPABASE_URL,
+            url = "${BuildConfig.SUPABASE_URL}/rest/v1",
             apiKey = BuildConfig.API_KEY
         )
         return postgrest
+    }
+
+    @Provides
+    @Singleton
+    fun provideSupabaseGoTrue(): GoTrue {
+        val goTrue = standaloneSupabaseModule(
+            GoTrue,
+            url = BuildConfig.SUPABASE_URL,
+            apiKey = BuildConfig.API_KEY
+        )
+        return goTrue
     }
 
 }
