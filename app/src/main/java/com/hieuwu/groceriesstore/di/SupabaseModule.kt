4e21c40b41de11816951a3aed1bb3191a523ed3c
Hieu Vu <star.hieu.99@gmail.com>
2023-04-22 10:24:20 +0700

    Remove unused import

--- app/src/main/java/com/hieuwu/groceriesstore/di/SupabaseModule.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/di/SupabaseModule.kt
@@ -9,7 +9,6 @@ import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.gotrue.GoTrue
 import io.github.jan.supabase.gotrue.gotrue
-import io.github.jan.supabase.plugins.standaloneSupabaseModule
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.postgrest
 import io.ktor.client.plugins.*
