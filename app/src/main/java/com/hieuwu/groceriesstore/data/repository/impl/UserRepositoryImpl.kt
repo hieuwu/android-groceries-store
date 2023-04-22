9c1b4404e60ee5aa7b47f2af2632cb44cc8de215
Hieu Vu <star.hieu.99@gmail.com>
2023-04-22 10:24:28 +0700

    Replace firebase auth

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/UserRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/UserRepositoryImpl.kt
@@ -1,21 +1,19 @@
 package com.hieuwu.groceriesstore.data.repository.impl
 
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.hieuwu.groceriesstore.data.database.dao.UserDao
 import com.hieuwu.groceriesstore.data.database.entities.User
 import com.hieuwu.groceriesstore.data.database.entities.asDomainModel
-import com.hieuwu.groceriesstore.data.network.dto.CategoriesDto
 import com.hieuwu.groceriesstore.data.network.dto.UserDto
 import com.hieuwu.groceriesstore.data.repository.UserRepository
 import com.hieuwu.groceriesstore.utilities.CollectionNames
 import com.hieuwu.groceriesstore.utilities.SupabaseMapper
-import com.hieuwu.groceriesstore.utilities.convertUserDocumentToEntity
 import com.hieuwu.groceriesstore.utilities.convertUserEntityToDocument
 import com.hieuwu.groceriesstore.utilities.createUpdateUserRequest
 import io.github.jan.supabase.gotrue.GoTrue
 import io.github.jan.supabase.gotrue.providers.builtin.Email
 import io.github.jan.supabase.postgrest.Postgrest
+import java.util.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.tasks.await
@@ -25,33 +23,30 @@ import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
     private val userDao: UserDao,
-    private val auth: FirebaseAuth,
     private val fireStore: FirebaseFirestore,
     private val authService: GoTrue,
     private val postgrest: Postgrest,
 ) : UserRepository {
 
     override suspend fun createAccount(email: String, password: String, name: String): Boolean {
-        val result = authService.signUpWith(Email) {
+        authService.signUpWith(Email) {
             this.email = email
             this.password = password
         }
-        return if (result != null) {
-            val user = User(
-                id = result.id,
-                name = name,
-                email = email,
-                address = null,
-                phone = null,
-                isOrderCreatedNotiEnabled = false,
-                isPromotionNotiEnabled = false,
-                isDataRefreshedNotiEnabled = false
-            )
-            userDao.insert(user)
-            true
-        } else {
-            false
-        }
+        val userDto = UserDto(
+            id = UUID.randomUUID().toString(),
+            name = name,
+            email = email,
+            address = null,
+            phone = null,
+            isOrderCreatedNotiEnabled = false,
+            isPromotionNotiEnabled = false,
+            isDataRefreshedNotiEnabled = false
+        )
+        postgrest[CollectionNames.users].insert(value = userDto, upsert = true)
+        val user = SupabaseMapper.mapDtoToEntity(userDto)
+        userDao.insert(user)
+        return true
     }
 
     override suspend fun authenticate(email: String, password: String): Boolean {
@@ -60,17 +55,10 @@ class UserRepositoryImpl @Inject constructor(
             this.password = password
         }
 
-//        val userDto = postgrest[CollectionNames.users].select().decodeSingle<UserDto>()
-//        val user = SupabaseMapper.mapDtoToEntity(userDto)
-//        userDao.insert(user)
+        val userDto = postgrest[CollectionNames.users].select().decodeSingle<UserDto>()
+        val user = SupabaseMapper.mapDtoToEntity(userDto)
+        userDao.insert(user)
         return true
-//        fireStore.collection(CollectionNames.users).document(auth.currentUser?.uid!!).get()
-//            .addOnSuccessListener {
-//                it?.let {
-//                    user = convertUserDocumentToEntity(id!!, it)
-//                }
-//            }
-//            .addOnFailureListener { e -> Timber.d(e) }.await()
 
     }
 
