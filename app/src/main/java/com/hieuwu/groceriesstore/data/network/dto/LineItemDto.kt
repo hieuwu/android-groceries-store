68214536fb4dc1349fc1ea1caf30c2ee58b55523
Hieu Vu <star.hieu.99@gmail.com>
2023-04-26 21:19:27 +0700

    Define dto

--- app/src/main/java/com/hieuwu/groceriesstore/data/network/dto/LineItemDto.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/network/dto/LineItemDto.kt
@@ -1,7 +1,27 @@
 package com.hieuwu.groceriesstore.data.network.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class LineItemDto(
 
-data class LineItemDto(val id: String)
+    @SerialName("id")
+    val id: Long,
+
+    @SerialName("lineItemId")
+    val lineItemId: Long,
+
+    @SerialName("productId")
+    val productId: String,
+
+    @SerialName("orderId")
+    val orderId: String,
+
+    @SerialName("quantity")
+    var quantity: Int,
+
+    @SerialName("subtotal")
+    var subtotal: Double
+
+)
