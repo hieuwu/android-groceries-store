a8a356750707f7c5e12da56152b504943d74d4b6
Hieu Vu <star.hieu.99@gmail.com>
2023-05-01 20:38:02 +0700

    Handle exception

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/RecipeRepositoryImpl.kt
@@ -5,25 +5,22 @@ import com.hieuwu.groceriesstore.data.database.entities.asDomainModel
 import com.hieuwu.groceriesstore.data.network.Api
 import com.hieuwu.groceriesstore.data.network.dto.asEntity
 import com.hieuwu.groceriesstore.data.repository.RecipeRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.withContext
+import timber.log.Timber
 import javax.inject.Inject
 
 class RecipeRepositoryImpl @Inject constructor(
     private val recipeDao: RecipeDao
 ) : RecipeRepository {
     override suspend fun refreshDatabase() {
-        withContext(Dispatchers.IO) {
+        try {
             val getRecipeDeferred = Api.retrofitService.getRecipesList()
-            try {
-                val listResult = getRecipeDeferred.await().recipesList.asEntity()
-                recipeDao.insertAll(listResult)
-            } catch (t: Throwable) {
-                var message = t.message
-            }
+            val listResult = getRecipeDeferred.await().recipesList.asEntity()
+            recipeDao.insertAll(listResult)
+        } catch (e: Exception) {
+            Timber.e(e.message)
         }
     }
 
-    override fun getFromLocal() = recipeDao.getAll().map { it.asDomainModel()}
+    override fun getFromLocal() = recipeDao.getAll().map { it.asDomainModel() }
 }
