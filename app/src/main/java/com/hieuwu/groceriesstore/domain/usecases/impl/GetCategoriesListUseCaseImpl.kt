44e6e5105e1411c500847db3899471fa5bebde45
Hieu Vu <star.hieu.99@gmail.com>
2023-05-02 20:08:02 +0700

    Wrap with dispatcher

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetCategoriesListUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetCategoriesListUseCaseImpl.kt
@@ -2,13 +2,17 @@ package com.hieuwu.groceriesstore.domain.usecases.impl
 
 import com.hieuwu.groceriesstore.data.repository.CategoryRepository
 import com.hieuwu.groceriesstore.domain.usecases.GetCategoriesListUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetCategoriesListUseCaseImpl @Inject constructor(
     private val categoryRepository: CategoryRepository
 ) : GetCategoriesListUseCase {
     override suspend fun execute(input: GetCategoriesListUseCase.Input): GetCategoriesListUseCase.Output {
-        val result = categoryRepository.getFromLocal()
-        return GetCategoriesListUseCase.Output(result)
+        return withContext(Dispatchers.IO) {
+            val result = categoryRepository.getFromLocal()
+            GetCategoriesListUseCase.Output(result)
+        }
     }
 }
