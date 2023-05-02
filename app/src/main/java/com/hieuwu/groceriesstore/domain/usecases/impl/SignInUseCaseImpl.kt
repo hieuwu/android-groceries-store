09db2d227a0f9693bd5dd545d65b8cde52be7f1f
Hieu Vu <star.hieu.99@gmail.com>
2023-05-02 20:07:45 +0700

    Wrap with dispatcher

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/SignInUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/SignInUseCaseImpl.kt
@@ -2,18 +2,22 @@ package com.hieuwu.groceriesstore.domain.usecases.impl
 
 import com.hieuwu.groceriesstore.data.repository.UserRepository
 import com.hieuwu.groceriesstore.domain.usecases.SignInUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
 
 class SignInUseCaseImpl @Inject constructor(private val userRepository: UserRepository) :
     SignInUseCase {
     override suspend fun execute(input: SignInUseCase.Input): SignInUseCase.Output {
-        return try {
-            val res = userRepository.authenticate(input.email, input.password)
-            SignInUseCase.Output(res)
-        } catch (e: Exception) {
-            Timber.d(e.message)
-            SignInUseCase.Output.AccountNotExistedError
+        return withContext(Dispatchers.IO) {
+            try {
+                val res = userRepository.authenticate(input.email, input.password)
+                SignInUseCase.Output(res)
+            } catch (e: Exception) {
+                Timber.d(e.message)
+                SignInUseCase.Output.AccountNotExistedError
+            }
         }
     }
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/SignOutUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/SignOutUseCaseImpl.kt
@@ -2,12 +2,16 @@ package com.hieuwu.groceriesstore.domain.usecases.impl
 
 import com.hieuwu.groceriesstore.data.repository.UserRepository
 import com.hieuwu.groceriesstore.domain.usecases.SignOutUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SignOutUseCaseImpl @Inject constructor(private val userRepository: UserRepository) :
     SignOutUseCase {
     override suspend fun execute(input: SignOutUseCase.Input): SignOutUseCase.Output {
-        userRepository.clearUser()
-        return SignOutUseCase.Output()
+        return withContext(Dispatchers.IO) {
+            userRepository.clearUser()
+            SignOutUseCase.Output()
+        }
     }
 }
