e5c03d97ba69d8728de451363e6d50934c14dc8f
Hieu Vu <star.hieu.99@gmail.com>
2023-05-02 20:08:05 +0700

    Wrap with dispatcher

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetCurrentCartUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetCurrentCartUseCaseImpl.kt
@@ -3,6 +3,8 @@ package com.hieuwu.groceriesstore.domain.usecases.impl
 import com.hieuwu.groceriesstore.data.repository.OrderRepository
 import com.hieuwu.groceriesstore.domain.usecases.GetCurrentCartUseCase
 import com.hieuwu.groceriesstore.utilities.OrderStatus
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetCurrentCartUseCaseImpl @Inject constructor(
@@ -10,7 +12,9 @@ class GetCurrentCartUseCaseImpl @Inject constructor(
 ) : GetCurrentCartUseCase {
 
     override suspend fun execute(input: GetCurrentCartUseCase.Input): GetCurrentCartUseCase.Output {
-        val res = orderRepository.getOneOrderByStatus(OrderStatus.IN_CART)
-        return GetCurrentCartUseCase.Output(res)
+        return withContext(Dispatchers.IO) {
+            val res = orderRepository.getOneOrderByStatus(OrderStatus.IN_CART)
+            GetCurrentCartUseCase.Output(res)
+        }
     }
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetProductsByCategoryUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetProductsByCategoryUseCaseImpl.kt
@@ -2,12 +2,16 @@ package com.hieuwu.groceriesstore.domain.usecases.impl
 
 import com.hieuwu.groceriesstore.data.repository.ProductRepository
 import com.hieuwu.groceriesstore.domain.usecases.GetProductsByCategoryUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetProductsByCategoryUseCaseImpl @Inject constructor(private val productRepository: ProductRepository) :
     GetProductsByCategoryUseCase {
     override suspend fun execute(input: GetProductsByCategoryUseCase.Input): GetProductsByCategoryUseCase.Output {
-        val result = productRepository.getAllProductsByCategory(input.categoryId)
-        return GetProductsByCategoryUseCase.Output(result)
+        return withContext(Dispatchers.IO) {
+            val result = productRepository.getAllProductsByCategory(input.categoryId)
+            GetProductsByCategoryUseCase.Output(result)
+        }
     }
 }
