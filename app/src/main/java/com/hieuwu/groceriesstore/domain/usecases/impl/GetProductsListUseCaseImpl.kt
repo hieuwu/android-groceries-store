e1d8c3f3909692e544efcbaa873ad8f2a0a527f9
Hieu Vu <star.hieu.99@gmail.com>
2023-05-02 20:07:52 +0700

    Wrap with dispatcher

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetProductsListUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetProductsListUseCaseImpl.kt
@@ -2,12 +2,16 @@ package com.hieuwu.groceriesstore.domain.usecases.impl
 
 import com.hieuwu.groceriesstore.data.repository.ProductRepository
 import com.hieuwu.groceriesstore.domain.usecases.GetProductsListUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetProductsListUseCaseImpl @Inject constructor(private val productRepository: ProductRepository) :
     GetProductsListUseCase {
     override suspend fun execute(input: GetProductsListUseCase.Input): GetProductsListUseCase.Output {
-        val result = productRepository.products
-        return GetProductsListUseCase.Output(result)
+        return withContext(Dispatchers.IO) {
+            val result = productRepository.products
+            GetProductsListUseCase.Output(result)
+        }
     }
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetProfileUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetProfileUseCaseImpl.kt
@@ -2,12 +2,16 @@ package com.hieuwu.groceriesstore.domain.usecases.impl
 
 import com.hieuwu.groceriesstore.data.repository.UserRepository
 import com.hieuwu.groceriesstore.domain.usecases.GetProfileUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetProfileUseCaseImpl @Inject constructor(private val userRepository: UserRepository) :
     GetProfileUseCase {
     override suspend fun execute(input: GetProfileUseCase.Input): GetProfileUseCase.Output {
-        val user = userRepository.getCurrentUser()
-        return GetProfileUseCase.Output(user)
+        return withContext(Dispatchers.IO) {
+            val user = userRepository.getCurrentUser()
+            GetProfileUseCase.Output(user)
+        }
     }
 }
