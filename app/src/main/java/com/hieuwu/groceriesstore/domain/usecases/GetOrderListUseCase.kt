1d2b63f01c97caac08cdc7376759469074506e65
Hieu Vu <star.hieu.99@gmail.com>
2023-05-06 20:58:38 +0700

    Implement use case

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetOrderListUseCase.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/GetOrderListUseCase.kt
@@ -1,4 +1,9 @@
 package com.hieuwu.groceriesstore.domain.usecases
 
-interface GetOrderListUseCase {
+import com.hieuwu.groceriesstore.domain.models.OrderModel
+import kotlinx.coroutines.flow.Flow
+
+interface GetOrderListUseCase: UseCase<GetOrderListUseCase.Input, GetOrderListUseCase.Output> {
+    class Input
+    class Output(val data: Flow<List<OrderModel>>)
 }
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetOrderListUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetOrderListUseCaseImpl.kt
@@ -1,4 +1,15 @@
 package com.hieuwu.groceriesstore.domain.usecases.impl
 
-class GetOrderListUseCaseImpl {
+import com.hieuwu.groceriesstore.data.repository.OrderRepository
+import com.hieuwu.groceriesstore.domain.usecases.GetOrderListUseCase
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetOrderListUseCaseImpl @Inject constructor(
+    private val orderRepository: OrderRepository
+) : GetOrderListUseCase {
+    override suspend fun execute(input: GetOrderListUseCase.Input): GetOrderListUseCase.Output {
+        val result = orderRepository.getOrders()
+        return GetOrderListUseCase.Output(data = flow { result })
+    }
 }
