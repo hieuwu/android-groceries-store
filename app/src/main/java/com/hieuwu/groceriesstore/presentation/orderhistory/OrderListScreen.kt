8d6ab77b4a7cf8983d5c25d2a904ed8973a96f96
Hieu Vu <star.hieu.99@gmail.com>
2023-05-07 09:24:36 +0700

    Declare order list

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/orderhistory/OrderListScreen.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/orderhistory/OrderListScreen.kt
@@ -1,2 +1,24 @@
 package com.hieuwu.groceriesstore.presentation.orderhistory
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun OrderHistoryScreen(
+    modifier: Modifier = Modifier,
+    viewModel: OrderListViewModel = hiltViewModel()
+) {
+    Column(modifier = modifier) {
+        LazyColumn {
+            items(mutableListOf<String>()) { it ->
+
+
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/hieuwu/groceriesstore/presentation/orderhistory/OrderListViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/orderhistory/OrderListViewModel.kt
@@ -1,4 +1,8 @@
 package com.hieuwu.groceriesstore.presentation.orderhistory
 
-class OrderListViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class OrderListViewModel : ViewModel() {
 }
