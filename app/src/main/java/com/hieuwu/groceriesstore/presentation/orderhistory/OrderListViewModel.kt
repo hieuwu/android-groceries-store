7e2e33d4c70cc65e7bc9e310eba75372d47736d8
Hieu Vu <star.hieu.99@gmail.com>
2023-05-07 09:28:42 +0700

    Fix build error

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/orderhistory/OrderListViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/orderhistory/OrderListViewModel.kt
@@ -2,7 +2,8 @@ package com.hieuwu.groceriesstore.presentation.orderhistory
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class OrderListViewModel : ViewModel() {
+class OrderListViewModel @Inject constructor() : ViewModel() {
 }
