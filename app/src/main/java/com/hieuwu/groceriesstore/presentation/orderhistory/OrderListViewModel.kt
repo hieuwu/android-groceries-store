d863213085c2ba6ef1138eb66131a6b175a4e37a
Hieu Vu <star.hieu.99@gmail.com>
2023-05-09 22:22:08 +0700

    Get order list

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/orderhistory/OrderListViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/orderhistory/OrderListViewModel.kt
@@ -1,9 +1,41 @@
 package com.hieuwu.groceriesstore.presentation.orderhistory
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hieuwu.groceriesstore.domain.models.OrderModel
+import com.hieuwu.groceriesstore.domain.usecases.GetOrderListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class OrderListViewModel @Inject constructor() : ViewModel() {
+class OrderListViewModel @Inject constructor(
+    private val getOrderListUseCase: GetOrderListUseCase
+) : ViewModel() {
+
+    private val _orderList = MutableStateFlow<List<OrderModel>>(listOf())
+    val orderList = _orderList
+
+    init {
+        getOrderList()
+    }
+
+    private fun getOrderList() {
+        viewModelScope.launch {
+            when (val result = getOrderListUseCase.execute(GetOrderListUseCase.Input())) {
+                is GetOrderListUseCase.Output.Success -> {
+                    result.data.collect {
+                        _orderList.value = it
+                    }
+                }
+                else -> {
+
+                }
+            }
+        }
+
+    }
+
 }
