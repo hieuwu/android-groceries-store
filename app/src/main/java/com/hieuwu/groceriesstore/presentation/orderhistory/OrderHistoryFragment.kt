912aed45e97a853f49a59e9655e9d8226165f46f
Hieu Vu <star.hieu.99@gmail.com>
2023-05-13 16:08:55 +0700

    Remove unused code

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/orderhistory/OrderHistoryFragment.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/orderhistory/OrderHistoryFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import com.hieuwu.groceriesstore.databinding.FragmentOrderHistoryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
