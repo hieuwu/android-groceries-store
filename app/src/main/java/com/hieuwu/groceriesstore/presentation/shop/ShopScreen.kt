8b47320daafb489aedf40266802c209d6bbd11fa
Hieu Vu <star.hieu.99@gmail.com>
2023-06-13 22:58:33 +0700

    Create composables

---
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/ShopScreen.kt
@@ -0,0 +1,9 @@
+package com.hieuwu.groceriesstore.presentation.shop
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ShopScreen(modifier: Modifier = Modifier) {
+
+}
