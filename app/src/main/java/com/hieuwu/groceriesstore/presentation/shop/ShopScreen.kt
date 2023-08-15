9e015f8f4247b5a18c3e1cd1cd0a4ce6c576d39e
Hieu Vu <star.hieu.99@gmail.com>
2023-08-15 23:16:06 +0700

    Update Ui spacing

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/ShopScreen.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/ShopScreen.kt
@@ -4,10 +4,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -18,6 +24,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.hieuwu.groceriesstore.R
@@ -31,27 +38,55 @@ fun ShopScreen(
 ) {
     val products = viewModel.productList.collectAsState()
     Box(modifier = modifier.background(colorResource(id = R.color.colorPrimary))) {
+
+        Text(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(vertical = 12.dp),
+            text = "Groceries Store",
+            color = Color.White,
+            style = MaterialTheme.typography.h5,
+            textAlign = TextAlign.Center
+        )
         Column(
             modifier = modifier
+                .verticalScroll(rememberScrollState())
                 .fillMaxSize()
+                .padding(top = 64.dp)
                 .clip(RoundedCornerShape(topStartPercent = 8, topEndPercent = 8))
                 .background(Color.White)
         ) {
             Image(
                 modifier = modifier
+                    .padding(top = 8.dp)
                     .align(Alignment.CenterHorizontally)
-                    .size(64.dp),
+                    .size(48.dp),
                 painter = painterResource(id = R.drawable.colorful_carrot),
                 contentDescription = null,
             )
-            Text(text = stringResource(id = R.string.welcome_prompt))
-            Carousel(modifier = modifier)
+            Text(
+                modifier = modifier.align(Alignment.CenterHorizontally),
+                text = stringResource(id = R.string.welcome_prompt)
+            )
+            Carousel(modifier = modifier.padding(4.dp))
             ProductCatalogue(
                 products = products.value,
                 title = "Best seller",
-                onAddToCartClick = { product -> viewModel.addToCart(product) })
+                onAddToCartClick = { product -> viewModel.addToCart(product) }
+            )
+            Spacer(modifier = modifier.height(12.dp))
+            ProductCatalogue(
+                products = products.value,
+                title = "Hot deal",
+                onAddToCartClick = { product -> viewModel.addToCart(product) }
+            )
+            Spacer(modifier = modifier.height(12.dp))
+            ProductCatalogue(
+                products = products.value,
+                title = "Exclusive offer",
+                onAddToCartClick = { product -> viewModel.addToCart(product) }
+            )
         }
-
     }
 
 }
