ce38deb33791d92fdc362fb1e5aea1c25c7abdb0
Hieu Vu <star.hieu.99@gmail.com>
2023-08-15 23:16:22 +0700

    Remove unused code

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/composables/Carousel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/composables/Carousel.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -62,18 +63,6 @@ fun Carousel(modifier: Modifier = Modifier) {
                 state = sectionItemListState,
             ) {
                 items(bannerImages) { image ->
-//                val isLeftScrolling = sectionItemListState.isScrollingUp()
-//                LaunchedEffect(key1 = isLeftScrolling) {
-//                    if (currentVisibleIndex.value != 0) {
-//
-//                        if (isLeftScrolling) {
-//                            currentVisibleIndex.value += 1
-//                        } else {
-//                            currentVisibleIndex.value -= 1
-//                        }
-//                        sectionItemListState.animateScrollToItem(currentVisibleIndex.value)
-//                    }
-//                }
                     GlideImage(
                         contentScale = ContentScale.Crop,
                         model = image,
