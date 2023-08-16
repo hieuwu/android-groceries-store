22e46a94ac09b2645000f36b14a5bcd336295b79
Hieu Vu <star.hieu.99@gmail.com>
2023-08-16 23:15:33 +0700

    Remove unused import

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/composables/ProductItem.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/composables/ProductItem.kt
@@ -1,7 +1,6 @@
 package com.hieuwu.groceriesstore.presentation.shop.composables
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -24,7 +23,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
