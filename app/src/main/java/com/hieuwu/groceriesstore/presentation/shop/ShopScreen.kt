36d73026e52354b7c25af2039cd82eb0f14c6f81
Hieu Vu <star.hieu.99@gmail.com>
2023-08-18 22:43:34 +0700

    Show snack bar

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/ShopScreen.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/shop/ShopScreen.kt
@@ -14,10 +14,15 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,6 +36,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.presentation.shop.composables.Carousel
 import com.hieuwu.groceriesstore.presentation.shop.composables.ProductCatalogue
+import kotlinx.coroutines.launch
 
 @Composable
 fun ShopScreen(
@@ -38,67 +44,96 @@ fun ShopScreen(
     viewModel: ShopViewModel = hiltViewModel(),
     navigateToProductDetails: (String) -> Unit
 ) {
-    val products = viewModel.productList.collectAsState()
-    Box(modifier = modifier.background(colorResource(id = R.color.colorPrimary))) {
 
-        Text(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(vertical = 12.dp),
-            text = "Groceries Store",
-            color = Color.White,
-            style = MaterialTheme.typography.h5,
-            textAlign = TextAlign.Center
-        )
-        Column(
-            modifier = modifier
-                .verticalScroll(rememberScrollState())
-                .fillMaxSize()
-                .padding(top = 64.dp)
-                .clip(RoundedCornerShape(topStartPercent = 8, topEndPercent = 8))
-                .background(Color.White)
-        ) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) },
 
-            val selectedProduct = viewModel.navigateToSelectedProperty.collectAsState().value
-            LaunchedEffect(key1 = selectedProduct) {
-                selectedProduct?.let {
-                    navigateToProductDetails(selectedProduct.id)
-                    viewModel.displayProductDetailsComplete()
-                }
-            }
-            Image(
-                modifier = modifier
-                    .padding(top = 8.dp)
-                    .align(Alignment.CenterHorizontally)
-                    .size(48.dp),
-                painter = painterResource(id = R.drawable.colorful_carrot),
-                contentDescription = null,
-            )
+        ) { paddingValues ->
+        Box(modifier = modifier.background(colorResource(id = R.color.colorPrimary))) {
+            val products = viewModel.productList.collectAsState()
+
             Text(
-                modifier = modifier.align(Alignment.CenterHorizontally),
-                text = stringResource(id = R.string.welcome_prompt)
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 12.dp),
+                text = "Groceries Store",
+                color = Color.White,
+                style = MaterialTheme.typography.h5,
+                textAlign = TextAlign.Center
             )
-            Carousel(modifier = modifier.padding(4.dp))
-            ProductCatalogue(
-                products = products.value,
-                title = "Best seller",
-                onAddToCartClick = { product -> viewModel.addToCart(product) },
-                onNavigateToProductDetails = { id -> viewModel.displayProductDetails(id) }
-            )
-            Spacer(modifier = modifier.height(12.dp))
-            ProductCatalogue(
-                products = products.value,
-                title = "Hot deal",
-                onAddToCartClick = { product -> viewModel.addToCart(product) },
-                onNavigateToProductDetails = { id -> viewModel.displayProductDetails(id) }
-            )
-            Spacer(modifier = modifier.height(12.dp))
-            ProductCatalogue(
-                products = products.value,
-                title = "Exclusive offer",
-                onAddToCartClick = { product -> viewModel.addToCart(product) },
-                onNavigateToProductDetails = { id -> viewModel.displayProductDetails(id) }
-            )
+            Column(
+                modifier = modifier
+                    .verticalScroll(rememberScrollState())
+                    .fillMaxSize()
+                    .padding(top = 64.dp)
+                    .clip(RoundedCornerShape(topStartPercent = 8, topEndPercent = 8))
+                    .background(Color.White)
+            ) {
+
+                val selectedProduct = viewModel.navigateToSelectedProperty.collectAsState().value
+                LaunchedEffect(key1 = selectedProduct) {
+                    selectedProduct?.let {
+                        navigateToProductDetails(selectedProduct.id)
+                        viewModel.displayProductDetailsComplete()
+                    }
+                }
+                Image(
+                    modifier = modifier
+                        .padding(top = 8.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .size(48.dp),
+                    painter = painterResource(id = R.drawable.colorful_carrot),
+                    contentDescription = null,
+                )
+                Text(
+                    modifier = modifier.align(Alignment.CenterHorizontally),
+                    text = stringResource(id = R.string.welcome_prompt)
+                )
+                Carousel(modifier = modifier.padding(4.dp))
+                ProductCatalogue(
+                    products = products.value,
+                    title = "Best seller",
+                    onAddToCartClick = { product ->
+                        viewModel.addToCart(product)
+                        scope.launch {
+                            snackbarHostState.showSnackbar(
+                                "Added ${product.name}"
+                            )
+                        }
+                    },
+                    onNavigateToProductDetails = { id -> viewModel.displayProductDetails(id) }
+                )
+                Spacer(modifier = modifier.height(12.dp))
+                ProductCatalogue(
+                    products = products.value,
+                    title = "Hot deal",
+                    onAddToCartClick = { product ->
+                        viewModel.addToCart(product)
+                        scope.launch {
+                            snackbarHostState.showSnackbar(
+                                "Added ${product.name}"
+                            )
+                        }
+                    },
+                    onNavigateToProductDetails = { id -> viewModel.displayProductDetails(id) }
+                )
+                Spacer(modifier = modifier.height(12.dp))
+                ProductCatalogue(
+                    products = products.value,
+                    title = "Exclusive offer",
+                    onAddToCartClick = { product ->
+                        viewModel.addToCart(product)
+                        scope.launch {
+                            snackbarHostState.showSnackbar(
+                                "Added ${product.name}"
+                            )
+                        }
+                    },
+                    onNavigateToProductDetails = { id -> viewModel.displayProductDetails(id) }
+                )
+            }
         }
     }
 
