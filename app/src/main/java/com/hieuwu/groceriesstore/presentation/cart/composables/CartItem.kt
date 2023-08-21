daa8ad2dfd91e915af878999c5caac773ed68c9e
Hieu Vu <star.hieu.99@gmail.com>
2023-08-21 22:50:59 +0700

    Add composables

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/cart/composables/CartItem.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/cart/composables/CartItem.kt
@@ -1,2 +1,20 @@
 package com.hieuwu.groceriesstore.presentation.cart.composables
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CartItem(
+    modifier: Modifier = Modifier,
+    onIncrease: () -> Unit,
+    onDecrease: () -> Unit
+) {
+
+}
+
+@Preview
+@Composable
+fun CartItemPreview() {
+
+}
