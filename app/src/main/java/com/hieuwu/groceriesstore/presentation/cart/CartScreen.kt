2817f8045f20e11821015557f3e92e23bb4e17ca
Hieu Vu <star.hieu.99@gmail.com>
2023-08-21 22:51:01 +0700

    Add composables

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/cart/CartScreen.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/cart/CartScreen.kt
@@ -1,2 +1,13 @@
 package com.hieuwu.groceriesstore.presentation.cart
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun CartScreen(
+    modifier: Modifier = Modifier,
+    viewModel: CartViewModel = hiltViewModel()
+) {
+
+}
