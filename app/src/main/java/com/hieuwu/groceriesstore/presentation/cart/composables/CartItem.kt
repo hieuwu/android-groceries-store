bdcc0defb6b07b9cbf5bee0fa06fd4297cfa74fb
Hieu Vu <star.hieu.99@gmail.com>
2023-08-24 23:15:30 +0700

    Refactor code with call back

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/cart/composables/CartItem.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/cart/composables/CartItem.kt
@@ -1,6 +1,5 @@
 package com.hieuwu.groceriesstore.presentation.cart.composables
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
@@ -18,17 +18,18 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.domain.models.LineItemModel
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun CartItem(
     modifier: Modifier = Modifier,
@@ -37,63 +38,67 @@ fun CartItem(
     onRemove: () -> Unit,
     lineItem: LineItemModel
 ) {
-    val quantity = remember { mutableStateOf(lineItem.quantity) }
-
-    Row(
+    Card(
         modifier = modifier
             .fillMaxWidth()
             .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
+        elevation = 2.dp,
     ) {
-        Image(
-            modifier = modifier
-                .width(120.dp)
-                .height(80.dp),
-            painter = painterResource(id = R.drawable.colorful_carrot),
-            contentDescription = null
-        )
-        Column(modifier = modifier.weight(1f)) {
-            Text(
-                text = lineItem.name ?: "",
-                style = MaterialTheme.typography.labelLarge
+        Row(modifier = modifier.fillMaxWidth()) {
+            GlideImage(
+                contentScale = ContentScale.Crop,
+                model = lineItem.image,
+                contentDescription = null,
+                modifier = Modifier
+                    .width(120.dp)
+                    .height(80.dp)
+                    .padding(8.dp)
             )
-            Text(text = "$ ${lineItem.price}")
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+            Column(
+                modifier = modifier
+                    .weight(1f)
+                    .padding(8.dp)
             ) {
-                IconButton(onClick = {
-                    quantity.value?.minus(1)
-                }) {
-                    Icon(imageVector = Icons.Filled.Remove, contentDescription = null)
-                }
-                Spacer(modifier = modifier.width(4.dp))
-                Text(text = "${quantity.value}")
-                Spacer(modifier = modifier.width(4.dp))
-                IconButton(onClick = {
-                    quantity.value?.plus(1)
-                }) {
-                    Icon(
-                        imageVector = Icons.Filled.Add, contentDescription = null,
-                        tint = colorResource(id = R.color.primary_button)
-                    )
+                Text(
+                    text = lineItem.name ?: "",
+                    style = MaterialTheme.typography.labelLarge
+                )
+                Spacer(modifier = modifier.height(4.dp))
+                Text(
+                    text = "$ ${lineItem.price}"
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    IconButton(onClick = onDecrease) {
+                        Icon(imageVector = Icons.Filled.Remove, contentDescription = null)
+                    }
+                    Spacer(modifier = modifier.width(4.dp))
+                    Text(text = "${lineItem.quantity}")
+                    Spacer(modifier = modifier.width(4.dp))
+                    IconButton(onClick = onIncrease) {
+                        Icon(
+                            imageVector = Icons.Filled.Add, contentDescription = null,
+                            tint = colorResource(id = R.color.primary_button)
+                        )
+                    }
                 }
             }
-
-        }
-        Column {
-            Text(
-                text = "$ ${lineItem.subtotal}",
-                style = MaterialTheme.typography.labelLarge
-            )
-            IconButton(
-                onClick = {}
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.Delete,
-                    contentDescription = null,
-                    tint = colorResource(id = R.color.colorPrimary)
+            Column(modifier = modifier.padding(8.dp)) {
+                Text(
+                    text = "$ ${lineItem.subtotal}",
+                    style = MaterialTheme.typography.labelLarge
                 )
+                IconButton(
+                    onClick = onRemove
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Delete,
+                        contentDescription = null,
+                        tint = colorResource(id = R.color.colorPrimary)
+                    )
+                }
             }
         }
     }
