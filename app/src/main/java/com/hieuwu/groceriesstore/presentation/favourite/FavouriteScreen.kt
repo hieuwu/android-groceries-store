980435de9f64aea7115c8c1bc6d36724eb53d984
Hieu Vu <star.hieu.99@gmail.com>
2023-08-26 15:23:00 +0700

    Implement item

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/favourite/FavouriteScreen.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/favourite/FavouriteScreen.kt
@@ -1,2 +1,8 @@
-package com.hieuwu.groceriesstore.presentation.favourite.composables
+package com.hieuwu.groceriesstore.presentation.favourite
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FavouriteScreen(modifier: Modifier = Modifier) {
+}
