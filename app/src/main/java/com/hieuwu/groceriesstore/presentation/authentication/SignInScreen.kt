46a12f27c1ae568ffebebd3947169bfcbd5a73a6
Hieu Vu <star.hieu.99@gmail.com>
2023-09-27 23:01:53 +0700

    Implement sign in screen with events

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInScreen.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInScreen.kt
@@ -1,2 +1,47 @@
 package com.hieuwu.groceriesstore.presentation.authentication
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Backspace
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.hieuwu.groceriesstore.presentation.authentication.composables.IconTextInput
+
+@Composable
+fun SignInScreen(
+    modifier: Modifier = Modifier,
+    viewModel: SignInViewModel = hiltViewModel()
+) {
+    val email = viewModel.email.collectAsState()
+    val password = viewModel.password.collectAsState()
+    Column {
+        Text(text = "Sign In")
+        IconTextInput(
+            leadingIcon = Icons.Filled.Email,
+            trailingIcon = Icons.Filled.Backspace,
+            value = email.value,
+            placeholder = "Email",
+            onValueChange = {
+                viewModel.onEmailChange(it)
+            },
+            onTrailingIconClick = {
+                viewModel.onRemoveText()
+            },
+        )
+
+        IconTextInput(
+            leadingIcon = Icons.Filled.Email,
+            trailingIcon = Icons.Filled.Backspace,
+            value = password.value,
+            placeholder = "Password",
+            onValueChange = {
+                viewModel.onPasswordChange(it)
+            },
+            onTrailingIconClick = {},
+        )
+    }
+}
