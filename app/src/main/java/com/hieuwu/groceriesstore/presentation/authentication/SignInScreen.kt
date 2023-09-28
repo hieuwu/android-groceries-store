ab8c95a623726b6a60cfbca867e71d2583a76f54
Hieu Vu <star.hieu.99@gmail.com>
2023-09-28 22:21:33 +0700

    Update spacing

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInScreen.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/authentication/SignInScreen.kt
@@ -1,6 +1,8 @@
 package com.hieuwu.groceriesstore.presentation.authentication
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Backspace
 import androidx.compose.material.icons.filled.Email
@@ -8,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.hieuwu.groceriesstore.presentation.authentication.composables.IconTextInput
 
@@ -32,7 +36,7 @@ fun SignInScreen(
                 viewModel.onRemoveText()
             },
         )
-
+        Spacer(modifier = modifier.height(12.dp))
         IconTextInput(
             leadingIcon = Icons.Filled.Email,
             trailingIcon = Icons.Filled.Backspace,
