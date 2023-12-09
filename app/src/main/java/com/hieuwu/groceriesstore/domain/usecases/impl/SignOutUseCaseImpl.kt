a58bdbc3340076b4676d15a8dbec0ff850852375
Hieu Vu <star.hieu.99@gmail.com>
2023-12-09 13:00:51 +0700

    Replace hardcoded dispatchers

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/SignOutUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/SignOutUseCaseImpl.kt
@@ -1,15 +1,19 @@
 package com.hieuwu.groceriesstore.domain.usecases.impl
 
 import com.hieuwu.groceriesstore.data.repository.UserRepository
+import com.hieuwu.groceriesstore.di.IoDispatcher
 import com.hieuwu.groceriesstore.domain.usecases.SignOutUseCase
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class SignOutUseCaseImpl @Inject constructor(private val userRepository: UserRepository) :
+class SignOutUseCaseImpl @Inject constructor(
+    private val userRepository: UserRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) :
     SignOutUseCase {
     override suspend fun execute(input: SignOutUseCase.Input): SignOutUseCase.Output {
-        return withContext(Dispatchers.IO) {
+        return withContext(ioDispatcher) {
             userRepository.clearUser()
             SignOutUseCase.Output()
         }
