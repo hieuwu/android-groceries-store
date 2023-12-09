c551ea228f2061c6f6516dfb73fb77cfdecba1b7
Hieu Vu <star.hieu.99@gmail.com>
2023-12-09 13:00:56 +0700

    Replace hardcoded dispatchers

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/RefreshAppDataUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/RefreshAppDataUseCaseImpl.kt
@@ -3,18 +3,20 @@ package com.hieuwu.groceriesstore.domain.usecases.impl
 import com.hieuwu.groceriesstore.data.repository.CategoryRepository
 import com.hieuwu.groceriesstore.data.repository.ProductRepository
 import com.hieuwu.groceriesstore.data.repository.RecipeRepository
+import com.hieuwu.groceriesstore.di.IoDispatcher
 import com.hieuwu.groceriesstore.domain.usecases.RefreshAppDataUseCase
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RefreshAppDataUseCaseImpl @Inject constructor(
     private val productRepository: ProductRepository,
     private val categoryRepository: CategoryRepository,
-    private val recipeRepository: RecipeRepository
-) : RefreshAppDataUseCase {
+    private val recipeRepository: RecipeRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    ) : RefreshAppDataUseCase {
     override suspend fun execute(input: Unit) {
-        withContext(Dispatchers.IO) {
+        withContext(ioDispatcher) {
             categoryRepository.refreshDatabase()
             productRepository.refreshDatabase()
             recipeRepository.refreshDatabase()
--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/SearchProductUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/SearchProductUseCaseImpl.kt
@@ -1,15 +1,19 @@
 package com.hieuwu.groceriesstore.domain.usecases.impl
 
 import com.hieuwu.groceriesstore.data.repository.ProductRepository
+import com.hieuwu.groceriesstore.di.IoDispatcher
 import com.hieuwu.groceriesstore.domain.usecases.SearchProductUseCase
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class SearchProductUseCaseImpl @Inject constructor(private val productRepository: ProductRepository) :
+class SearchProductUseCaseImpl @Inject constructor(
+    private val productRepository: ProductRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+) :
     SearchProductUseCase {
     override suspend fun execute(input: SearchProductUseCase.Input): SearchProductUseCase.Output {
-        return withContext(Dispatchers.IO) {
+        return withContext(ioDispatcher) {
             val result = productRepository.searchProductsListByName(input.name)
             SearchProductUseCase.Output(result)
         }
