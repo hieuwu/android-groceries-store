326b63addfbd1721733611e7d66ed81b48184a8d
Hieu Vu <star.hieu.99@gmail.com>
2023-12-09 13:05:23 +0700

    Update scope

--- app/src/main/java/com/hieuwu/groceriesstore/di/DispatcherModule.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/di/DispatcherModule.kt
@@ -2,11 +2,14 @@ package com.hieuwu.groceriesstore.di
 
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Qualifier
 
 @Module
+@InstallIn(SingletonComponent::class)
 class DispatcherModule {
 
     @DefaultDispatcher
