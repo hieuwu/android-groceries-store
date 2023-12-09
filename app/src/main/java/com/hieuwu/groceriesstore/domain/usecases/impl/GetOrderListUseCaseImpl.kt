dcf9db0fb16f27b37a343de7156bb34c73989be8
Hieu Vu <star.hieu.99@gmail.com>
2023-12-09 13:00:59 +0700

    Replace hardcoded dispatchers

--- app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetOrderListUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/domain/usecases/impl/GetOrderListUseCaseImpl.kt
@@ -1,16 +1,18 @@
 package com.hieuwu.groceriesstore.domain.usecases.impl
 
 import com.hieuwu.groceriesstore.data.repository.OrderRepository
+import com.hieuwu.groceriesstore.di.IoDispatcher
 import com.hieuwu.groceriesstore.domain.usecases.GetOrderListUseCase
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetOrderListUseCaseImpl @Inject constructor(
-    private val orderRepository: OrderRepository
+    private val orderRepository: OrderRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : GetOrderListUseCase {
     override suspend fun execute(input: GetOrderListUseCase.Input): GetOrderListUseCase.Output {
-        return withContext(Dispatchers.IO) {
+        return withContext(ioDispatcher) {
             val result = orderRepository.getOrders()
             GetOrderListUseCase.Output.Success(data = result)
         }
