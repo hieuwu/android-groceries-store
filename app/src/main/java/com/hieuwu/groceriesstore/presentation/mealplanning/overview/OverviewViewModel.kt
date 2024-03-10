c9bd72ee15fd56a5a2feccaec3c3bd063a2c1af2
Hieu Vu <star.hieu.99@gmail.com>
2024-03-10 22:27:06 +0700

    Remove space, add sample data

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewViewModel.kt
@@ -1,4 +1,18 @@
 package com.hieuwu.groceriesstore.presentation.mealplanning.overview
 
-class OverviewViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OverviewViewModel @Inject constructor() : ViewModel() {
+
+    private val _days = MutableStateFlow(listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"))
+    val day: StateFlow<List<String>> = _days
+
+
 }
