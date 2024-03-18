9436e013ad6b6bd4e9f88e7b888f14a9c035bf99
Hieu Vu <star.hieu.99@gmail.com>
2024-03-18 22:03:21 +0700

    Add section views

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewScreen.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewScreen.kt
@@ -1,23 +1,24 @@
 package com.hieuwu.groceriesstore.presentation.mealplanning.overview
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
@@ -25,39 +26,64 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.hieuwu.groceriesstore.R
+import com.hieuwu.groceriesstore.presentation.mealplanning.overview.composable.LineTextButton
 
 @Composable
 fun OverViewScreen(
     modifier: Modifier = Modifier,
     viewModel: OverviewViewModel = hiltViewModel()
 ) {
-    Column(modifier = modifier.fillMaxSize()) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(20.dp)
+    ) {
         val weekDays = viewModel.day.collectAsState().value
-        LazyRow(modifier = modifier.fillMaxWidth()) {
-            items(weekDays) { currentDay ->
+        LazyVerticalGrid(
+            modifier = modifier.fillMaxWidth(),
+            columns = GridCells.Fixed(7)
+        ) {
+            itemsIndexed(weekDays) { index, currentDay ->
                 Box(
                     modifier = modifier
-                        .width(120.dp)
-                        .height(120.dp)
+                        .height(64.dp)
                         .padding(8.dp)
-                        .background(
-                            color = colorResource(id = R.color.colorPrimary).copy(alpha = 0.2f)
+
+                        .clip(
+                            shape = RoundedCornerShape(6.dp),
                         )
-                        .border(
-                            width = 0.5.dp,
-                            shape = RoundedCornerShape(5.dp),
-                            color = Color.LightGray
-                        ),
+                        .background(
+                            color = if (currentDay.isSelected.value)
+                                colorResource(id = R.color.colorPrimary)
+                            else Color.Transparent
+                        )
+                        .clickable {
+                            viewModel.onWeekDaySelected(index)
+                        },
                     contentAlignment = Alignment.Center
                 ) {
                     Text(
-                        modifier = modifier.fillMaxSize(),
-                        text = currentDay,
-                        textAlign = TextAlign.Center
+                        modifier = modifier
+                            .fillMaxSize()
+                            .padding(4.dp),
+                        text = currentDay.name,
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.labelLarge,
+                        color = Color.Black
                     )
                 }
             }
         }
+        LineTextButton(modifier = modifier,
+            textTitle = "Breakfast",
+            onButtonClick = {}
+        )
+        Text(
+            text = "Lunch"
+        )
+        Text(
+            text = "Dinner"
+        )
     }
 }
 
--- app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewViewModel.kt
@@ -1,5 +1,7 @@
 package com.hieuwu.groceriesstore.presentation.mealplanning.overview
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,8 +13,24 @@ import javax.inject.Inject
 @HiltViewModel
 class OverviewViewModel @Inject constructor() : ViewModel() {
 
-    private val _days = MutableStateFlow(listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"))
-    val day: StateFlow<List<String>> = _days
+    private val _days = MutableStateFlow(
+        mutableListOf(
+            WeekDay("Mon"), WeekDay("Tue"), WeekDay("Wed"),
+            WeekDay("Thu"), WeekDay("Fri"), WeekDay("Sat"), WeekDay("Sun")
+        )
+    )
+    val day: StateFlow<List<WeekDay>> = _days
 
-
+    fun onWeekDaySelected(index: Int) {
+        viewModelScope.launch {
+            for (i in 0 until _days.value.size) {
+                _days.value[i].isSelected.value = i == index
+            }
+        }
+    }
 }
+
+data class WeekDay(
+    val name: String,
+    val isSelected: MutableState<Boolean> = mutableStateOf(false),
+)
