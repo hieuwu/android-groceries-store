ea8f949d617076c9e9b9c10a99ae1cc65c02ebc0
Hieu Vu <star.hieu.99@gmail.com>
2024-03-20 22:52:36 +0700

    Display list

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewScreen.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewScreen.kt
@@ -8,14 +8,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,6 +36,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.hieuwu.groceriesstore.R
 import com.hieuwu.groceriesstore.presentation.mealplanning.overview.composable.LineTextButton
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OverViewScreen(
     modifier: Modifier = Modifier,
@@ -74,23 +83,40 @@ fun OverViewScreen(
                 }
             }
         }
+        val breakfastList = remember { listOf("Rice", "Egg") }
         LineTextButton(
             modifier = modifier,
             textTitle = "Breakfast",
             onButtonClick = { viewModel.onAddBreakfast() }
         )
-        LineTextButton(
-            modifier = modifier,
-            textTitle = "Lunch",
-            onButtonClick = { viewModel.onAddLunch() }
-        )
-        LineTextButton(
-            modifier = modifier,
-            textTitle = "Dinner",
-            onButtonClick = { viewModel.onAddDinner() }
-        )
+        LazyColumn(modifier = modifier.fillMaxWidth()) {
+            items(breakfastList) { meal ->
+                Card(
+                    onClick = { /*TODO*/ },
+                    modifier = modifier.padding(8.dp),
+                    elevation = CardDefaults.cardElevation(),
+                ) {
+                    Text(
+                        modifier = modifier.fillMaxWidth(),
+                        text = meal
+                    )
+                }
+            }
+
+        }
     }
+    LineTextButton(
+        modifier = modifier,
+        textTitle = "Lunch",
+        onButtonClick = { viewModel.onAddLunch() }
+    )
+    LineTextButton(
+        modifier = modifier,
+        textTitle = "Dinner",
+        onButtonClick = { viewModel.onAddDinner() }
+    )
 }
+
 
 @Composable
 @Preview
