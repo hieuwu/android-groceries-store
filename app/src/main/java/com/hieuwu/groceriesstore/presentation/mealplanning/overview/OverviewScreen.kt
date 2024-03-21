4845eba4c6404bf3f39e380dc00dbdf28af05141
Hieu Vu <star.hieu.99@gmail.com>
2024-03-21 22:08:29 +0700

    Extract composable

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewScreen.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewScreen.kt
@@ -1,42 +1,24 @@
 package com.hieuwu.groceriesstore.presentation.mealplanning.overview
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.hieuwu.groceriesstore.R
-import com.hieuwu.groceriesstore.presentation.core.widgets.WebImage
 import com.hieuwu.groceriesstore.presentation.mealplanning.overview.composable.LineTextButton
+import com.hieuwu.groceriesstore.presentation.mealplanning.overview.composable.MealItem
+import com.hieuwu.groceriesstore.presentation.mealplanning.overview.composable.WeekDayItem
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OverViewScreen(
     modifier: Modifier = Modifier,
@@ -54,38 +36,18 @@ fun OverViewScreen(
     ) {
         item {
             LazyVerticalGrid(
-                modifier = modifier.fillMaxWidth().height(64.dp),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(64.dp),
                 columns = GridCells.Fixed(7)
             ) {
                 itemsIndexed(weekDays) { index, currentDay ->
-                    Box(
-                        modifier = modifier
-                            .height(64.dp)
-                            .padding(8.dp)
-
-                            .clip(
-                                shape = RoundedCornerShape(6.dp),
-                            )
-                            .background(
-                                color = if (currentDay.isSelected.value)
-                                    colorResource(id = R.color.colorPrimary)
-                                else Color.Transparent
-                            )
-                            .clickable {
-                                viewModel.onWeekDaySelected(index)
-                            },
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            modifier = modifier
-                                .fillMaxSize()
-                                .padding(4.dp),
-                            text = currentDay.name,
-                            textAlign = TextAlign.Center,
-                            style = MaterialTheme.typography.labelLarge,
-                            color = Color.Black
-                        )
-                    }
+                    WeekDayItem(
+                        modifier = modifier,
+                        currentDay = currentDay,
+                        onItemClick = { viewModel.onWeekDaySelected(index) },
+                        index = index
+                    )
                 }
             }
         }
@@ -97,34 +59,10 @@ fun OverViewScreen(
             )
         }
         items(breakfastList) { meal ->
-            Card(
-                onClick = { /*TODO*/ },
-                modifier = modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(),
-                colors = CardDefaults.cardColors(containerColor = Color.White),
-                elevation = CardDefaults.elevatedCardElevation(),
-                shape = MaterialTheme.shapes.medium,
-            ) {
-                Row(modifier = modifier.fillMaxWidth()) {
-                    WebImage(
-                        model = meal.imageUrl,
-                        contentDescription = "",
-                        contentScale = ContentScale.FillBounds,
-                        modifier = modifier.size(124.dp)
-                    )
-                    Text(
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .padding(12.dp),
-                        text = meal.name
-                    )
-                }
-            }
+            MealItem(meal = meal, modifier = modifier)
         }
 
         item {
-
             LineTextButton(
                 modifier = modifier,
                 textTitle = "Lunch",
@@ -133,30 +71,7 @@ fun OverViewScreen(
         }
 
         items(lunchMeals) { meal ->
-            Card(
-                onClick = { /*TODO*/ },
-                modifier = modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(),
-                colors = CardDefaults.cardColors(containerColor = Color.White),
-                elevation = CardDefaults.elevatedCardElevation(),
-                shape = MaterialTheme.shapes.medium,
-            ) {
-                Row(modifier = modifier.fillMaxWidth()) {
-                    WebImage(
-                        model = meal.imageUrl,
-                        contentDescription = "",
-                        contentScale = ContentScale.FillBounds,
-                        modifier = modifier.size(124.dp)
-                    )
-                    Text(
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .padding(12.dp),
-                        text = meal.name
-                    )
-                }
-            }
+            MealItem(meal = meal, modifier = modifier)
         }
 
         item {
@@ -167,30 +82,7 @@ fun OverViewScreen(
             )
         }
         items(dinnerMeals) { meal ->
-            Card(
-                onClick = { /*TODO*/ },
-                modifier = modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(),
-                colors = CardDefaults.cardColors(containerColor = Color.White),
-                elevation = CardDefaults.elevatedCardElevation(),
-                shape = MaterialTheme.shapes.medium,
-            ) {
-                Row(modifier = modifier.fillMaxWidth()) {
-                    WebImage(
-                        model = meal.imageUrl,
-                        contentDescription = "",
-                        contentScale = ContentScale.FillBounds,
-                        modifier = modifier.size(124.dp)
-                    )
-                    Text(
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .padding(12.dp),
-                        text = meal.name
-                    )
-                }
-            }
+            MealItem(meal = meal, modifier = modifier)
         }
 
     }
