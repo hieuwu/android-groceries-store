553c54478ac99721a5544bc899bac8ca7d4a5b32
Hieu Vu <star.hieu.99@gmail.com>
2024-03-21 18:00:18 +0700

    Move down data source, retweak UI

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewScreen.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewScreen.kt
@@ -3,11 +3,12 @@ package com.hieuwu.groceriesstore.presentation.mealplanning.overview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -17,23 +18,22 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.hieuwu.groceriesstore.R
+import com.hieuwu.groceriesstore.presentation.core.widgets.WebImage
 import com.hieuwu.groceriesstore.presentation.mealplanning.overview.composable.LineTextButton
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -42,79 +42,158 @@ fun OverViewScreen(
     modifier: Modifier = Modifier,
     viewModel: OverviewViewModel = hiltViewModel()
 ) {
-    Column(
+    val breakfastList = viewModel.breakfastMeals.collectAsState().value
+    val weekDays = viewModel.day.collectAsState().value
+    val lunchMeals = viewModel.lunchMeals.collectAsState().value
+    val dinnerMeals = viewModel.dinnerMeals.collectAsState().value
+
+    LazyColumn(
         modifier = modifier
             .fillMaxSize()
             .padding(20.dp)
     ) {
-        val weekDays = viewModel.day.collectAsState().value
-        LazyVerticalGrid(
-            modifier = modifier.fillMaxWidth(),
-            columns = GridCells.Fixed(7)
-        ) {
-            itemsIndexed(weekDays) { index, currentDay ->
-                Box(
-                    modifier = modifier
-                        .height(64.dp)
-                        .padding(8.dp)
+        item {
+            LazyVerticalGrid(
+                modifier = modifier.fillMaxWidth().height(64.dp),
+                columns = GridCells.Fixed(7)
+            ) {
+                itemsIndexed(weekDays) { index, currentDay ->
+                    Box(
+                        modifier = modifier
+                            .height(64.dp)
+                            .padding(8.dp)
 
-                        .clip(
-                            shape = RoundedCornerShape(6.dp),
+                            .clip(
+                                shape = RoundedCornerShape(6.dp),
+                            )
+                            .background(
+                                color = if (currentDay.isSelected.value)
+                                    colorResource(id = R.color.colorPrimary)
+                                else Color.Transparent
+                            )
+                            .clickable {
+                                viewModel.onWeekDaySelected(index)
+                            },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            modifier = modifier
+                                .fillMaxSize()
+                                .padding(4.dp),
+                            text = currentDay.name,
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.labelLarge,
+                            color = Color.Black
                         )
-                        .background(
-                            color = if (currentDay.isSelected.value)
-                                colorResource(id = R.color.colorPrimary)
-                            else Color.Transparent
-                        )
-                        .clickable {
-                            viewModel.onWeekDaySelected(index)
-                        },
-                    contentAlignment = Alignment.Center
-                ) {
+                    }
+                }
+            }
+        }
+        item {
+            LineTextButton(
+                modifier = modifier,
+                textTitle = "Breakfast",
+                onButtonClick = { viewModel.onAddBreakfast() }
+            )
+        }
+        items(breakfastList) { meal ->
+            Card(
+                onClick = { /*TODO*/ },
+                modifier = modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                elevation = CardDefaults.elevatedCardElevation(),
+                shape = MaterialTheme.shapes.medium,
+            ) {
+                Row(modifier = modifier.fillMaxWidth()) {
+                    WebImage(
+                        model = meal.imageUrl,
+                        contentDescription = "",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = modifier.size(124.dp)
+                    )
                     Text(
                         modifier = modifier
-                            .fillMaxSize()
-                            .padding(4.dp),
-                        text = currentDay.name,
-                        textAlign = TextAlign.Center,
-                        style = MaterialTheme.typography.labelLarge,
-                        color = Color.Black
+                            .fillMaxWidth()
+                            .padding(12.dp),
+                        text = meal.name
                     )
                 }
             }
         }
-        val breakfastList = remember { listOf("Rice", "Egg") }
-        LineTextButton(
-            modifier = modifier,
-            textTitle = "Breakfast",
-            onButtonClick = { viewModel.onAddBreakfast() }
-        )
-        LazyColumn(modifier = modifier.fillMaxWidth()) {
-            items(breakfastList) { meal ->
-                Card(
-                    onClick = { /*TODO*/ },
-                    modifier = modifier.padding(8.dp),
-                    elevation = CardDefaults.cardElevation(),
-                ) {
-                    Text(
-                        modifier = modifier.fillMaxWidth(),
-                        text = meal
-                    )
-                }
-            }
 
+        item {
+
+            LineTextButton(
+                modifier = modifier,
+                textTitle = "Lunch",
+                onButtonClick = { viewModel.onAddLunch() }
+            )
         }
+
+        items(lunchMeals) { meal ->
+            Card(
+                onClick = { /*TODO*/ },
+                modifier = modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                elevation = CardDefaults.elevatedCardElevation(),
+                shape = MaterialTheme.shapes.medium,
+            ) {
+                Row(modifier = modifier.fillMaxWidth()) {
+                    WebImage(
+                        model = meal.imageUrl,
+                        contentDescription = "",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = modifier.size(124.dp)
+                    )
+                    Text(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(12.dp),
+                        text = meal.name
+                    )
+                }
+            }
+        }
+
+        item {
+            LineTextButton(
+                modifier = modifier,
+                textTitle = "Dinner",
+                onButtonClick = { viewModel.onAddDinner() }
+            )
+        }
+        items(dinnerMeals) { meal ->
+            Card(
+                onClick = { /*TODO*/ },
+                modifier = modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                elevation = CardDefaults.elevatedCardElevation(),
+                shape = MaterialTheme.shapes.medium,
+            ) {
+                Row(modifier = modifier.fillMaxWidth()) {
+                    WebImage(
+                        model = meal.imageUrl,
+                        contentDescription = "",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = modifier.size(124.dp)
+                    )
+                    Text(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(12.dp),
+                        text = meal.name
+                    )
+                }
+            }
+        }
+
     }
-    LineTextButton(
-        modifier = modifier,
-        textTitle = "Lunch",
-        onButtonClick = { viewModel.onAddLunch() }
-    )
-    LineTextButton(
-        modifier = modifier,
-        textTitle = "Dinner",
-        onButtonClick = { viewModel.onAddDinner() }
-    )
 }
 
 
--- app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewViewModel.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/OverviewViewModel.kt
@@ -21,6 +21,36 @@ class OverviewViewModel @Inject constructor() : ViewModel() {
     )
     val day: StateFlow<List<WeekDay>> = _days
 
+    private val _breakfastMeals = MutableStateFlow<List<Meal>>(value = emptyList())
+    val breakfastMeals: StateFlow<List<Meal>> = _breakfastMeals
+
+
+    private val _lunchMeals = MutableStateFlow<List<Meal>>(value = emptyList())
+    val lunchMeals: StateFlow<List<Meal>> = _lunchMeals
+
+    private val _dinnerMeals = MutableStateFlow<List<Meal>>(value = emptyList())
+
+    init {
+        viewModelScope.launch {
+            val meals = listOf(
+                Meal(
+                    name = "Chicken Fried Rice",
+                    imageUrl = ""
+                ),
+                Meal(
+                    name = "Chicken Fried Rice",
+                    imageUrl = ""
+                )
+            )
+            _breakfastMeals.emit(meals)
+            _lunchMeals.emit(meals)
+            _dinnerMeals.emit(meals)
+        }
+    }
+
+    val dinnerMeals: StateFlow<List<Meal>> = _dinnerMeals
+
+
     fun onWeekDaySelected(index: Int) {
         viewModelScope.launch {
             for (i in 0 until _days.value.size) {
@@ -45,4 +75,9 @@ class OverviewViewModel @Inject constructor() : ViewModel() {
 data class WeekDay(
     val name: String,
     val isSelected: MutableState<Boolean> = mutableStateOf(false),
+)
+
+data class Meal(
+    val name: String,
+    val imageUrl: String,
 )
