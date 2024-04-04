d356de8185449c1f4df0c327fd18a5ab67ec3ba2
Hieu Vu <star.hieu.99@gmail.com>
2024-04-04 23:42:42 +0700

    Remove unused import

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/composable/MealItem.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/composable/MealItem.kt
@@ -1,6 +1,5 @@
 package com.hieuwu.groceriesstore.presentation.mealplanning.overview.composable
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
