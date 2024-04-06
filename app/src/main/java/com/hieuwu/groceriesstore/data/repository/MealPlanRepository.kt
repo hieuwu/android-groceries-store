0d1eb56e5808ed3515bf758319c8bfa90233ad77
Hieu Vu <star.hieu.99@gmail.com>
2024-04-06 23:05:00 +0700

    Make network call to create meal plan

--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/MealPlanRepository.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/MealPlanRepository.kt
@@ -1,5 +1,5 @@
 package com.hieuwu.groceriesstore.data.repository
 
 interface MealPlanRepository {
-    fun addMealToPlan()
+    suspend fun addMealToPlan(name: String, ingredients: List<String>)
 }
--- app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/MealPlanRepositoryImpl.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/data/repository/impl/MealPlanRepositoryImpl.kt
@@ -1,13 +1,28 @@
 package com.hieuwu.groceriesstore.data.repository.impl
 
+import com.hieuwu.groceriesstore.data.network.dto.Meal
 import com.hieuwu.groceriesstore.data.repository.MealPlanRepository
+import com.hieuwu.groceriesstore.data.repository.UserRepository
 import io.github.jan.supabase.postgrest.Postgrest
+import java.util.UUID
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class MealPlanRepositoryImpl @Inject constructor(
-    private val postgrest: Postgrest
+    private val postgrest: Postgrest,
+    private val userRepository: UserRepository
 ) : MealPlanRepository {
-    override fun addMealToPlan() {
-        postgrest["meal_plans"].postgrest["meal_plans"].postgrest["meal_plans"].postgrest["meal_plans"]
+    override suspend fun addMealToPlan(name: String, ingredients: List<String>) {
+        userRepository.getCurrentUser().collect {
+            postgrest["meal_plans"].insert<Meal>(
+                Meal(
+                    id = UUID.randomUUID().toString(),
+                    name = name,
+                    ingredients = ingredients,
+                    creatorId = it?.id ?: "",
+                    weekDay = "mon"
+                )
+            )
+        }
     }
 }
