92fdc7b3c6cb3ff4d39f83b70d72912f631fed77
Hieu Vu <star.hieu.99@gmail.com>
2024-04-26 21:36:39 +0700

    Update id

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/Meal.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/mealplanning/overview/Meal.kt
@@ -1,6 +1,7 @@
 package com.hieuwu.groceriesstore.presentation.mealplanning.overview
 
 data class Meal(
+    val id: String,
     val name: String,
     val imageUrl: String,
     val ingredients: List<String> = listOf(),
