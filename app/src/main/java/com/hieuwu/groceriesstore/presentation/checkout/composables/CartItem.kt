8b0a9b136c708dca539bd44b5ba732cb7c1a2093
Hieu Vu <star.hieu.99@gmail.com>
2024-08-03 22:30:01 +0700

    Update package

--- app/src/main/java/com/hieuwu/groceriesstore/presentation/checkout/composables/CartItem.kt
+++ app/src/main/java/com/hieuwu/groceriesstore/presentation/checkout/composables/CartItem.kt
@@ -31,7 +31,7 @@ import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.hieuwu.groceriesstore.R
-import com.hieuwu.groceriesstore.core.models.LineItemModel
+import com.hieuwu.groceriesstore.models.LineItemModel
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
