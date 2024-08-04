097bfadf21cf7ace5bb25dde1bd0b38e63c256fc
Hieu Vu <star.hieu.99@gmail.com>
2024-08-04 21:25:47 +0700

    Add dependency for lib

--- models/build.gradle.kts
+++ models/build.gradle.kts
@@ -7,3 +7,7 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+}
